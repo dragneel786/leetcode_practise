58aaa1b0ba11d3acfb8c5a607e4a359f30523bc0
shahrukh_unbxd <mohammad.shahrukh@unbxd.com>
2021-12-02 12:00:06 +0530

    feat(cod): Code of the day 4 pushed

---
+++ LinkedNthNodeRemove.py
@@ -0,0 +1,21 @@
+def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    first = head
+    second = head
+    
+    for i in range(n - 1):
+        if not first.next:
+            break;
+        first = first.next
+    
+    prev = None
+    while(first.next):
+        first = first.next
+        prev = second
+        second = second.next
+        
+    if(second == head):
+        head = head.next
+    else:
+        prev.next = second.next
+    
+    return head
---
+++ MiddleLInkedList.py
@@ -0,0 +1,8 @@
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    fast = head
+    slow = head
+    while(fast != None and fast.next != None):
+        slow = slow.next
+        fast = fast.next.next
+    
+    return slow
