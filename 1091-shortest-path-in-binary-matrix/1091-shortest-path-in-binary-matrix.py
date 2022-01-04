f5da351978034cbb1ece5480678d0c977c85f488
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-04 12:08:19 +0530

    Time: 1633 ms (5.01%), Space: 14.7 MB (66.61%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -7,25 +7,18 @@ class Solution:
 
         q = Queue()
         path = 0
-        q.put((0, 0))
-        q.put((-1, -1))
+        q.put((0, 0, 1))
         grid[0][0] = 1
         while(not q.empty()):
-            r, c = q.get()
+            r, c, d = q.get()
             if(r == n - 1 and c == n - 1):
-                return path + 1
-
-            if(r == -1 and c == -1):
-                path += 1
-                if(not q.empty()):
-                    q.put((-1, -1))   
-                continue
+                return d
 
             for i, j in ((r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)\
                 , (r + 1, c + 1), (r + 1, c - 1), (r - 1, c + 1), (r - 1, c - 1)):
 
                 if(i > -1 and j > -1 and i < n and j < n and not grid[i][j]):
                     grid[i][j] = 1
-                    q.put((i, j))
-
+                    q.put((i, j, d + 1))
+                    
         return -1
