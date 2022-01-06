bdf38b6ef1ac70e86bab7c8b02cfc47f175366a4
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-06 14:07:49 +0530

    Time: 56 ms (85.70%), Space: 14.5 MB (63.13%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,19 +1,17 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
+        nums = nums + [11]
         nums.sort()
         self.genPermute(nums, res)
         return res
 
     def genPermute(self, nums, res, op = []):
-        if(not nums):
-            if(op not in res):
-                res.append(op)
+        if(len(nums) == 1):
+            res.append(op)
             return
 
-        self.genPermute(nums[1:], res, op + [nums[0]])
-        for i in range(1, len(nums)):
-            if(nums[i] != nums[0] and nums[i] != nums[i - 1]):
-                nums[0], nums[i] = nums[i], nums[0]
-                self.genPermute(nums[1:], res, op + [nums[0]])
-                nums[0], nums[i] = nums[i], nums[0]
+        for i in range(0, len(nums) - 1):
+            if(nums[i] != nums[i - 1]):
+                self.genPermute(nums[0: i] + nums[i + 1: ], res, op + [nums[i]])
+            
