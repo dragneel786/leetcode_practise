a3375a21f6a6ef93e2e97f1c40bde84b7c76ea74
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-11 19:46:42 +0530

    Time: 59 ms (17.79%), Space: 14.3 MB (98.47%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,28 +1,18 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        n = len(nums)
-        if(n < 3):
-            return 0
-        dp = [float('inf') for _ in range(n)]
-        val = nums[0] - nums[1]
+        currCount = 0
+        currDiff = 0
+        prevDiff = float('-inf')
+        res = 0
+        for i in range(1, len(nums)):
+            currDiff = nums[i] - nums[i - 1]
+            if(currDiff == prevDiff):
+                currCount += 1
+            else:
+                currCount = 1
+                prevDiff = currDiff
 
-        for i in range(2, n):
-            temp = nums[i - 1] - nums[i]
-            if(val == temp):
-                dp[i - 1], dp[i] = temp, temp
-            val = temp
-            
-        length = 2
-        count = 0
-        for i in range(n - 2):
-            newList = [float('inf')] * (n - length + 1)
-            for j in range(n - length):
-                temp1 = dp[j + 1]
-                temp2 = dp[j + 2]
-                if(temp1 != float('inf') and temp2 != float('inf') and temp1 == temp2):
-                    newList[j + 1] = temp1
-                    count += 1
-            dp = newList
-            length += 1
+            if(currCount > 1):
+                res += (currCount - 1)
 
-        return count
+        return res
