073131dd861298cf0089b65300bdfbfcf6949bd8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-12 21:43:54 +0530

    Time: 35 ms (33.41%), Space: 14.4 MB (35.49%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,7 +1,6 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         leng_last = 0
-        found = False
         for i in range(len(s) - 1, -1, -1):
             if(s[i] == " "):
                 if(leng_last):
