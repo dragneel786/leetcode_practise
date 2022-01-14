1942ec11e387bb56cc8f21c7fb5fc9b8a9e4f738
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-14 16:54:17 +0530

    Time: 1676 ms (43.66%), Space: 14.8 MB (35.49%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,15 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int, memo = {}) -> int:
-        dp = [-1] * (amount + 1)
+        dp = [amount + 1 for i in range(amount + 1)]
         dp[0] = 0
+        coins.sort()
         for i in range(1, amount + 1):
-            minVal = float('inf')
             for coin in coins:
                 idx = i - coin
-                if(idx >= 0 and dp[idx] >= 0):
-                    minVal = min(minVal, 1 + dp[idx])
-
-            if(minVal != float("inf")):
-                dp[i] = minVal
-
-        return dp[amount]
+                if(idx < 0):
+                    break
+                
+                if(dp[idx] != amount + 1):
+                    dp[i] = min(dp[i], 1 + dp[idx])
+            
+                    
+        return dp[amount] if (dp[amount] != amount + 1) else -1
