bb5d01f6b83bf99370457913d5f56033dce21618
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-15 21:12:44 +0530

    Time: 24 ms (97.45%), Space: 14.2 MB (86.07%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -3,14 +3,12 @@ class Solution:
         n = len(s)
         dp = [0] * (n + 1)
         dp[n] = 1
-        if(s[n - 1] != "0"):
-            dp[n - 1] = 1
-        for i in range(n - 2, -1, -1):
+        for i in range(n - 1, -1, -1):
             if(s[i] == "0"):
                 dp[i] = 0
             else:
                 dp[i] = dp[i + 1]
-                if(int(s[i: i + 2]) < 27):
+                if(i < n - 1 and int(s[i: i + 2]) < 27):
                     dp[i] += dp[i + 2]
 
         return dp[0]
