0da5e7714b448089ff337dec73ad94e6adaa2719
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-16 16:49:08 +0530

    Time: 468 ms (15.13%), Space: 19.4 MB (39.15%) - LeetHub

--- 384-shuffle-an-array/384-shuffle-an-array.py
+++ 384-shuffle-an-array/384-shuffle-an-array.py
@@ -3,11 +3,11 @@ class Solution:
     original = list()
     curr = list()
     def __init__(self, nums: List[int]):
-        self.original = nums[:]
-        self.curr = nums[:]
+        self.original = nums
+        self.curr = nums.copy()
 
     def reset(self) -> List[int]:
-        self.curr = self.original[:]
+        self.curr = self.original.copy()
         return self.curr
 
     def shuffle(self) -> List[int]:
