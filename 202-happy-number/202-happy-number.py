f121ff156e3b6faffe76cc667dbf91cb988afe1d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-17 16:34:51 +0530

    Time: 69 ms (6.06%), Space: 14.2 MB (75.05%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,35 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        fast, slow = n, n
-        while(True):
-            slow = self.getSquareSum(slow)
-            fast = self.getSquareSum(self.getSquareSum(fast))
-            if(slow == fast):
-                break
-
-        if(slow == 1):
-            return True
+        lookup = set()
+        while(n not in lookup):
+            lookup.add(n)
+            temp = 0
+            while(n):
+                rem = n % 10
+                n //= 10
+                temp += (rem ** 2)
+            
+            if(temp == 1):
+                return True
+            
+            n = temp
+            
         return False
-
-    def getSquareSum(self, n):
-        temp = n
-        n = 0
-        while(temp):
-            rem = (temp % 10)
-            temp //= 10
-            n += (rem ** 2)
-        return n
-#         lookup = set()
-#         while(n not in lookup):
-#             lookup.add(n)
-#             temp = n
-#             n = 0
-#             while(temp != 0):
-#                 rem = temp % 10
-#                 temp //= 10
-#                 n += (rem ** 2)
-            
-#             if(n == 1):
-#                 return True
-            
-#         return False
