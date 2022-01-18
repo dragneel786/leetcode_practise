bb1f0dc082525e0d93be0dc6b3fac6805cd23190
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-18 14:59:16 +0530

    Time: 25 ms (89.21%), Space: 14 MB (90.27%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -6,8 +6,6 @@ class Solution:
         f1 = 0
         f2 = 1
         for i in range(n - 1):
-            temp = f1 + f2
-            f1 = f2
-            f2 = temp
-        
+            f1, f2 = f2, f1 + f2
+            
         return f2
