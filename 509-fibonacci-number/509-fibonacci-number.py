426e81c38c4514e19f20f00c4503e77dd2a26bbc
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-18 14:58:29 +0530

    Time: 42 ms (45.93%), Space: 14 MB (97.66%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if(n == 0):
+            return 0
+        
+        f1 = 0
+        f2 = 1
+        for i in range(n - 1):
+            temp = f1 + f2
+            f1 = f2
+            f2 = temp
+        
+        return f2
