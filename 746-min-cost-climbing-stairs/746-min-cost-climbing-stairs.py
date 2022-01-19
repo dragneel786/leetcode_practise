0b3cb7051846825372fa3c326d35695876c86246
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-19 16:24:17 +0530

    Time: 59 ms (68.82%), Space: 14.4 MB (74.80%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,16 +1,20 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        return min(self.getMinCost(cost), self.getMinCost(cost[1:]))
-        
-    def getMinCost(self, cost):
         n = len(cost)
-        if(n == 1):
-            return cost[0]
-        if(n <= 2):
-            dp = [cost[0], cost[0] + cost[1]]
-        else:
-            dp = [cost[0] + cost[1], cost[0] + cost[2]]
-        for i in range(3, n):
-            dp[0], dp[1] = dp[1], min(cost[i] + dp[0], cost[i] + dp[1])
-
+        dp = [cost[0], cost[1]]
+        for i in range(2, n):
+            dp[0], dp[1] = dp[1], min(dp[0] + cost[i], dp[1] + cost[i])
         return min(dp)
+        
+#     def getMinCost(self, cost):
+#         n = len(cost)
+#         if(n == 1):
+#             return cost[0]
+#         if(n <= 2):
+#             dp = [cost[0], cost[0] + cost[1]]
+#         else:
+#             dp = [cost[0] + cost[1], cost[0] + cost[2]]
+#         for i in range(3, n):
+#             dp[0], dp[1] = dp[1], min(cost[i] + dp[0], cost[i] + dp[1])
+
+#         return min(dp)
