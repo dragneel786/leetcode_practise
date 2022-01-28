96856a560bac11013727e016aea5af3496ca3b1c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-28 13:09:59 +0530

    Time: 43 ms (49.93%), Space: 14.1 MB (100.00%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,18 +1,17 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        currCount = 0
         currDiff = 0
-        prevDiff = float('-inf')
+        prevDiff = float("-inf")
+        count = 0
         res = 0
         for i in range(1, len(nums)):
-            currDiff = nums[i] - nums[i - 1]
+            currDiff = nums[i - 1] - nums[i]
             if(currDiff == prevDiff):
-                currCount += 1
+                count += 1
             else:
-                currCount = 1
+                count = 1
                 prevDiff = currDiff
-
-            if(currCount > 1):
-                res += (currCount - 1)
-
+            
+            if(count > 1):
+                res += (count - 1)
         return res
