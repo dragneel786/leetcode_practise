2bcc9e6b83b2d499e6dccc534f4f6a5a5f14fb9f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-01-28 18:51:38 +0530

    Time: 720 ms (73.35%), Space: 21.3 MB (83.31%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,9 +3,6 @@ class Solution:
         bs = -prices[0]
         ss = 0
         for i in range(1, len(prices)):
-            temp_bs = max(bs, ss - prices[i])
-            temp_ss = max(ss, bs + prices[i] - fee)
-            bs = temp_bs
-            ss = temp_ss
-
+            bs, ss = max(bs, ss - prices[i]), max(ss, bs + prices[i] - fee)
+            
         return ss
