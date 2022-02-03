6721ae92389cbb635ec13121982935c90665a54f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-02-03 20:18:25 +0530

    Time: 5144 ms (18.60%), Space: 22 MB (9.84%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -10,9 +10,9 @@ class Solution:
         for i in range(1, n):
             for j in range(0, n - i):
                 if(s[j] == s[j + i] and (i == 1 or dp[j + 1][j + i - 1])):
-                    if(i + 1 > length):
-                        res = s[j : j + i + 1]
-                        length = i + 1
+                    # if(i + 1 > length):
+                    res = s[j : j + i + 1]
+                        # length = i + 1
                     dp[j][j + i] = 1
 
         return res
