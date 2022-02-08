9ab9bf0b911a5217c4d4200ea43eacea23e28fb3
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-02-08 21:22:03 +0530

    Time: 100 ms (32.21%), Space: 14.2 MB (86.51%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,26 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        dic = defaultdict(int)
+        for n in nums:
+            dic[n] += 1
+
+        nums = list(set(nums))
+        nums.sort()
+        max_till = nums[0] * dic[nums[0]]
+        max_before = 0
+        for i in range(1, len(nums)):
+            temp = 0
+            val = (nums[i] * dic[nums[i]])
+            if(nums[i - 1] + 1 == nums[i]):
+                temp = max_before + val
+                if(temp > max_till):
+                    max_before = max_till
+                    max_till = temp
+                else:
+                    max_before = max_till
+            else:
+                temp = max_till + val
+                max_before = max_till
+                max_till = temp
+
+        return max_till
