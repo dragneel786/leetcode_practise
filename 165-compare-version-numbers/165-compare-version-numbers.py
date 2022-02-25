522772b438b6cffd1c4610f38601e266aa3a5cc4
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-02-25 17:32:10 +0530

    Time: 59 ms (12.58%), Space: 13.9 MB (92.33%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.py
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -1,26 +1,17 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        m = len(version1)
-        n = len(version2)
-        i = 0
-        j = 0
+        version1 = list(map(int, version1.split(".")))
+        version2 = list(map(int, version2.split(".")))
+        i, j = 0, 0
+        m, n = len(version1), len(version2)
         while(i < m or j < n):
-            val1 = 0
-            val2 = 0
-            while(i < m and version1[i] != "."):
-                val1 = (val1 * 10) + int(version1[i])
-                i += 1
+            val1 = version1[i] if i < m else 0
+            val2 = version2[j] if j < n else 0
             i += 1
-
-            while(j < n and version2[j] != "."):
-                val2 = (val2 * 10) + int(version2[j])
-                j += 1
             j += 1
-
-            if(val1 < val2):
-                return -1
-
             if(val1 > val2):
                 return 1
-
+            if(val1 < val2):
+                return -1
+        
         return 0
