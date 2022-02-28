94dedcb0c412d7c0f7078a8c1e6cd93b4acdb520
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-02-28 14:29:32 +0530

    Time: 41 ms (56.43%), Space: 13.8 MB (99.05%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -1,12 +1,9 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         n = len(nums)
-        if(n == 0 or n == 1):
-            return list(map(str, nums))
-        
         ranges = []
-        start = nums[0]
         nums.append(float('inf'))
+        start = nums[0]
         for i in range(1, n + 1):
             if(nums[i] != nums[i - 1] + 1):
                 if(start == nums[i - 1]):
