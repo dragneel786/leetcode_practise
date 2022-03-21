cdb1299148254fef682e19e8dc0b416f1c5bfc86
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-03-21 20:54:25 +0530

    Time: 43 ms (83.87%), Space: 14 MB (36.30%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,30 +1,20 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        count = [0] * 26
+        hash = defaultdict(int)
         n = len(s)
-        for i in range(1, n):
-            count[ord(s[i]) - 97] += 1
-
+        for i in range(n):
+            hash[s[i]] = i
 
         s += '#'
         res = []
-        seen = set(s[0])
-        size = 1
-        for i in range(1, n + 1):
-            clear = True
-            for k in seen:
-                if(count[ord(k) - 97] != 0):
-                    clear = False
-                    break
+        maxIdx = 0
+        prev = -1
+        for i in range(n + 1):
+            maxIdx = max(maxIdx, hash[s[i]])
+            if(i == maxIdx):
+                res.append(maxIdx - prev)
+                prev = maxIdx
+                maxIdx = hash[s[i]]
 
-            if(clear):
-                res.append(size)
-                size = 0
-                seen.clear()
-
-            if(s[i] == '#'):
+            if(i == n):
                 return res
-
-            size += 1
-            seen.add(s[i])
-            count[ord(s[i]) - 97] -= 1
