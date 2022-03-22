3f8f44fb95dd833381129eba835bae59a844668b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-03-22 20:41:15 +0530

    Time: 48 ms (47.28%), Space: 13.7 MB (99.87%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,13 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        digits = [0] + digits
-        rem = 1
-        for i in range(len(digits) - 1, -1, -1):
-            if(rem == 0):
-                return digits[1:]
+        n = len(digits) - 1
+        carry = (digits[n] + 1) // 10
+        digits[n] = (digits[n] + 1) % 10
+        n -= 1
+        while(n > -1 and carry):
+            val = digits[n] + carry
+            carry = val // 10
+            digits[n] = val % 10
+            n -= 1
 
-            sumVal = (digits[i] + rem)
-            digits[i] = sumVal % 10
-            rem = sumVal // 10
+        if(carry):
+            digits = [carry] + digits
 
         return digits
+        
