41ef4d63d2050dbe518eb877064ca7110a592cfa
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-03-22 20:42:25 +0530

    Time: 66 ms (9.83%), Space: 13.9 MB (67.96%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,9 +1,7 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n = len(digits) - 1
-        carry = (digits[n] + 1) // 10
-        digits[n] = (digits[n] + 1) % 10
-        n -= 1
+        carry = 1
         while(n > -1 and carry):
             val = digits[n] + carry
             carry = val // 10
