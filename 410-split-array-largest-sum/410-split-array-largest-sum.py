fa4535d4e8b82c6e944e2e0745d3f65117ef3fa5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-03-31 14:24:32 +0530

    Time: 64 ms (43.67%), Space: 14 MB (69.95%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        n = len(nums)
+        low = float('-inf')
+        high = 0
+        for i in range(n):
+            low = max(low, nums[i])
+            high += nums[i]
+
+        ans = 0
+        while(low <= high):
+            tillSum = 0
+            split = 1
+            mid = low + (high - low) // 2
+            for i in range(n):
+                if(tillSum + nums[i] > mid):
+                    tillSum = 0
+                    split += 1
+                tillSum += nums[i]
+
+            if(split == m):
+                ans = mid
+
+            if(split <= m):
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans if ans else max(nums)
+
+   
+    
+
