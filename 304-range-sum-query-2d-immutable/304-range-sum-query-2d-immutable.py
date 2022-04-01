194313e010db30d246c1000cf23f0ae016035b98
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-04-01 21:16:03 +0530

    Time: 1532 ms (91.74%), Space: 24.6 MB (88.62%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,30 +1,30 @@
 class NumMatrix:
-    prefix = None
-    def __init__(self, matrix: List[List[int]]):
-        rows = len(matrix)
-        cols = len(matrix[0])
 
-        for c in range(1, cols):
-            matrix[0][c] += matrix[0][c - 1]
-        for r in range(1, rows):
-            matrix[r][0] += matrix[r - 1][0]
-        for r in range(1, rows):
-            for c in range(1, cols):
-                matrix[r][c] += matrix[r - 1][c] + matrix[r][c - 1] - matrix[r - 1][c - 1]
-        
-        self.prefix = matrix
+    def __init__(self, matrix: List[List[int]]):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if(i > 0):
+                    matrix[i][j] += matrix[i - 1][j]
+                
+                if(j > 0):
+                    matrix[i][j] += matrix[i][j - 1]
+
+                if(i > 0 and j > 0):
+                    matrix[i][j] -= matrix[i - 1][j - 1]
+                    
+        self.matrix = matrix
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        val = self.prefix[row2][col2]
-        row1 -= 1
-        col1 -= 1
-        if(row1 >= 0):
-            val -= self.prefix[row1][col2]
-        if(col1 >= 0):
-            val -= self.prefix[row2][col1]
-        if(col1 >=0 and row1 >= 0):
-            val += self.prefix[row1][col1]
+        val = self.matrix[row2][col2]
+        if(row1 > 0):
+            val -= self.matrix[row1 - 1][col2]
         
+        if(col1 > 0):
+            val -= self.matrix[row2][col1 - 1]
+        
+        if(row1 > 0 and col1 > 0):
+            val += self.matrix[row1 - 1][col1 - 1]
+
         return val
 
 
