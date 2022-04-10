c713a910f10af400d4571fd5de26d9e477d4fa84
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-04-10 15:28:58 +0530

    Time: 261 ms (88.46%), Space: 14.7 MB (92.98%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,18 +1,17 @@
 class MyCalendar:
 
     def __init__(self):
-        self.q = []
-
+        self.books = []
+    
     def book(self, start: int, end: int) -> bool:
-        idx = bisect.bisect_left(self.q, (start, end))
-        if idx-1 >= 0 is not None and start < self.q[idx-1][1]:
+        idx = bisect.bisect_left(self.books, (start, end))
+        if(idx - 1 >= 0 and self.books[idx - 1][1] > start):
             return False
-        elif idx < len(self.q) and self.q[idx][0] < end:
+        if(idx < len(self.books) and self.books[idx][0] < end):
             return False
-        # add
-        self.q.insert(idx, (start, end))
+        
+        self.books.insert(idx, (start, end))
         return True
-
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
