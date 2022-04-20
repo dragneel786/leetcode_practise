423d877c122b1232ff7cda2a34642f67a4450261
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-04-20 19:18:46 +0530

    Time: 126 ms (72.25%), Space: 14.2 MB (51.92%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,27 +1,13 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        def getCount(m):
-            h = len(m) - 1
-            l = 0
-            idx = -1
-            while(l <= h):
-                mid = l + (h - l) // 2
-                if(m[mid] == 0):
-                    idx = mid
-                    h = mid - 1
-                elif(m[mid] > 0):
-                    l = mid + 1
-    
-            return idx if(idx != -1) else len(m)
-        
         counts = []
-        for i in range(len(mat)):
-            counts.append([getCount(mat[i]), i])
-
-        res = []
+        for row in range(len(mat)):
+            idx = bisect.bisect(list(reversed(mat[row])),0)
+            counts.append([len(mat[row]) - idx, row])
+        
         heapify(counts)
+        res = []
         while(k):
             res.append(heappop(counts)[1])
             k -= 1
         return res
-    
