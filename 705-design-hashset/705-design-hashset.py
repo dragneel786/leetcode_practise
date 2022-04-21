d3eb72ba25457e823e47563032c74a6634dcb5ff
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-04-21 19:08:29 +0530

    Time: 338 ms (43.09%), Space: 40.5 MB (10.21%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,24 +1,19 @@
-class ListNode:
-    def __init__(self, val = 0, next = None):
-        self.val = val
-        self.next = next
-        
 class MyHashSet:
 
     def __init__(self):
         self.arr = [False] * (10 ** 6 + 1)
         
     def add(self, key: int) -> None:
-        self.arr[key % ((10 ** 6 + 1))] = True
+        self.arr[key] = True
 
     def remove(self, key: int) -> None:
-        if(self.arr[key % ((10 ** 6 + 1))]):
-            self.arr[key % ((10 ** 6 + 1))] = False
+        if(self.arr[key]):
+            self.arr[key] = False
             return key
         
 
     def contains(self, key: int) -> bool:
-        return self.arr[key % ((10 ** 6 + 1))]
+        return self.arr[key]
 
 
 # Your MyHashSet object will be instantiated and called as such:
