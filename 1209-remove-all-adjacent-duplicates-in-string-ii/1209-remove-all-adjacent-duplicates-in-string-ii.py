989c1f0bd5b36b476d111e98b92d86a0284719db
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-06 22:09:12 +0530

    Time: 1120 ms (5.01%), Space: 24.4 MB (8.93%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -3,13 +3,13 @@ class Solution:
         st = deque()
         for c in s:
             if(len(st) and st[-1][0] == c):
-                val = [c, st[-1][1] + 1]
+                val = (c, st[-1][1] + 1)
                 st.append(val)
                 if(val[1] == k):
                     for _ in range(k):
                         st.pop()
             else:
-                val = [c, 1]
+                val = (c, 1)
                 st.append(val)
         
         res = ""
