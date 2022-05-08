b81d50cb57e033c6cb90d97111aafdee46178366
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-08 20:27:19 +0530

    Time: 693 ms (25.38%), Space: 37.6 MB (23.26%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -1,9 +1,14 @@
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        res = []
+        res = nums[:]
         for i in range(1, n):
-            for j in range(n - i + 1):
-                res.append(sum(nums[j: j + i]))
+            nums[i] += nums[i - 1]
+            
+        for i in range(2, n):
+            res.append(nums[i - 1])
+            for j in range(1, n - i + 1):
+                res.append(nums[j + i - 1] - nums[j - 1])
         res.sort()
-        res.append(sum(nums))
+        res.append(nums[-1])
+        # print(res)
         return sum(res[left - 1: right]) % (10 ** 9 + 7)
