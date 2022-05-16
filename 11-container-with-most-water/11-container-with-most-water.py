c88d02f3f497fa088bb834272bc4558234f9a49e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-16 23:56:22 +0530

    Time: 1181 ms (21.28%), Space: 27.5 MB (17.30%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -3,12 +3,13 @@ class Solution:
         n = len(height)
         i = 0
         j = n - 1
-        area = 0
+        maxA = 0
         while(i < j):
-            area = max(area, min(height[i], height[j]) * (j - i))
+            computed = min(height[i], height[j]) * (j - i)
+            maxA = max(maxA, computed)
             if(height[i] < height[j]):
                 i += 1
             else:
                 j -= 1
-            
-        return area
+        
+        return maxA
