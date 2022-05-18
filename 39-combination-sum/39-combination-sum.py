22bb4351badf78867f5b83c06cdac348c58a885b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-19 00:16:09 +0530

    Time: 141 ms (31.14%), Space: 14.1 MB (26.08%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,19 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        def getCombo(candi, t, op = []):
-            if(not t):
-                return [op.copy()]
-
-            if(t < 0):
-                return []
-
-            res = []
-            for i in range(len(candi)):
-                temp = getCombo(candi[i:], t - candi[i], op + [candi[i]])
-                if(temp):
-                    res.extend(temp)
-
-            return res
-    
-        return getCombo(candidates, target)
+        combos = []
+        def genCombo(cand, target, op = []):
+            if(target < 0 or not cand):
+                return
+            
+            if(target == 0):
+                combos.append(op)
+                return 
+            
+            genCombo(cand, target - cand[0], op + [cand[0]])
+            genCombo(cand[1:], target, op)
+        
+        genCombo(candidates, target)
+        return combos
