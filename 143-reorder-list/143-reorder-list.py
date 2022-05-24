6f6124593be049583135ce36db5ea3b1fdd0ee51
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-25 00:02:14 +0530

    Time: 193 ms (5.04%), Space: 49 MB (5.07%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,20 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        def orderMyList(head, node):
-            if(not head):
-                return node
-            node = orderMyList(head.next, node)
+        def reOrder(head, node):
+            if(not head.next):
+                head.next = node.next
+                node.next = head
+                return head.next
+            
+            node = reOrder(head.next, node)
             if(not node):
                 return None
-
+            
             if(node == head or node.next == head):
                 head.next = None
                 return None
-
-            temp = node.next
-            node.next = head
-            head.next = temp
-            return temp
         
-        orderMyList(head, head)
+            head.next = node.next
+            node.next = head
+            return head.next
+    
+        if(head.next and head.next.next):
+            reOrder(head, head)
+            
+            
