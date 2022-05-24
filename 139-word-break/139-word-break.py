971e7bace456c0752a10cb7ac53d3d809e5f5547
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-24 19:44:29 +0530

    Time: 124 ms (5.08%), Space: 14 MB (71.81%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,15 +1,14 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        wordDict = set(wordDict)
-        isBreakable = False
-        idx = [n]
+        seen = set(wordDict)
+        dp = [False] * (n + 1)
+        dp[n] = True
         for i in range(n - 1, -1, -1):
-            isBreakable = False
-            for j in idx:
-                if(s[i: j] in wordDict):
-                    isBreakable = True
-                    idx = [i] + idx
-                    break
-
-        return isBreakable
+            for j in range(n - 1, i - 1, -1):
+                print(s[i:j + 1])
+                if(s[i:j + 1] in seen):
+                    dp[i] = dp[j + 1]
+                    if(dp[i]):
+                        break
+        return dp[0]
