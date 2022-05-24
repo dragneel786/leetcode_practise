bce48b505b4e9cef573fea770f8b79cd5cb7a07a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-24 19:55:58 +0530

    Time: 87 ms (8.81%), Space: 14 MB (46.49%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -7,8 +7,7 @@ class Solution:
         for i in range(n - 1, -1, -1):
             for j in range(n - 1, i - 1, -1):
                 print(s[i:j + 1])
-                if(s[i:j + 1] in seen):
-                    dp[i] = dp[j + 1]
-                    if(dp[i]):
-                        break
+                if(dp[j + 1] and s[i:j + 1] in seen):
+                    dp[i] = True
+                    break
         return dp[0]
