4d16bdb1199e22ed8fe19fdf386d457f9d9d071d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-05-29 15:43:04 +0530

    Time: 136 ms (20.27%), Space: 17.4 MB (37.60%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,20 +1,8 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        size = 0
-        for i in intervals:
-            size = max(i[1], size)
-        
-        if(not size):
-            return True
-        inter = [0] * (size + 2)
-        for i in intervals:
-            inter[i[1]] += -1
-            inter[i[0]] += 1
-        
-        for i in range(1, size):
-            inter[i] += inter[i - 1]
-            if(inter[i] > 1):
+        intervals.sort()
+        for i in range(1, len(intervals)):
+            if(intervals[i - 1][1] > intervals[i][0]):
                 return False
-        
         return True
                 
