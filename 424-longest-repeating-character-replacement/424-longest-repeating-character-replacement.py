e0eade5d153255767a43f81061e0aa476bdfa5c5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-03 20:00:24 +0530

    Time: 124 ms (88.59%), Space: 13.9 MB (56.54%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,25 +1,24 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        chars = defaultdict(int)
+        chars[s[0]] += 1
+        
+        index = count = 1
+        maxCount = last = 0
         n = len(s)
-        if(n - 1 <= k):
-            return n
-
-        maxL = k + 1
-        first = Counter(s[0:maxL])
-        count = max(first.values())
-        for w in range(k + 2, n + 1):
-            if(w <= n):
-                first[s[w - 1]] = first.get(s[w - 1], 0) + 1
-                count = max(first[s[w - 1]], count)
-            look, c = first.copy(), count
-            for j in range(n - w + 1):
-                if(w - c <= k):
-                    maxL = w
-                    break
-                look[s[j]] -= 1
-                if(j + w < n):
-                    look[s[j + w]] += 1
-                    c = max(look[s[j + w]], c)
-            if(w != maxL):
-                break
-        return maxL
+        while(index < n):
+            chars[s[index]] += 1
+            count += 1
+            
+            if(count - chars[s[last]] > k):
+                chars[s[last]] -= 1
+                last += 1
+                count -= 1
+            
+            maxCount = max(count, maxCount)
+            index += 1
+        
+        if(count - chars[s[last]] < k):
+            count += k
+        
+        return min(max(count, maxCount), n)
