f908a4c77a33ae0328b51d283c93f49bc2bd1d0f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-05 10:46:56 +0530

    Time: 124 ms (28.17%), Space: 13.8 MB (80.48%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -8,17 +8,17 @@ class Solution:
             
             for i in range(n):
                 if(validate(invalid, [r, i])):
-                    invalid[r] = i
-                    placeQueens(r + 1, invalid)
-                    invalid.pop(r)
+                    # invalid[r] = i
+                    placeQueens(r + 1, invalid + [(r, i)])
+                    # invalid.pop(r)
         
         def validate(invalid, pos):
-            for k in invalid.keys():
-                if(invalid[k] == pos[1] or\
-                  abs(pos[0] - k) == abs(pos[1] - invalid[k])):
+            for v in invalid:
+                if(v[1] == pos[1] or\
+                  abs(pos[0] - v[0]) == abs(pos[1] - v[1])):
                     return False
             return True
         
         count = [0]
-        placeQueens(0, dict())
+        placeQueens(0, list())
         return count[0]
