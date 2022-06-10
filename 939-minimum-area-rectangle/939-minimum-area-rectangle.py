0fd8c89ec4ef9672ef1a9118689694666b202822
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-10 23:27:46 +0530

    Time: 1721 ms (44.66%), Space: 14.3 MB (50.49%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.py
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.py
@@ -10,6 +10,7 @@ class Solution:
             x1, y1 = points[i]
             for j in range(i + 1, n):
                 x2, y2 = points[j]
+                if(x1 == x2 or y1 == y2): continue
                 area = abs(x1 - x2) * abs(y1 - y2)
                 if(area and (minA > area) and (x1, y2) in coord and (x2, y1) in coord):
                     minA = area
