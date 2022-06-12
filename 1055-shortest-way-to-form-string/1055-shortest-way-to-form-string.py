d8f76bc36a45f3431cc29f2adfcc19e0440113e3
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-12 23:00:21 +0530

    Time: 132 ms (13.30%), Space: 14.2 MB (10.46%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,24 +1,19 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        maps = dict()
+        maps = defaultdict(lambda:([0] * n))
         n = len(source)
         for i, c in enumerate(source):
-            if(c not in maps):
-                maps[c] = ([0] * n)
             maps[c][i] = i + 1
         
         for c in maps.keys():
-            prev = 0
-            for j in range(n - 1, -1, -1):
-                if(maps[c][j]):
-                    prev = maps[c][j]
-                maps[c][j] = prev
-        
+            for j in range(n - 2, -1, -1):
+                if(not maps[c][j]):
+                    maps[c][j] = maps[c][j + 1]
         
         j = 0
         counts = 1
         for c in target:
-            if(c not in maps): return -1
+            if(not maps[c][0]): return -1
             
             if(j == n or not maps[c][j]):
                 counts += 1
