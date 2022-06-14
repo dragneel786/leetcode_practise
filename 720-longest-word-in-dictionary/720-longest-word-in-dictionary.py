c62b08ae580f636463d156acb7a4e281dffe313d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-14 20:14:18 +0530

    Time: 236 ms (27.58%), Space: 14.5 MB (71.57%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -1,19 +1,21 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
         words = set(words)
-        combo = [""]
-        while(True):
-            temp = []
-            for c in combo:
-                for i in range(26):
-                    w = c + chr(i + 97)
-                    if(w in words):
-                        temp.append(w)
-            if(not temp):
-                break
-            combo = temp[:]
-
-        return combo[0]
+        ans = ""
+        for w in words:
+            if(len(ans) <= len(w)):
+                present = True
+                for i in range(len(w) - 1, 0, -1):
+                    if(w[0: i] not in words):
+                        present = False
+                        break
+                if(present):
+                    if(len(ans) < len(w)):
+                        ans = w
+                    else:
+                        ans = min(ans, w)
+        return ans
+                    
 
                 
         
