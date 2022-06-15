027e1bbdb848a7258d5ed86dac6221aff4de5b1e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-15 19:03:00 +0530

    Time: 214 ms (62.94%), Space: 14.3 MB (88.74%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,38 +1,16 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        n = len(words)
-        if(n == 1):
-            return 1
+        wordSet = set(words)
+        words.sort(key=lambda x: -len(x))
+        length = defaultdict(lambda:1)
+        maxChain = 1
+        for w in words:
+            for i in range(len(w)):
+                term = w[0:i] + w[i + 1:]
+                if(term in wordSet):
+                    length[term] = max(length[term], length[w] + 1)
+                    maxChain = max(length[term], maxChain)
         
-        def isSubseq(w1, w2):
-            j = 0
-            n = len(w2)
-            for i in range(len(w1)):
-                if(j == n):
-                    break
-                
-                if(w1[i] == w2[j]):
-                    j += 1
-                    
-            if(j == n):
-                return True
-            return False
+        return maxChain
             
-        words.sort(key=lambda s: len(s))
-        dp = [0] * n
-        dp[0] = 1
-        res = 1
-        for i in range(1, n):
-            maxChain = 0
-            for j in range(i - 1, -1, -1):
-                diff = len(words[i]) - len(words[j])
-                if(diff > 1):
-                    break
-                
-                if(diff and isSubseq(words[i], words[j])):
-                    maxChain = max(maxChain, dp[j])
-                
-            dp[i] = maxChain + 1
-            res = max(dp[i], res)
-        return res
         
