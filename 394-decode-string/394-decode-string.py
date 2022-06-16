4320c8127cc9ae7f601453fd1619ebb0080093ca
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-16 22:52:55 +0530

    Time: 53 ms (22.46%), Space: 13.8 MB (70.84%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -2,21 +2,19 @@ class Solution:
     def decodeString(self, s: str) -> str:
         st = deque()
         for c in s:
-            if(c == "]"):
-                t = ""
-                while(not st[-1].isdigit()):
-                    p = st.pop()
-                    if(p == "["):
-                        continue
-                    t = p + t
-                st.append(int(st.pop()) * t)
-            else:
+            if(c != "]"):
                 if(st and st[-1].isdigit() and c.isdigit()):
-                    st.append(st.pop() + c)
+                    st[-1] += c
                 else:
                     st.append(c)
+                continue
+            
+            t = ""
+            while(not st[-1].isdigit()):
+                p = st.pop()
+                if(p == "["):
+                    continue
+                t = p + t
+            st.append(int(st.pop()) * t)
         
-        res = ""
-        while(st):
-            res = st.pop() + res
-        return res
+        return "".join(st)
