6653da607271641739d781c1ef390092db2f996f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-16 19:33:17 +0530

    Time: 7833 ms (7.17%), Space: 21.7 MB (12.52%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,22 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        subStr = s[0]
-        maxS = 1
-        mat = [([0] * n) for _ in range(n)]
+        dp = [([0] * n) for _ in range(n)]
+        res = s[0]
         for i in range(n):
-            mat[i][i] = 1
+            dp[i][i] = 1
             if(i < n - 1 and s[i] == s[i + 1]):
-                mat[i][i + 1] = 2
-                maxS = 2
-                subStr = s[i : i + 2]
-
-
+                dp[i][i + 1] = 1
+                res = s[i:i + 2]
+        
         for i in range(2, n):
-            for j in range(i, n):
-                if(s[j - i] == s[j] and mat[j - i + 1][j - 1]):
-                    mat[j - i][j] = mat[j - i + 1][j - 1] + 2
-                    if(maxS < mat[j - i][j]):
-                        subStr = s[j - i: j + 1]
-                        maxS = mat[j - i][j]
-        return subStr
+            temp = None
+            for j in range(n - i):
+                if(s[j] == s[j + i] and dp[j + 1][j + i - 1]):
+                    dp[j][j + i] = 1
+                    if(not temp):
+                        temp = s[j:j + i + 1]
+                
+            if(temp):
+                res = temp
+        return res
+        
+            
