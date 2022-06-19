b84b5c6d795cf0673a8ef4d0cf7802010b764377
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-19 13:35:44 +0530

    Time: 2232 ms (5.04%), Space: 21.5 MB (22.44%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,28 +1,38 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
         def makeTrie():
             for w in products:
                 t = trie
                 for c in w:
                     t[c] = t.get(c, dict())
-                    t[c][3] = t[c].get(3, list())
-                    if(len(t[c][3]) < 3) :
-                        t[c][3].append(w)
                     t = t[c]
+                t["$"] = None
+        
+        def getThree(tie, s):
+            if(len(three) == 3):
+                return
+            if("$" in tie):
+                three.append(s)
                 
+            for i in range(26):
+                ch = chr(97 + i)
+                if(ch in tie):
+                    getThree(tie[ch], s + ch)
+                    
         trie = dict()
         makeTrie()
         res = []
-        empty = False
-        for c in searchWord:
-            if(c not in trie or empty):
-                empty = True
-                res.append([])
-                continue
-            res.append(trie[c][3])
+        n = len(searchWord)
+        p = ""
+        for i,c in enumerate(searchWord):
+            if(c not in trie):
+                extras = [[]] * (n - i)
+                res.extend(extras)
+                break
+            p += c
+            three = []
+            getThree(trie[c], p)
+            res.append(three)
             trie = trie[c]
-            
-        
         
         return res
