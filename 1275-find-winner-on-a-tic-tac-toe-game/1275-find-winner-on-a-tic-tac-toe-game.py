313240e6ce84e022a2f871caed81caa4b9f4cb62
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-22 19:57:48 +0530

    Time: 59 ms (23.55%), Space: 13.9 MB (83.74%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -8,17 +8,14 @@ class Solution:
         for i, m in enumerate(moves):
             x, y = m
             inc = -1 if (i % 2) else 1
-            rows[x] += inc
-            cols[y] += inc
-            if(x == y):
-                dig += inc
-            if((x, y) in [(2, 0), (0, 2), (1, 1)]):
-                anti += inc
+            rows[x], cols[y] = rows[x] + inc, cols[y] + inc
+            dig += inc if(x == y) else 0
+            anti += inc if(x + y == 2) else 0
+            if(rows[x] == 3 or cols[y] == 3 or anti == 3 or dig == 3):
+                return 'A'
+            if(rows[x] == -3 or cols[y] == -3 or anti == -3 or dig == -3):
+                return 'B'
             
-        if(3 in rows or 3 in cols or anti == 3 or dig == 3):
-            return 'A'
-        if(-3 in rows or -3 in cols or anti == -3 or dig == -3):
-            return 'B'
         if(len(moves) == 9):
             return 'Draw'
         return 'Pending'
