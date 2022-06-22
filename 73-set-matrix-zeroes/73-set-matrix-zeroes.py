d38fd42084afb54c4f89e0545340218798d2ebda
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-22 16:56:15 +0530

    Time: 166 ms (65.00%), Space: 14.8 MB (52.34%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,18 +4,15 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         rows, cols = len(matrix), len(matrix[0])
-        zeroRow, zeroCol = False, False
+        markC = False
         for r in range(rows):
             for c in range(cols):
                 if(not matrix[r][c]):
-                    if(c != 0):
-                        matrix[0][c] = 0
+                    matrix[0][c] = 0
+                    if(r == 0):
+                        markC = True
                     else:
-                        zeroRow = True
-                    if(r != 0):
                         matrix[r][0] = 0
-                    else:
-                        zeroCol = True
                     
         
         for r in range(1, rows):
@@ -26,12 +23,7 @@ class Solution:
             if(not matrix[0][c]):
                 for i in range(rows):
                     matrix[i][c] = 0
-        
-        if(zeroRow):
-            for i in range(rows):
-                matrix[i][0] = 0
-        
-        if(zeroCol):
+        if(markC):
             for i in range(cols):
                 matrix[0][i] = 0
                 
