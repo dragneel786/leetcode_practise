7d74e80760f8289cc4867a43792f609b0e34ccbc
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-24 17:33:30 +0530

    Time: 56 ms (22.74%), Space: 13.8 MB (55.57%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.py
+++ 551-student-attendance-record-i/551-student-attendance-record-i.py
@@ -4,18 +4,19 @@ class Solution:
         # keep checking the Absent count if it gets >= 2 return False
         # keep checking for the consecutive L if get >= 3 return False
         # return true
+        s += "P"
         acount = lcount = 0
         for c in s:
-            if(c == "L"):
-                lcount += 1
-            elif(c == "A"):
-                acount += 1
-                lcount = 0
-            else:
-                lcount = 0
-            
             if(lcount == 3 or acount >= 2):
                 return False
+            if(c == "L"):
+                lcount += 1
+                continue
+            if(c == "A"):
+                acount += 1
+            lcount = 0
+            
+            
         return True
             
             
