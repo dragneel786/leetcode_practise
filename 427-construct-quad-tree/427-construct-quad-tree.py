b58292132517f2b17529fac0d418556af0bd165c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-25 20:09:16 +0530

    Time: 293 ms (5.00%), Space: 14.9 MB (19.29%) - LeetHub

--- 427-construct-quad-tree/427-construct-quad-tree.py
+++ 427-construct-quad-tree/427-construct-quad-tree.py
@@ -12,50 +12,26 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
-        newNode = Node()
-        if(self.checkSame(grid)):
-            newNode.isLeaf = True
-            newNode.val = grid[0][0]
-            return newNode
         
-        n = len(grid)
-        ns = n // 2
-        sgrid = [([0] * ns) for _ in range(ns)]
-        #top Right.
-        for r in range(ns):
-            for c in range(ns):
-                sgrid[r][c] = grid[r][c]
-        newNode.topLeft = self.construct(sgrid)
-        
-        #top left
-        for r in range(ns):
-            for c in range(ns, n):
-                sgrid[r][c - ns] = grid[r][c]
-        newNode.topRight = self.construct(sgrid)
-        
-        #top Right.
-        for r in range(ns, n):
-            for c in range(ns):
-                sgrid[r - ns][c] = grid[r][c]
-        newNode.bottomLeft = self.construct(sgrid)
-        
-        #top Right.
-        for r in range(ns, n):
-            for c in range(ns, n):
-                sgrid[r - ns][c - ns] = grid[r][c]
-        newNode.bottomRight = self.construct(sgrid)
-        
-        return newNode
-        
-    def checkSame(self, grid):
-        ones, zeros = 0, 0
-        s = len(grid)
-        for r in range(s):
-            for c in range(s):
-                if(grid[r][c]):
-                    ones += 1
-                else:
-                    zeros += 1
-                if(ones and zeros):
-                    return False
-        return True
+        def helper(x, y, n):
+            if(n == 1):
+                return Node(grid[x][y], True)
+            
+            result = Node()
+            topLeft = helper(x, y, n // 2)
+            topRight = helper(x, y + n // 2, n // 2)
+            bottomLeft = helper(x + n // 2, y, n // 2)
+            bottomRight = helper(x + n // 2, y + n // 2, n // 2)
+            if(topLeft.isLeaf and topRight.isLeaf and bottomLeft.isLeaf and bottomRight.isLeaf and\
+               topLeft.val == topRight.val == bottomLeft.val == bottomRight.val):
+                result.isLeaf = True
+                result.val = grid[x][y]
+            else:
+                result.topLeft = topLeft
+                result.topRight = topRight
+                result.bottomLeft = bottomLeft
+                result.bottomRight = bottomRight
+            return result
+            
+        return helper(0, 0, len(grid))
+                
