fa28bc7c324e5e5a9e5ef961fc7a2e08f0165fc3
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-27 12:16:46 +0530

    Time: 179 ms (46.42%), Space: 14.1 MB (96.82%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -1,29 +1,51 @@
 class Solution:
     def validUtf8(self, data: List[int]) -> bool:
-        binR = []
-        for d in data:
-            b = bin(d)[2:]
-            binR.append((8 - len(b)) * '0' + bin(d)[2:])
+        def maskBit(num):
+            bits = 0
+            mask = 1 << 7
+            while(mask & num):
+                bits += 1
+                mask >>= 1
+            return bits
         
-        h = {"0":0, "110":1, "1110":2, "11110":3}
         form = 0
-        for b in binR:
-            if(form == 0):
-                if(b[0] in h):
-                    form = 0
-                elif(b[:5] in h):
-                    form = h[b[:5]]
-                elif(b[:4] in h):
-                    form = h[b[:4]]
-                elif(b[:3] in h):
-                    form = h[b[:3]]
-                else:
+        for d in data:
+            if(not form):
+                b = maskBit(d)
+                if(b == 1 or b > 4):
                     return False
+                form = b - 1 if b > 0 else 0
             else:
-                if(b[:2] != "10"):
+                if(maskBit(d) != 1):
                     return False
                 form -= 1
         return form == 0
+                
+        
+#         binR = []
+#         for d in data:
+#             b = bin(d)[2:]
+#             binR.append((8 - len(b)) * '0' + bin(d)[2:])
+        
+#         h = {"0":0, "110":1, "1110":2, "11110":3}
+#         form = 0
+#         for b in binR:
+#             if(form == 0):
+#                 if(b[0] in h):
+#                     form = 0
+#                 elif(b[:5] in h):
+#                     form = h[b[:5]]
+#                 elif(b[:4] in h):
+#                     form = h[b[:4]]
+#                 elif(b[:3] in h):
+#                     form = h[b[:3]]
+#                 else:
+#                     return False
+#             else:
+#                 if(b[:2] != "10"):
+#                     return False
+#                 form -= 1
+#         return form == 0
             
             
             
