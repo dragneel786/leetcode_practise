ae90b4b61595316d5ffd7a3a7a04ffe2dc527a47
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-06-29 23:41:57 +0530

    Time: 73 ms (89.03%), Space: 13.8 MB (99.54%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,5 +1,17 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
+        #greedy
+        res = 0
+        x, y = abs(x), abs(y)
+        while(x > 4 or y > 4):
+            res += 1
+            if(x >= y):
+                x -= 2
+                y -= 1 if(y >= 1) else -1
+            else:
+                x -= 1 if(x >= 1) else -1
+                y -= 2
+        
         st = deque()
         drc = [(2, 1), (1, 2), (-1, 2), (-2, 1), \
                (-2, -1), (-1, -2), (1, -2), (2, -1)]
@@ -9,7 +21,7 @@ class Solution:
             for _ in range(len(st)):
                 r, c, m = st.popleft()
                 if((r, c) == (x, y)):
-                    return m
+                    return m + res
                 for dr, dc in drc:
                     nr, nc = r + dr, c + dc
                     if((nr, nc) not in visit):
