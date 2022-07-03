0725f14f3fcb89dd134a418db99545c7580d5f33
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-03 23:15:46 +0530

    Time: 966 ms (47.50%), Space: 16.5 MB (64.93%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canReorderDoubled(self, arr: List[int]) -> bool:
+        c = Counter(arr)
+        for x in sorted(c, key=abs):
+            if(c[x] > c[2 * x]):
+                return False
+            c[2 * x] -= c[x]
+        return True
