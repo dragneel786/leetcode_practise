5d30d21b0a7776e22ab21edbdf627ab9cee5f507
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-04 20:11:34 +0530

    Time: 467 ms (5.02%), Space: 15.5 MB (70.41%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        def findAndAdd():
+            for i,d in enumerate(ind):
+                if(not d and i not in visited):
+                    q.append(i)
+                    visited.add(i)
+        
+        graph = defaultdict(lambda:[])
+        ind = [0] * numCourses
+        for p in prerequisites:
+            graph[p[1]].append(p[0])
+            ind[p[0]] += 1
+        
+        q = deque()
+        taken = 0
+        res = []
+        visited = set()
+        findAndAdd()
+        while(q):
+            for _ in range(len(q)):
+                c = q.popleft()
+                taken += 1
+                res.append(c)
+                for v in graph[c]:
+                    ind[v] -= 1
+            findAndAdd()
+        
+        return res if(taken == numCourses) else []
+                
+                    
+            
