7270980d807f591ab0d60185fa6e38c5f1e0778a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-05 20:14:27 +0530

    Time: 63 ms (80.57%), Space: 14.3 MB (60.90%) - LeetHub

--- 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
+++ 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
@@ -1,19 +1,21 @@
 class Solution:
-    def mergeStones(self, stones: List[int], K: int) -> int:
+    def mergeStones(self, stones: List[int], k: int) -> int:
         n = len(stones)
-        if (n - 1) % (K - 1): return -1
+        if((n - 1) % (k - 1) != 0):
+            return -1
+
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i + 1] = prefix[i] + stones[i]
 
-        import functools
         @functools.lru_cache(None)
         def dp(i, j):
-            if j - i + 1 < K: return 0
-            res = min(dp(i, mid) + dp(mid + 1, j) for mid in range(i, j, K - 1))
-            if (j - i) % (K - 1) == 0:
+            if((j - i + 1) < k): return 0
+            res = min(dp(i, mid) + dp(mid + 1, j) for mid in range(i, j, k - 1))
+            if((j - i) % (k - 1) == 0):
                 res += prefix[j + 1] - prefix[i]
             return res
+
         return dp(0, n - 1)
             
         
