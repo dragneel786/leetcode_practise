8f075136b47c45936bd4fad47047988dc2529fc1
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-05 11:38:53 +0530

    Time: 794 ms (52.00%), Space: 21.6 MB (38.35%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        slots1.sort()
+        slots2.sort()
+        i = j = 0
+        m, n = len(slots1), len(slots2)
+        while(i < m and j < n):
+            s1, e1 = slots1[i]
+            s2, e2 = slots2[j]
+            if(e2 < s1):
+                j += 1
+            elif(s2 > e1):
+                i += 1
+            else:
+                s, e = max(s1, s2), min(e1, e2)
+                if(s + duration <= e):
+                    return [s, s + duration]
+                if(e1 > e2): j += 1
+                else: i += 1
+        return []
+            
+            
+        
