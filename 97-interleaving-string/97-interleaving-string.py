de7466b158f0c687e95019a045906c05215cc129
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-07 14:17:13 +0530

    Time: 74 ms (37.10%), Space: 14.9 MB (38.00%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -4,17 +4,20 @@ class Solution:
         if(l1 + l2 != l3):
             return False
         
-        @functools.lru_cache(None)
-        def checkInter(i, j, k):
+        def checkInter(i, j, k, memo = {}):
             nonlocal l1, l2, l3
             if(i == l1 and j == l2 and k == l3):
                 return True
+            
+            if((i, j, k) in memo):
+                return memo[(i, j, k)]
             
             res = False
             if(i < l1 and s1[i] == s3[k]):
                 res |= checkInter(i + 1, j, k + 1)
             if(not res and j < l2 and s2[j] == s3[k]):
                 res |= checkInter(i, j + 1, k + 1)
+            memo[(i, j, k)] = res
             return res
         
         return checkInter(0, 0, 0)
