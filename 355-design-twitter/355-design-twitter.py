9a902c4e230221150815d925e0482aee73606940
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-09 21:02:00 +0530

    Time: 70 ms (5.15%), Space: 14.1 MB (18.53%) - LeetHub

--- 355-design-twitter/355-design-twitter.py
+++ 355-design-twitter/355-design-twitter.py
@@ -1,22 +1,48 @@
+class Tweets:
+    def __init__(self, id = -1, timestamp = -1, next = None):
+        self.id = id
+        self.timestamp = timestamp
+        self.next = next
+    
+    def __lt__(self, other):
+        return other.timestamp < self.timestamp
+        
+class Users:
+    def __init__(self, userId = -1, head = None):
+        self.id = userId
+        self.head = head
+
 class Twitter:
 
     def __init__(self):
         self.ff = defaultdict(set)
-        self.mr = []
+        self.mr = defaultdict(lambda:0)
+        self.ts = 1
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.mr.append((userId, tweetId))
+        tweet = Tweets(tweetId, self.ts)
+        if(not self.mr[userId]):
+            self.mr[userId] = Users(userId, tweet)
+        else:
+            tweet.next = self.mr[userId].head
+            self.mr[userId].head = tweet
+        self.ts += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        follows = self.ff[userId]
+        follows = [self.mr[userId].head] if(self.mr[userId]) else []
+        for f in self.ff[userId]:
+            if(self.mr[f]):
+                follows.append(self.mr[f].head)
+        heapify(follows)
         res = []
-        for m in range(len(self.mr) - 1, -1, -1):
-            u, t = self.mr[m]
-            if(u in follows or u == userId):
-                res.append(t)
-                
-            if(len(res) == 10):
-                break
+        j = 10
+        while(follows and j):
+            t = heappop(follows)
+            res.append(t.id)
+            t = t.next
+            if(t):
+                heappush(follows, t)
+            j -= 1
         return res
 
     def follow(self, followerId: int, followeeId: int) -> None:
