69cf863928213b4d48155fb0bbd82e1d1f4b8212
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-14 18:48:09 +0530

    Time: 113 ms (75.02%), Space: 18.6 MB (96.61%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,24 +6,22 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def buildThem(pre, ino):
-            nonlocal p
-            if(not ino):
+        
+        def builder(ino, pre, stop = None):
+            if(not ino or stop == ino[-1]):
                 return None
             
-            newNode = TreeNode(pre[p])
-            idx = getIndex(pre[p], ino)
-            p += 1
-            newNode.left = buildThem(pre, ino[:idx])
-            newNode.right = buildThem(pre, ino[idx + 1:])
-            return newNode
+            node = TreeNode(pre.pop())
+            node.left = builder(ino, pre, node.val)
+            if(ino and node.val == ino[-1]):
+                ino.pop()
+            node.right = builder(ino, pre, stop)
+            return node
         
-        def getIndex(v, arr):
-            for i,a in enumerate(arr):
-                if(a == v):
-                    return i
-            
-            
-        p = 0
-        return buildThem(preorder, inorder)
+        inorder.reverse()
+        preorder.reverse()
+        return builder(inorder, preorder)
+    
+        
+        
             
