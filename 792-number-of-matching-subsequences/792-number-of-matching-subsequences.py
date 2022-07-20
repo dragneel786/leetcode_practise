145aec877559d29f42947e48b20b7d30243dbfee
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-20 20:12:40 +0530

    Time: 574 ms (74.25%), Space: 17.3 MB (24.29%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,26 +1,25 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        def isSub(w):
-            i = 0
-            for c in w:
-                if(not indexes[c]):
+        
+        def isSubSeq(word, counter):
+            pos = 0
+            for i, c in enumerate(word):
+                idx = bisect_left(counter[c], pos)
+                if(idx >= len(counter[c])):
                     return False
-                idx = bisect_left(indexes[c], i)
-                if(idx == len(indexes[c])):
-                    return False
-                i = indexes[c][idx] + 1
+                pos = counter[c][idx] + 1
             return True
-        
-        indexes = defaultdict(lambda:[])
-        for i,c in enumerate(s):
-            indexes[c].append(i)
-        
-        res = 0
+    
+        counter = defaultdict(list)
+        for i, c in enumerate(s):
+            counter[c].append(i)
+            
+        counts = 0
         for w in words:
-            if(isSub(w)):
-                res += 1
+            if(isSubSeq(w, counter)):
+                counts += 1
+        return counts
         
-        return res
-                
+        
         
         
