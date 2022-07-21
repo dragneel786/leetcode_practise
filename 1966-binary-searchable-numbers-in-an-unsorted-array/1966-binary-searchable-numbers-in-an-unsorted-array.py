f4fc30b6c20e8742b9bff03276a39721b51697e8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-21 19:54:06 +0530

    Time: 1108 ms (10.91%), Space: 28 MB (50.91%) - LeetHub

--- 1966-binary-searchable-numbers-in-an-unsorted-array/1966-binary-searchable-numbers-in-an-unsorted-array.py
+++ 1966-binary-searchable-numbers-in-an-unsorted-array/1966-binary-searchable-numbers-in-an-unsorted-array.py
@@ -5,10 +5,10 @@ class Solution:
         max_v, min_v = -inf, inf
         for i in range(n):
             max_v = max(max_v, nums[i])
-            state[i] = max_v <= nums[i]
+            state[i] = max_v == nums[i]
         
         counts = 0
         for i in range(n - 1, -1, -1):
             min_v = min(min_v, nums[i])
-            counts += state[i] and min_v >= nums[i]
+            counts += state[i] and min_v == nums[i]
         return counts
