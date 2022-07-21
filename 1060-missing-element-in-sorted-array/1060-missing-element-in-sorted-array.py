97c0a7edab3fb0c43021b0ceb4ca1c851a1e5b5f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-21 22:58:34 +0530

    Time: 276 ms (99.53%), Space: 20.2 MB (86.92%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
@@ -1,18 +1,19 @@
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
         
-        def getMissingTill():
-            return bisect_right(nums, mid)
+        missing_count = lambda idx: nums[idx] - nums[0] - idx
         
-        low = nums[0]
-        high = nums[-1] + k
-        while(low <= high):
-            mid = ((high - low) >> 1) + low
-            idx = getMissingTill()
-            val = ((mid - nums[0]) + 1) - idx
-            if(val == k and nums[idx - 1] != mid):
-                return mid
-            if(val < k):
-                low = mid + 1
+        n = len(nums)
+        if(k > missing_count(n - 1)):
+            return nums[-1] + k - missing_count(n - 1)
+        
+        low, high = 0, n - 1
+        while(low != high):
+            pivot = ((high - low) >> 1) + low
+            
+            if(missing_count(pivot) < k):
+                low = pivot + 1
             else:
-                high = mid - 1
+                high = pivot
+        
+        return nums[low - 1] + k - missing_count(low - 1)
