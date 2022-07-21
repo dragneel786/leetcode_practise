10cf41398d9caf3d46227b74967c7c51e7f4afa5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-21 15:01:46 +0530

    Time: 64 ms (19.16%), Space: 13.9 MB (16.00%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,12 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
         sign = -1 if(x < 0) else 1
+        bound = 2**31 - 1
         rev = 0
         x = abs(x)
         while(x):
             rem = x % 10
+            if(sign == 1 and ((rev > bound // 10) or (rev == bound // 10 and rem > 7))): return 0
+            if((sign == -1) and ((rev > (bound + 1) // 10)\
+                     or ((rev == (bound + 1) // 10) and (rem > 8)))): return 0
             rev = (rev * 10) + rem
             x //= 10
-        
-        bound = 2**31
-        return sign * rev if(-bound <= rev < bound) else 0
+               
+        return rev * sign
