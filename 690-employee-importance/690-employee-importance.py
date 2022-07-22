1b5a14982ec465130cca439554ea6c9e8504808a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-22 19:56:23 +0530

    Time: 378 ms (5.15%), Space: 15.6 MB (58.83%) - LeetHub

--- 690-employee-importance/690-employee-importance.py
+++ 690-employee-importance/690-employee-importance.py
@@ -14,16 +14,16 @@ class Solution:
             graph[emp.id] = emp
         
         q = deque([id])
-        seen = set([id])
+        # seen = set([id])
         total_importance = 0
         while(q):
             for _ in range(len(q)):
                 emp = graph[q.popleft()]
                 total_importance += emp.importance
                 for sub in emp.subordinates:
-                    if(sub not in seen):
-                        q.append(sub)
-                        seen.add(sub)
+                    # if(sub not in seen):
+                    q.append(sub)
+                        # seen.add(sub)
         return total_importance
                     
                 
