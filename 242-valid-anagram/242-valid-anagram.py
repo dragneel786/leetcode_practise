8ea4fda8d45eadf5d21562801897986c99e361c0
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-28 19:31:57 +0530

    Time: 71 ms (59.30%), Space: 14.5 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,4 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if(len(s) != len(t)):
             return False
-        return sorted(s) == sorted(t)
+        letters = [0] * 26
+        for c in s: letters[ord(c) - ord('a')] += 1
+        for c in t: letters[ord(c) - ord('a')] -= 1
+        return not any(letters)
