b60e7d0679e5199c411c53adbb23bbb2b73caed0
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-28 23:50:08 +0530

    Time: 62 ms (21.70%), Space: 13.9 MB (43.63%) - LeetHub

--- 1138-alphabet-board-path/1138-alphabet-board-path.py
+++ 1138-alphabet-board-path/1138-alphabet-board-path.py
@@ -1,35 +1,14 @@
 class Solution:
     def alphabetBoardPath(self, target: str) -> str:
-        
-        def createBoard():
-            alpha = dict()
-            for r, row in enumerate(["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"]):
-                for c, ch in enumerate(row):
-                    alpha[ch] = (r, c)
-            return alpha
-        
-        def bfs(start, end):
-            dic = {"U":(-1, 0), "D": (1, 0), "R":(0, 1), "L":(0, -1)}
-            q = deque([(start, "")])
-            visited = set([start])
-            while(q):
-                (x, y), steps = q.popleft()
-                if((x, y) == end):
-                    return steps + "!"
-                
-                for di, (dx, dy) in dic.items():
-                    nx, ny = x + dx, y + dy
-                    if((nx, ny) not in visited and\
-                       (nx, ny) in validPositions):
-                        q.append(((nx, ny), steps + di))
-                        visited.add((nx, ny))
-            
-        board = createBoard()
-        validPositions = set(board.values())
-        prev = "a"
+        ox, oy = 0, 0
         res = ""
-        for curr in target:
-            res += bfs(board[prev], board[curr])
-            prev = curr
+        for c in target:
+            val = ord(c) - 97
+            nx, ny = val // 5, val % 5
+            res += (max(0, oy - ny) * 'L')
+            res += (max(0, nx - ox) * 'D')
+            res += (max(0, ox - nx) * 'U')
+            res += (max(0, ny - oy) * 'R') + "!"
+            ox, oy = nx, ny
         return res
                 
