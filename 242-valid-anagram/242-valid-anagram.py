7be4e785a963c10fda49531fa84ce8050c920f0e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-28 19:32:41 +0530

    Time: 82 ms (42.88%), Space: 14.4 MB (96.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,6 +3,6 @@ class Solution:
         if(len(s) != len(t)):
             return False
         letters = [0] * 26
-        for c in s: letters[ord(c) - ord('a')] += 1
-        for c in t: letters[ord(c) - ord('a')] -= 1
+        for c in s: letters[ord(c) - 97] += 1
+        for c in t: letters[ord(c) - 97] -= 1
         return not any(letters)
