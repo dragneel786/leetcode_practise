db7c3c8625958c1e672be8fa33167515b2a8d641
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-29 21:28:55 +0530

    Time: 24 ms (99.83%), Space: 13.8 MB (76.64%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -2,9 +2,7 @@ class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
         def matcher(word):
             p = {}
-            for w, x in zip(word, pattern):
-                if(p.setdefault(w, x) != x):
-                    return False
-            return len(set(p.values())) == len(p.values())
-        
-        return filter(matcher, words)
+            return [p.setdefault(w, len(p)) for w in word]
+                
+        pat = matcher(pattern)
+        return filter(lambda w: matcher(w) == pat, words)
