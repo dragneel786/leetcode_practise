291b079cdc095df283cac9fc745645d8993a1358
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-30 13:40:51 +0530

    Time: 2200 ms (7.34%), Space: 17.6 MB (99.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -5,11 +5,9 @@ class Solution:
             W = Counter(w)
             return all(W[k] >= v for k,v in combine.items())
         
-        
         combine = Counter()
         for w in words2:
-            temp_counter = Counter(w)
-            for k in temp_counter:
-                combine[k] = max(combine[k], temp_counter[k])
+            for k, v in Counter(w).items():
+                combine[k] = max(combine[k], v)
             
         return filter(lambda w:checkUniversal(w, combine), words1)
