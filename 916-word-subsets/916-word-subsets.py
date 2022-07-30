749206abaea72cb217eaef4252b908a62c61397f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-30 14:11:23 +0530

    Time: 1575 ms (32.00%), Space: 18.4 MB (87.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,13 +1,7 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        
-        def checkUniversal(w, combine):
-            W = Counter(w)
-            return all(W[k] >= v for k,v in combine.items())
-        
         combine = Counter()
         for w in words2:
-            for k, v in Counter(w).items():
-                combine[k] = max(combine[k], v)
-            
-        return filter(lambda w:checkUniversal(w, combine), words1)
+            combine |= Counter(w)
+        return [w for w in words1 if not combine - Counter(w)]
+    
