f823102aa3fa2d8460fa40bd32ebecb42d3e368a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-07-31 21:04:22 +0530

    Time: 6237 ms (5.03%), Space: 49.5 MB (8.87%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -1,64 +1,58 @@
 class Node:
-    def __init__(self, val = 0, start = -1, end = -5, left = None, right = None):
+    def __init__(self, val = 0, start = -1, end = -1, left = None, right = None):
         self.val = val
         self.start = start
         self.end = end
         self.left = left
         self.right = right
-        
+    
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        
-        def buildTree(nums, start, end):
-            if(start > end):
-                return None
+        def buildTree(low, high):
+            if(low == high):
+                return Node(nums[low], low, high)
             
-            if(start == end):
-                return Node(nums[start], start, end)
-            
-            newNode = Node()
-            mid = (end - start) // 2 + start
-            newNode.left = buildTree(nums, start, mid)
-            newNode.right = buildTree(nums, mid + 1, end)
+            mid = ((high - low) // 2) + low
+            newNode = Node(0, low, high)
+            newNode.left = buildTree(low, mid)
+            newNode.right = buildTree(mid + 1, high)
             newNode.val = newNode.left.val + newNode.right.val
-            newNode.start, newNode.end = start, end
             return newNode
         
-        self.root = buildTree(nums, 0, len(nums) - 1)
-            
-    def update(self, index: int, val: int) -> None:
-        def updateVal(root):
-            if(root.start == root.end == index):
-                root.val = val
-                return
-            
-            mid = (root.end - root.start) // 2 + root.start
-            if(index <= mid):
-                updateVal(root.left)
-            else:
-                updateVal(root.right)
-            root.val = root.left.val + root.right.val
-        
-        updateVal(self.root)
+        self.root = buildTree(0, len(nums) - 1)
 
+    def update(self, index: int, val: int) -> None:
+        def updateValue(root, index, val):
+            if(index == root.start == root.end):
+                root.val = val
+                return 
+            
+            mid = (root.start + root.end) // 2
+            if(index <= mid):
+                updateValue(root.left, index, val)
+            else:
+                updateValue(root.right, index, val)
+            root.val = root.left.val + root.right.val
+                
+        updateValue(self.root, index, val)
+        
     def sumRange(self, left: int, right: int) -> int:
-        def getSumRange(root, left, right):
+        def sumRangeValue(root, left, right):
+            if(not root):
+                return 0
+            
             if(root.start == left and root.end == right):
                 return root.val
             
-            mid = (root.end - root.start) // 2 + root.start
-            if(right <= mid):
-                return getSumRange(root.left, left, right)
-            elif(left > mid):
-                return getSumRange(root.right, left, right)
-            else:
-                return getSumRange(root.left, left, mid) + \
-            getSumRange(root.right, mid + 1, right)
-        
-        return getSumRange(self.root, left, right)
+            mid = (root.start + root.end) // 2
+            ret = 0
+            if(left <= mid):
+                ret += sumRangeValue(root.left, left, min(mid, right))
+            
+            if(right > mid):
+                ret += sumRangeValue(root.right, max(mid + 1, left), right)
+            
+            return ret
     
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
+        return sumRangeValue(self.root, left, right)
