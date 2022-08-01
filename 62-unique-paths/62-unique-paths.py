cb5431deb9b651fc9ffa8961a4f32d8e9f4ed7f9
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-01 20:48:58 +0530

    Time: 55 ms (35.36%), Space: 14.3 MB (6.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,12 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        @lru_cache(None)
-        def paths(i, j):
+        def paths(i, j, memo={}):
+            key = (i, j)
+            if(key in memo): return memo[key]
             if(i == m - 1 and j == n - 1): return 1
             if(i >= m or j >= n): return 0
 
-            return paths(i + 1, j) + paths(i, j + 1)
+            memo[key] = paths(i + 1, j) + paths(i, j + 1)
+            return memo[key]
 
         
         return paths(0, 0)
