49899e49bf77ee25221d5d30bb2f47ffa25cb998
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-03 23:07:42 +0530

    Time: 65 ms (41.32%), Space: 13.9 MB (67.94%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.py
+++ 397-integer-replacement/397-integer-replacement.py
@@ -1,13 +1,14 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
         
-        @lru_cache(None)
-        def getSteps(num):
-            if(num == 2): return 1
-            if(num == 1): return 0
+        def getSteps(num, memo = {1: 0, 2: 1}):
+            if(num in memo): return memo[num]
             
-            return 1 + (min(getSteps(num - 1), getSteps(num + 1))\
-        if(num & 1) else getSteps(num // 2))
+            val = min(getSteps(num - 1), getSteps(num + 1)) if(num & 1)\
+            else getSteps(num // 2)
+            
+            memo[num] = val + 1
+            return val + 1
         
         return getSteps(n)
     
