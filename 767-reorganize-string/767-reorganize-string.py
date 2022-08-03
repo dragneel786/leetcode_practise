9db566435ac493f5d8f1837febe6e251e6f44d94
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-03 13:16:51 +0530

    Time: 59 ms (36.20%), Space: 13.8 MB (77.94%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,30 +1,8 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        C = Counter(s)
-        
-        heap = [(-C[key], key) for key in C.keys()] #a:2, b:2, c:4
-        heapify(heap)
-        
-        res = ""
-        while(heap):
-            count1, ch1 = heappop(heap)
-            count1 *= -1
-            res += ch1
-            
-            if(not heap):
-                if(count1 > 1):
-                    return ""
-            else:
-                count2, ch2 = heappop(heap)
-                count2 *= -1
-                res += ch2
-                
-                if(count1 > 1):
-                    heappush(heap, (-(count1 - 1), ch1))
-                if(count2 > 1):
-                    heappush(heap, (-(count2 - 1), ch2))
-        
-        return res
-                
+        a = sorted(sorted(s), key=s.count)
+        h = len(a) // 2
+        a[1::2], a[::2] = a[:h], a[h:]
+        return ''.join(a) * (a[-1:] != a[-2:-1])
             
             
