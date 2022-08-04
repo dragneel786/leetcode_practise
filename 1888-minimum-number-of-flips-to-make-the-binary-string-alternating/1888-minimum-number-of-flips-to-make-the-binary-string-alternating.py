8f8a4a43178cc24d3d003e843243f8788ad2ae4c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-04 12:44:17 +0530

    Time: 2400 ms (6.59%), Space: 15 MB (72.09%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -1,34 +1,23 @@
 class Solution:
     def minFlips(self, s: str) -> int:
         switch = lambda v: '0' if(v == '1') else '1'
-            
+        decrement = lambda pos, st, pre: (pre != st) if(pos >= n) else 0
+
         n = len(s)
         s += s
         flip1 = flip2 = 0
-        start = '0'
-        
-        for i in range(n):
-            flip1 += (start != s[i])
-            flip2 += (switch(start) != s[i])
+        start = prev = '0'
+        min_flip = inf
+        for i in range(2 * n):
+            min_flip = min(flip1, flip2, min_flip) if(i >= n) else inf
+
+            flip1 += (start != s[i]) - decrement(i, s[i - n], prev)
+            flip2 += (switch(start) != s[i]) - decrement(i, s[i - n], switch(prev))
+
+            prev = switch(prev) if(i >= n) else prev
             start = switch(start)
-        
-        prev = '0'
-        temp_flip1 = flip1
-        temp_flip2 = flip2
-        for i in range(n - 1):
-            temp_flip1 -= (prev != s[i])
-            temp_flip2 -= (switch(prev) != s[i])
-            
-            temp_flip1 += (start != s[n + i])
-            temp_flip2 += (switch(start) != s[n + i])
-            
-            flip1 = min(temp_flip1, flip1)
-            flip2 = min(temp_flip2, flip2)
-            
-            prev = switch(prev)
-            start = switch(start)
-        
-        return min(flip1, flip2)
+
+        return min_flip
             
             
         
