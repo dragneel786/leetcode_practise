11bcf8fb8c51c015ec049b8db463406f19fedc4c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-05 21:13:46 +0530

    Time: 57 ms (64.59%), Space: 14.5 MB (5.17%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,13 +1,14 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
-        nums.sort()
-        dp[0] = 1
-        for i in range(target + 1):
+        
+        @lru_cache(None)
+        def totalCombos(val):
+            if(val <= 0):
+                return val == 0
+            
+            ret = 0
             for n in nums:
-                idx = i - n
-                if(idx < 0):
-                    break
-                dp[i] += dp[idx]
-
-        return dp[target]
+                ret += totalCombos(val - n)
+            return ret
+            
+        return totalCombos(target)
