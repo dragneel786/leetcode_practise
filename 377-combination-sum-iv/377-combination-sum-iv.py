f7338d44de3b9e97da5bfad9fa1fde360418213d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-05 21:14:45 +0530

    Time: 62 ms (54.53%), Space: 14.1 MB (18.29%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,14 +1,16 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
-        @lru_cache(None)
-        def totalCombos(val):
+        def totalCombos(val, memo = {}):
             if(val <= 0):
                 return val == 0
+            
+            if(val in memo): return memo[val]
             
             ret = 0
             for n in nums:
                 ret += totalCombos(val - n)
+            memo[val] = ret
             return ret
             
         return totalCombos(target)
