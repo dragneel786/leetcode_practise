b855cd87da43aeaba9b4d92cdb6ed55ea1194cb6
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-06 00:09:27 +0530

    Time: 50 ms (55.84%), Space: 13.9 MB (66.26%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,13 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counter = Counter(s)
-        is_one = False
+        center = False
         length = 0
-        for v in counter.values():
-            if(v & 1):
-                is_one = True
-            
+        for v in Counter(s).values():
+            center = center or (v & 1) == 1
             length += v // 2
-        
-        return (length * 2) + is_one
+        return (length * 2) + center
                 
