cbedd2a5c33765a7929aeea30a6cf29a28fc55f2
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-07 11:57:29 +0530

    Time: 856 ms (39.77%), Space: 13.9 MB (93.55%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,25 +1,15 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        
-        paths = {'$':['a', 'e', 'i', 'o', 'u'], 'a':['e'], 'e':['a', 'i'],\
-                 'i':['a', 'e', 'o', 'u'], 'o':['u', 'i'], 'u':['a']}
-        
         M = (10 ** 9) + 7
-        
-        def getStringCounts(l, op = '$', memo = {}):
-            if(not l): return 1
-            
-            key = (l, op[-1])
-            if(key in memo):
-                return memo[key]
-            
-            counts = 0
-            for c in paths[op[-1]]:
-                counts = (counts + getStringCounts(l - 1, op + c)) % M
-            
-            memo[key] = counts
-            return counts
-        
-        return getStringCounts(n)
+        # a:0, e:1, i:2, o:3, u:4 
+        paths = {0:[1], 1:[0, 2], 2:[0, 1, 3, 4], 3:[4, 2], 4:[0]}
+    
+        dp = [1] * 5
+        for _ in range(n - 1):
+            temp = [0] * 5
+            for i in range(5):
+                temp[i] = sum(dp[v] for v in paths[i]) % M
+            dp = temp
+        return sum(dp) % M
         
             
