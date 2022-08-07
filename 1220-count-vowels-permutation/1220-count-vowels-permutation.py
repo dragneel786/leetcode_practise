114f3ef8199563582767df02ac44b4d1d80bc26a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-07 11:59:53 +0530

    Time: 890 ms (38.85%), Space: 13.9 MB (93.55%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,12 +1,11 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
         M = (10 ** 9) + 7
-        # a:0, e:1, i:2, o:3, u:4 
         paths = {0:[1], 1:[0, 2], 2:[0, 1, 3, 4], 3:[4, 2], 4:[0]}
-    
+        
         dp = [1] * 5
         for _ in range(n - 1):
-            dp = [sum(dp[v] for v in paths[i]) % M for i in range(5)] 
+            dp = [sum([dp[v] for v in paths[i]]) % M for i in range(5)] 
         return sum(dp) % M
         
             
