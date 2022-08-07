d3760291dfd2bfcebd5ca757f2a98c9baf867d62
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-07 18:31:43 +0530

    Time: 135 ms (61.63%), Space: 14.1 MB (62.23%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,8 +1,5 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
         C = Counter(nums)
-        for v in C.values():
-            if(v & 1):
-                return False
-        return True
+        return all(v % 2 == 0 for v in C.values())
         
