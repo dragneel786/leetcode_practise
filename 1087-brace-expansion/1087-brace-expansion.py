de7d6d4de6cb1358539df05d6621edabe6ae4814
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-08 21:03:58 +0530

    Time: 52 ms (67.67%), Space: 14.3 MB (52.19%) - LeetHub

---
+++ 1087-brace-expansion/1087-brace-expansion.py
@@ -0,0 +1,28 @@
+class Solution:
+    def expand(self, s: str) -> List[str]:
+        
+        def get_options(s, i):
+            j = i + 1
+            while(s[j] != '}'): j += 1
+            return (s[i + 1: j]).split(","), j
+        
+        def allCombos(start, op = ""):
+            nonlocal n, res
+            if(start == n): 
+                res.append(op)
+                return
+            
+            if(s[start] == '{'):
+                options, idx = get_options(s, start)
+                for option in sorted(options):
+                    allCombos(idx + 1, op + option)
+            else:
+                allCombos(start + 1, op + s[start])
+        
+        n = len(s)
+        res = []
+        allCombos(0)
+        return res
+        
+        
+            
