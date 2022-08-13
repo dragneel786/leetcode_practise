5301086735d2c74b4782a9fe52cdafa85077e2ee
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-13 14:11:01 +0530

    Time: 776 ms (46.28%), Space: 14.1 MB (77.75%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -1,23 +1,30 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         
+        def check_present(start):
+            remaining = words_hash.copy()
+            substr = s[start: start + window]
+            words_used = 0
+            
+            while(remaining[substr] > 0):
+                remaining[substr] -= 1
+                start += window
+                substr = s[start: start + window]
+                words_used += 1
+            
+            return words_used == words_len
+        
+        
         words_hash = Counter(words)
         window = len(words[0])
         words_len = len(words)
+        substring_size = window * words_len
         res = []
-        for i in range(len(s)):
-            temp = Counter()
-            temp_len = words_len
-            start = i
-            while(s[start: start + window] in  words_hash\
-                 and temp_len):
-                temp[s[start: start + window]] += 1
-                start += window
-                temp_len -= 1
-            
-            if(words_hash == temp):
-                res.append(i)
         
+        for i in range(len(s) - substring_size + 1):
+            if(check_present(i)):
+                res.append(i)
+            
         return res
                 
         
