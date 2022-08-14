2070bd80888a58736b91dfe43f36facc8258970d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-14 11:57:34 +0530

    Time: 382 ms (20.41%), Space: 26 MB (61.06%) - LeetHub

--- 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
+++ 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
@@ -1,39 +1,24 @@
 class Solution:
     def findStrobogrammatic(self, n: int) -> List[str]:
-        combos = {1: ['0', '1', '8']}
-        combos[2] = ['11', '69', '88', '96']
-        combos[4] = ['00', '11', '69', '88', '96']
+        combos = {'0':'0', '1':'1', '8':'8', '6':'9', '9': '6'}
+        final_length = n
         
-        def split_add(p1, p2):
-            if(not p2 or not p1):
-                return p1 if(p1) else p2
+        def create_storo(n):
+            nonlocal final_length
+            if(not n):
+                return ['']
             
-            temp = []
-            for o in p1:
-                mid = len(o) // 2
-                h1, h2 = o[:mid], o[mid:]
-                for c in p2:
-                    temp.append(h1 + c + h2)
+            if(n == 1):
+                return ['0', '1', '8']
             
-            return temp
-        
-        def create_number(n, op = []):
-            nonlocal res
-            if(n <= 1):
-                op = split_add(op, combos[1]) if(n == 1) else op
-                res = op[:]
-                return 
+            prev_combo = create_storo(n - 2)
+            curr_combo = []
             
-            if(n == 2 or n == 3):
-                op = split_add(combos[2], op)
-                create_number(n - 2, op)
-                return
-                
-            op = split_add(combos[4], op)
-            create_number(n - 2, op)
-                
+            for p in prev_combo:
+                for i, j in combos.items():
+                    if(i != '0' or n != final_length):
+                        curr_combo.append(i + p + j)
+            
+            return curr_combo
         
-        res = []
-        create_number(n)
-        return res
-        
+        return create_storo(n)
