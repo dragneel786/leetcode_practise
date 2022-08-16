3e4732a227fe72db40d77de97f4acb373ee46401
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-16 21:55:15 +0530

    Time: 497 ms (28.19%), Space: 23.2 MB (7.87%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -1,25 +1,25 @@
-class Node:
-    def __init__(self, num = ""):
-        self.num = num
-        self.size = len(num)
+# class Node:
+#     def __init__(self, num = ""):
+#         self.num = num
+#         self.size = len(num)
         
-    def __lt__(self, other):
-        return self.size < other.size or \
-    (self.size == other.size and self.compare_two(self.num, other.num))
+#     def __lt__(self, other):
+#         return self.size < other.size or \
+#     (self.size == other.size and self.compare_two(self.num, other.num))
     
-    def compare_two(self, num1, num2):
-        for s1, s2 in zip(num1, num2):
-            v1, v2 = int(s1), int(s2)
-            if(v1 == v2): continue
+#     def compare_two(self, num1, num2):
+#         for s1, s2 in zip(num1, num2):
+#             v1, v2 = int(s1), int(s2)
+#             if(v1 == v2): continue
                 
-            if(v1 < v2):
-                return True
-            else:
-                return False
+#             if(v1 < v2):
+#                 return True
+#             else:
+#                 return False
             
-        return False
+#         return False
     
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        return nlargest(k, [Node(num) for num in nums])[-1].num
+        return str(nlargest(k, [int(num) for num in nums])[-1])
         
