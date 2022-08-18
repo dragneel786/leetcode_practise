4274f893daa8346887840b3ecf9e43ecaf71f2f9
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-19 00:24:01 +0530

    Time: 2190 ms (41.03%), Space: 14.1 MB (86.58%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -5,16 +5,18 @@ class Solution:
         if(total & 1): return False
 
         half = total // 2
-        dp = [([False] * (half + 1)) for _ in range(n + 1)]
-
-        for x in range(n + 1): dp[x][0] = True
-
+        dp = [True] + ([False] * half)
+        
         for i in range(1, n + 1):
+            temp = [True] + ([False] * half)
+            
             for j in range(1, half + 1):
-                dp[i][j] = dp[i - 1][j]
-                if(j >= nums[i - 1] and not dp[i][j]):
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]]
-                
-        return dp[n][half]
+                temp[j] = dp[j]
+                if(j >= nums[i - 1] and not temp[j]):
+                    temp[j] = dp[j - nums[i - 1]]
+            
+            dp = temp[:]
+            
+        return dp[half]
         
         
