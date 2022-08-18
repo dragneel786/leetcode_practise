4411fcea3b7d0e5856c211bf59b85f6cbf3e1388
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-18 22:36:22 +0530

    Time: 1748 ms (49.13%), Space: 148.6 MB (8.36%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,23 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        def partitionIt(index, val = 0):
-            nonlocal total
-            if(len(nums) == index):
-                return val == (total - val)
+        def partitionIt(index, half):
+            if(half <= 0 or index >= len(nums)):
+                return half == 0
             
-            key = (index, val)
+            key = (index, half)
             
             if(key in memo):
                 return memo[key]
             
-            memo[key] = partitionIt(index + 1, val + nums[index]) or \
-        partitionIt(index + 1, val)
+            memo[key] = partitionIt(index + 1, half - nums[index]) or \
+        partitionIt(index + 1, half)
             
             return memo[key]
         
         total = sum(nums)
         if(total & 1): return False
         
+        half = total // 2
         memo = {}
-        return partitionIt(0)
+        return partitionIt(0, half)
