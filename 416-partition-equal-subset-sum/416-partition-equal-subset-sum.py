3e1b45799c9ae6d5d7518e0b5ff0f1692037753e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-19 00:16:43 +0530

    Time: 4506 ms (16.53%), Space: 29.5 MB (36.28%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,23 +1,20 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        
-        def partitionIt(index, half):
-            if(half <= 0 or index >= len(nums)):
-                return half == 0
-            
-            key = (index, half)
-            
-            if(key in memo):
-                return memo[key]
-            
-            memo[key] = partitionIt(index + 1, half - nums[index]) or \
-        partitionIt(index + 1, half)
-            
-            return memo[key]
-        
         total = sum(nums)
+        n = len(nums)
         if(total & 1): return False
-        
+
         half = total // 2
-        memo = {}
-        return partitionIt(0, half)
+        dp = [([False] * (half + 1)) for _ in range(n + 1)]
+
+        for x in range(n + 1): dp[x][0] = True
+
+        for i in range(1, n + 1):
+            for j in range(1, half + 1):
+                dp[i][j] = dp[i - 1][j]
+                if(j >= nums[i - 1] and not dp[i][j]):
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]]
+                
+        return dp[n][half]
+        
+        
