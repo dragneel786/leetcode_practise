56c6a97877d3ba38406444d6b96d6e0a19e947b5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-19 23:37:17 +0530

    Time: 74 ms (17.86%), Space: 13.9 MB (16.48%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,15 +1,13 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         
-        check_validity = lambda a, b: ((a.islower() and b.isupper()) or\
-        (a.isupper() and b.islower())) and (a.lower() == b.lower())
+        check_validity = lambda a, b: abs(ord(a) - ord(b)) == 32
         
         st = deque([s[0]])
+        
         for c in s[1:]:
-            
             if(st and check_validity(st[-1], c)):
-                st.pop()
-                
+                st.pop()   
             else:
                 st.append(c)
         
