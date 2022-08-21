79ec445fa164b5ad6cc017bc84d83197c5d30574
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-21 18:04:05 +0530

    Time: 281 ms (27.58%), Space: 14.2 MB (74.58%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.py
@@ -1,16 +1,27 @@
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        refueling_options = []
-        curr_station = refueling_stops = 0
-        reachable_distance = startFuel
-        while reachable_distance < target:
-            while curr_station < len(stations) and stations[curr_station][0] <= reachable_distance:
-                heapq.heappush(refueling_options, -stations[curr_station][1])
-                curr_station += 1
-            if not refueling_options: return -1
-            reachable_distance += -heapq.heappop(refueling_options)
-            refueling_stops += 1
-        return refueling_stops
+        heap = []
+        stations.sort()
+        reached = startFuel
+        stops = 0
+        index = 0
+        
+        while(reached < target):
+            
+            while(index < len(stations) and\
+                  stations[index][0] <= reached):
+                heappush(heap, -stations[index][1])
+                index += 1
+            
+            if(not heap): return -1
+            
+            reached -= heappop(heap)
+            stops += 1
+        
+        return stops
+                
+        
+        
             
                 
                 
