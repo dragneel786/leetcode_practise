da32903f19f00957e90dbcf98f9361e0d966a799
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-23 09:54:33 +0530

    Time: 1279 ms (84.41%), Space: 32.5 MB (53.76%) - LeetHub

--- 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.py
+++ 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.py
@@ -1,21 +1,14 @@
 class Solution:
     def countPairs(self, nums1: List[int], nums2: List[int]) -> int:
         
-        def create_pairs():
-            nonlocal nums1, nums2
-            for i in range(len(nums1)):
-                nums1[i], nums2[i] = (nums1[i] - nums2[i]),\
-                (nums2[i] - nums1[i])
+        nums = [x - y for x, y in zip(nums1, nums2)]
+        nums.sort()
         
-        
-        create_pairs()
-        sorted_values = [nums1[0]]
-        
+        n = len(nums)
         counts = 0
-        for j in range(1, len(nums2)):
-            idx = bisect_right(sorted_values, nums2[j])
-            counts += (len(sorted_values) - idx)
-            insort(sorted_values, nums1[j])
+        for i, x in enumerate(nums):
+            idx = bisect_right(nums, -x)
+            counts += n - max(i + 1, idx)
         
         return counts
         
