52ccff66f298c524d7fdb1bdd79b7528373716dd
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-24 22:53:24 +0530

    Time: 160 ms (37.37%), Space: 13.9 MB (27.32%) - LeetHub

--- 788-rotated-digits/788-rotated-digits.py
+++ 788-rotated-digits/788-rotated-digits.py
@@ -1,12 +1,18 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
         
-        def is_valid(val):
-            s1 = set([1, 8, 0])
-            s2 = set([1, 8, 0, 2, 5, 6, 9])
-            s = set([int(c) for c in str(val)])
-            return not s.issubset(s1) and s.issubset(s2)
+        s1 = set([2, 5, 6, 9])
+        s2 = set([1, 8, 0])
         
+        def is_valid(val):
+            valid = False
+            for c in str(val):
+                v = int(c)
+                if(v not in s1 and v not in s2):
+                    return False
+                valid = valid or v in s1
+                
+            return valid
         
         counts = 0
         for num in range(n + 1):
