183cb1313aecc4e1b9b3505a00a664d6582ff944
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-25 20:16:44 +0530

    Time: 1242 ms (30.00%), Space: 25 MB (52.38%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.py
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.py
@@ -1,39 +1,22 @@
 class Solution:
-    def maximumANDSum(self, nums: List[int], numSlots: int) -> int:
+    def maximumANDSum(self, nums: List[int], ns: int) -> int:
         
-        def validate(slots, pos):
-            pos *= 2
-            d1, d2 = (1 << (pos - 2)), (1 << (pos - 1))
-            if(slots & d1): return d1
-            if(slots & d2): return d2
-            return -1
-        
-        
-        def max_and_sum(index, slots):
-            nonlocal numSlots
+        @lru_cache(None)
+        def get_max(index, slots):
             if(index == len(nums)):
                 return 0
             
-            key = (index, slots)
-            if(key in memo):
-                return memo[key]
-
-            max_sum = 0
-            for i in range(1, numSlots + 1):
-                value = validate(slots, i)
-                if(value == -1): continue
-                    
-                asum = nums[index] & i
-                max_sum = max(max_sum, asum +
-                              max_and_sum(index + 1,
-                                          slots ^ value))
-                
-            memo[key] = max_sum
-            return max_sum
+            max_val = 0
+            for i in range(1, ns + 1):
+                base = 3 ** (i - 1)
+                left = (slots // base) % 3
+                if(left > 0):
+                    max_val = max(max_val, (nums[index] & i) +\
+                                  get_max(index + 1, slots - base))
+            
+            return max_val
         
-        memo = {}
-        slots = (1 << (numSlots * 2)) - 1
-        return max_and_sum(0, slots)
+        return get_max(0, 3 ** ns - 1)
         
         
         
