965c830cc568a8a67cdceebb1657776eb55733ee
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-25 12:39:20 +0530

    Time: 806 ms (79.52%), Space: 24.8 MB (75.24%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.py
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.py
@@ -1,26 +1,32 @@
 class Solution:
     def maximumANDSum(self, nums: List[int], numSlots: int) -> int:
         
+        def validate(slots, pos):
+            pos *= 2
+            d1, d2 = (1 << (pos - 2)), (1 << (pos - 1))
+            if(slots & d1): return d1
+            if(slots & d2): return d2
+            return -1
+        
         @lru_cache(None)
         def max_and_sum(index, slots):
             nonlocal numSlots
             if(index == len(nums)):
                 return 0
 
-            slots = list(slots)
             max_sum = 0
-            for i in range(numSlots):
-                if(slots[i] < 2):
-                    slots[i] += 1
-
-                    max_sum = max(max_sum, (nums[index] & (i + 1))
-                                  + max_and_sum(index + 1, tuple(slots)))
-
-                    slots[i] -= 1
-
+            for i in range(1, numSlots + 1):
+                value = validate(slots, i)
+                if(value == -1): continue
+                    
+                asum = nums[index] & i
+                max_sum = max(max_sum, asum +
+                              max_and_sum(index + 1,
+                                          slots ^ value))
+                
             return max_sum
 
-        slots = tuple([0 for i in range(numSlots)])
+        slots = (1 << (numSlots * 2)) - 1
         return max_and_sum(0, slots)
         
         
