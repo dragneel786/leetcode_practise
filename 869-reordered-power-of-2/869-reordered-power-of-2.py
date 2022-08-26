dbef18336e3b42227bfe78fffc8f49617c7685ce
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-26 19:15:06 +0530

    Time: 3968 ms (17.11%), Space: 14 MB (26.74%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,15 +1,7 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        
-        def is_pow(num):
-            bit_pos = 1 << int(log(num) / log(2))
-            return bit_pos == num
-            
-        
-        n = list(str(n))
-        for combo in permutations(n):
-            if(combo[0] == '0'): continue
-            if(is_pow(int(''.join(combo)))):
+        for perm in permutations(str(n)):
+            if(perm[0] != '0' and bin(int(''.join(perm))).count('1') == 1):
                 return True
             
             
