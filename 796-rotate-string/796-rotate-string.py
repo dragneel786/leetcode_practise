e9eb5053a7ba0d048a3f5325373e4dce57b4c047
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-27 13:08:26 +0530

    Time: 52 ms (37.28%), Space: 13.7 MB (97.36%) - LeetHub

--- 796-rotate-string/796-rotate-string.py
+++ 796-rotate-string/796-rotate-string.py
@@ -1,17 +1,11 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         
-        def compare(pos):
-            for c1, c2 in zip(s, goal[i:] + goal[:i]):
-                if(c1 != c2):
-                    return False
-            return True
-        
         m, n = len(s), len(goal)
         if(m != n):
             return False
         
         for i, c in enumerate(goal):
-            if(c == s[0] and compare(i)):
+            if(c == s[0] and s == goal[i:] + goal[:i]):
                 return True
         return False
