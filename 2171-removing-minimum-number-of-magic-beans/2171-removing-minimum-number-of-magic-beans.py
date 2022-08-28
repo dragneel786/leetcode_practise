8487eee459cee40428399ba4c4ce5b8d34042499
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-28 21:04:49 +0530

    Time: 1599 ms (80.62%), Space: 28 MB (58.71%) - LeetHub

---
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumRemoval(self, beans: List[int]) -> int:
+        # 1, 4, 5, 6
+        # 2, 2, 3, 10
+        beans.sort()
+        prefix_sum = [0]
+        n = len(beans)
+        min_value = lambda idx, bean: prefix_sum[idx] +\
+        (prefix_sum[-1] - prefix_sum[idx] - (bean * (n - idx)))
+        
+        for b in beans:
+            prefix_sum.append(prefix_sum[-1] + b)
+        
+        min_cost = inf
+        for i, b in enumerate(beans):
+            min_cost = min(min_cost, min_value(i, b))
+        
+        return min_cost
+        
+        
+        
+            
+        
+        
