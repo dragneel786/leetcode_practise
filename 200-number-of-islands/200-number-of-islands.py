5b4147fd9bbb26cf11e209ad1f776fac7972ee93
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-29 20:58:45 +0530

    Time: 781 ms (5.83%), Space: 16.6 MB (50.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,25 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
-        rows = len(grid)
-        cols = len(grid[0])
         
-        def bfs(x, y):
-            st = deque()
-            st.append((x, y))
-            while(len(st)):
-                for _ in range(len(st)):
-                    node = st.pop()
-                    for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-                        xi, yi = node[0] + di, node[1] + dj
-                        if(0 <= xi < rows and 0 <= yi < cols and grid[xi][yi] != "0"):
-                            grid[xi][yi] = "0"
-                            st.append((xi, yi))
+        def dfs(x, y):
+            if(x < 0 or x >= rows or y < 0\
+               or y >= cols or grid[x][y] == '0'):
+                return
             
+            grid[x][y] = '0'
+            for dx, dy in [(1, 0), (0, 1),\
+                           (-1, 0), (0, -1)]:
+                dfs(x + dx, y + dy)
+        
+        
+        counts = 0
+        rows, cols = len(grid), len(grid[0])
         for r in range(rows):
             for c in range(cols):
-                if(grid[r][c] == "1"):
-                    islands += 1
-                    grid[r][c] = "0"
-                    bfs(r, c)
-        return islands
+                if(grid[r][c] == '1'):
+                    counts += 1
+                    dfs(r, c)
+                    
+        return counts
