37b520742fe2ffcd3a5e0fd8af2075bdb003d8b3
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-08-30 20:42:41 +0530

    Time: 70 ms (16.71%), Space: 13.8 MB (74.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,10 +3,6 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        """
-        00, 03, 33, 30 | 01, 13, 32, 20 | 02, 23, 31, 10
-        11, 12, 22, 21
-        """
         n = len(matrix)
         for i in range(n // 2):
             for j in range(i, n - i - 1):
