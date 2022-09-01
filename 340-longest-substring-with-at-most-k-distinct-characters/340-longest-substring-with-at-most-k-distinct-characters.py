1d569b3cf8b8845617803768dd097ec98ed025fb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-01 21:42:09 +0530

    Time: 244 ms (5.77%), Space: 14.1 MB (73.87%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -3,18 +3,6 @@ class Solution:
         if(k == 0): 
             return 0
         
-        def update(uniq, start):
-            for idx, ch in enumerate(s[start: i]):
-                uniq[ch] -= 1
-                if(not uniq[ch]):
-                    del uniq[ch]
-                
-                if(len(uniq) == k):
-                    return idx + 1
-            
-            return idx + 1
-        
-        
         j = 0
         uniq = Counter()
         longest = 0
@@ -22,8 +10,11 @@ class Solution:
             uniq[c] += 1
             longest = max(longest, i - j)
             
-            if(len(uniq) > k):
-                j += update(uniq, j)
+            while(len(uniq) > k):
+                uniq[s[j]] -= 1
+                if(not uniq[s[j]]):
+                    del uniq[s[j]]
+                j += 1
         
         return longest
         
