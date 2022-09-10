49b37ea9933c2d8633f470eb4314eb54184d13e7
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-10 23:02:02 +0530

    Time: 2057 ms (5.04%), Space: 100 MB (5.01%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -10,13 +10,16 @@ class Solution:
             val2 = get_me_profit(state, index + 1, remain_tns)
             return max(val1, val2)
 
-        @lru_cache(None)
         def get_me_profit(state, index, remain_tns):
             # Base Case.
             if(not remain_tns or\
                 index >= len(prices)): return 0
 
             # Recursive function.
+            key = (state, index, remain_tns)
+            if(key in memo):
+                return memo[key]
+            
             max_val = 0
             if(state[0] == 'S' and state[1] < prices[index]):
                 max_val = max(max_val, calculate_sell(
@@ -26,6 +29,8 @@ class Solution:
                 max_val = max(max_val, calculate_buy(
                     state, index, remain_tns))
 
+            memo[key] = max_val
             return max_val
 
+        memo = {}
         return get_me_profit(('B', -1), 0, k)
