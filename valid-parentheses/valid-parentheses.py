d550de6cf9eb35574e438314f904645f8af5df87
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-13 23:38:01 +0530

    Time: 63 ms, Memory: 13.8 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = deque()
+        for c in s:
+            if(st and (c == ')' and st[-1] == '('\
+               or c == '}' and st[-1] == '{'\
+               or c == ']' and st[-1] == '[')): st.pop()
+            else:
+                st.append(c)
+        return len(st) == 0
+                
+                    
+                
