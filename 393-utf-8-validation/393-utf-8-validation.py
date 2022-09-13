fa58dd6015e4a351532abf03a51e9058b9c9574d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-13 23:26:47 +0530

    Time: 121 ms (90.20%), Space: 14.3 MB (35.67%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -1,51 +1,32 @@
 class Solution:
     def validUtf8(self, data: List[int]) -> bool:
-        def maskBit(num):
-            bits = 0
-            mask = 1 << 7
-            while(mask & num):
-                bits += 1
-                mask >>= 1
-            return bits
         
-        form = 0
-        for d in data:
-            if(not form):
-                b = maskBit(d)
-                if(b == 1 or b > 4):
+        def count_ones(num):
+            s = 128
+            counts = 0
+            while(s & num):
+                s >>= 1
+                counts += 1
+            return counts
+
+        ones = 0
+        for i in range(len(data)):
+            if(not ones):
+                ones = count_ones(data[i])
+                if(ones not in [0,2,3,4]):
                     return False
-                form = b - 1 if b > 0 else 0
+                if(ones): ones -= 1
+
+            elif(count_ones(data[i]) == 1):
+                ones -= 1
+
             else:
-                if(maskBit(d) != 1):
-                    return False
-                form -= 1
-        return form == 0
-                
-        
-#         binR = []
-#         for d in data:
-#             b = bin(d)[2:]
-#             binR.append((8 - len(b)) * '0' + bin(d)[2:])
-        
-#         h = {"0":0, "110":1, "1110":2, "11110":3}
-#         form = 0
-#         for b in binR:
-#             if(form == 0):
-#                 if(b[0] in h):
-#                     form = 0
-#                 elif(b[:5] in h):
-#                     form = h[b[:5]]
-#                 elif(b[:4] in h):
-#                     form = h[b[:4]]
-#                 elif(b[:3] in h):
-#                     form = h[b[:3]]
-#                 else:
-#                     return False
-#             else:
-#                 if(b[:2] != "10"):
-#                     return False
-#                 form -= 1
-#         return form == 0
+                return False
+
+        return ones == 0
+
             
             
+        
+        
             
