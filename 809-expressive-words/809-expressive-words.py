f9afbba1c150ee317b6ea70d665a9d5cd0d6adbd
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-22 21:05:36 +0530

    Time: 117 ms (12.64%), Space: 13.9 MB (71.98%) - LeetHub

--- 809-expressive-words/809-expressive-words.py
+++ 809-expressive-words/809-expressive-words.py
@@ -1,32 +1,40 @@
 class Solution:
     def expressiveWords(self, s: str, words: List[str]) -> int:
-        def encode(w):
-            w += "-"
-            res = []
+        
+        def char_counts(s):
+            s += " "
             count = 1
-            for i in range(1, len(w)):
-                if(w[i] != w[i - 1]):
-                    res.append((w[i - 1], count))
-                    count = 1
-                else:
-                    count += 1
-            return res
+            ret = []
+            for i in range(1, len(s)):
+                if(s[i - 1] != s[i]):
+                    ret.append([s[i - 1], count])
+                    count = 0
+                
+                count += 1
+            
+            return ret
         
-        def compare(w1, w2):
-            for i in range(len(w1)):
-                a, b = w1[i], w2[i]
-                if(a[0] == b[0] and (a[1] == b[1] or\
-                                     (a[1] > b[1] and a[1] > 2))):
+        count = 0
+        scount = char_counts(s)
+        for word in words:
+            wcount = char_counts(word)
+            if(len(scount) != len(wcount)):
+                continue
+            
+            for a, b in zip(scount, wcount):
+                if(a[0] == b[0] and \
+                   (a[1] == b[1] or\
+                   a[1] > b[1] and a[1] > 2)):
                     continue
-                return False
-            return True
+                break
+            else:
+                count += 1
         
-        s = encode(s)
-        ans = 0
-        for wo in words:
-            v = encode(wo)
-            if(len(v) == len(s) and compare(s, v)):
-                ans += 1
-        return ans
+        return count
+            
+                    
+                    
                 
-                
+        
+        
+        
