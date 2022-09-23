b8bbdf2a028aebe4b9ee223870a39f0992f9f0c1
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-23 19:42:10 +0530

    Time: 2809 ms (45.67%), Space: 13.8 MB (80.31%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -5,6 +5,6 @@ class Solution:
         size = 1
         for i in range(2, n + 1):
             size += (i & (i - 1) == 0)
-            res = ((res << size) + i) % MOD
+            res = ((res << size) | i) % MOD
         
         return res
