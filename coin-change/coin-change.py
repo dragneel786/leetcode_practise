5340e8947dc6b0634863454e8e80c2533628dd43
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-27 23:39:16 +0530

    Time: 3087 ms, Memory: 14.1 MB - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,21 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        @lru_cache(None)
-        def calculate(amount):
-            if(not amount):
-                return 0
-            
-            if(amount < -1):
-                return inf
-            
-            ret = inf
-            for c in coins:
-                if(c > amount): break
-                ret = min(ret, 1 + calculate(amount - c))
-        
-            return ret
-        
         coins.sort()
-        val = calculate(amount)
-        return -1 if(val == inf) else val
+        dp = [inf] * (amount + 1)
+        dp[0] = 0
+        
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if(a < coin): break
+                dp[a] = min(dp[a - coin] + 1, dp[a])
+        
+        return dp[amount] if(dp[amount] != inf) else -1
