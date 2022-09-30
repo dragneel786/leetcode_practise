296b0640af6107727d2c49d3153cea8c89b81cf2
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-09-30 23:07:07 +0530

    Time: 709 ms, Memory: 16.5 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,28 +1,22 @@
-from queue import Queue
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        count = 0
-
-        for row in range(rows):
-            for col in range(cols):
-                if(grid[row][col] == '1'):
-                    self.bfs(grid, row, col, rows, cols)
-                    count += 1
-
-        return count
-    
-    def bfs(self, grid, row, col, n1, n2):
-        q = Queue()
-        q.put((row, col))
-        visited = set()
-        visited.add((row, col))
-        grid[row][col] = '0'
-        while(not q.empty()):
-            r, c = q.get()
-            for i, j in ((r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)):
-                if(i > -1 and j > -1 and i < n1 and j < n2 and grid[i][j] == '1' and (i, j) not in visited):
-                    grid[i][j] = '0'
-                    visited.add((i, j))
-                    q.put((i, j))
+        
+        def dfs(x, y):
+            for dx, dy in [[1, 0], [-1, 0],\
+                           [0, 1], [0, -1]]:
+                nx, ny = x + dx, y + dy
+                if(0 <= nx < m and 0 <= ny < n and grid[nx][ny] == '1'):
+                    grid[nx][ny] = '0'
+                    dfs(nx, ny)
+        
+        m, n = len(grid), len(grid[0])
+        islands_count = 0
+        
+        for r in range(m):
+            for c in range(n):
+                if(grid[r][c] == '1'):
+                    dfs(r, c)
+                    islands_count += 1
+        
+        return islands_count
+                    
