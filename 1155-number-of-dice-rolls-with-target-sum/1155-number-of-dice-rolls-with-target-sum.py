bc423e901b3f560e43bdef3a6d732cff2558f916
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-02 20:04:12 +0530

    Time: 1866 ms (10.71%), Space: 19.3 MB (37.99%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        
+        def roll_it(n, s = 0):
+            nonlocal target, k
+            if(s > target or n < 0):
+                return 0
+            
+            if(s == target and n == 0):
+                return 1
+            
+            key = (n, s)
+            if(key in memo): return memo[key]
+            
+            count = 0
+            for i in range(1, k + 1):
+                count += roll_it(n - 1, s + i)
+                count %= MOD
+            
+            memo[key] = count
+            return memo[key]
+        
+        memo = {}
+        MOD = (10 ** 9) + 7
+        return roll_it(n)
+        
+                
