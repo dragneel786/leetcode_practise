5041eaf590716fe91f7d6e69dcc971aff13dc523
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-03 20:35:30 +0530

    Time: 920 ms (42.33%), Space: 15 MB (70.29%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,33 +1,28 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        dp = [([1] * cols) for _ in range(rows)]
-        visited = set()
-        dir = [[1,0], [0,1], [-1,0], [0,-1]]
+        def dfs(x, y):
+            if(memo[x][y] != 0):
+                return memo[x][y]
+            
+            for dx, dy in direction:
+                nx, ny = x + dx, y + dy
+                if(0 <= nx < m and 0 <= ny < n\
+                    and matrix[nx][ny] > matrix[x][y]):
+                    memo[x][y] = max(dfs(nx, ny), memo[x][y])
+            
+            memo[x][y] += 1
+            return memo[x][y]
 
-        def dfs(r, c, seen = set()):
-            seen.add((r,c))
-            maxV = 0
-            for di, dj in dir:
-                x = r + di
-                y = c + dj
-                if((x, y) in visited and matrix[r][c] < matrix[x][y]):
-                    maxV = max(maxV, dp[x][y])
-                elif(0 <= x < rows and 0 <= y < cols and\
-                     (x, y) not in seen and\
-                          matrix[r][c] < matrix[x][y]):
-                    dfs(x, y, seen)
-                    maxV = max(maxV, dp[x][y])
-
-            dp[r][c] += maxV
-            visited.add((r,c))
-
-        longest = 0
-        for r in range(rows):
-            for c in range(cols):
-                if((r, c) not in visited):
+        direction = [[0, 1], [1, 0],\
+                        [-1, 0], [0, -1]]
+        m, n = len(matrix), len(matrix[0])
+        memo = [([0] * n) for _ in range(m)]
+        max_len = -inf
+        for r in range(m):
+            for c in range(n):
+                if(not memo[r][c]):
                     dfs(r, c)
-                longest = max(longest, dp[r][c])
+                max_len = max(memo[r][c], max_len)
 
-        return longest
+        return max_len
+                
