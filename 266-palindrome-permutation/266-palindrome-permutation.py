fdae60cbeac220cb52416536e41be0b4e1941033
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-07 22:56:54 +0530

    Time: 36 ms (85.40%), Space: 13.8 MB (54.09%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.py
+++ 266-palindrome-permutation/266-palindrome-permutation.py
@@ -1,16 +1,19 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
         
-        odd = 0
-        counts = Counter(s)
-        for v in counts.values():
-            if(v & 1):
-                odd += 1
-            
-                if(odd > 1): 
-                    return False
+        char_map = [0] * 26
+        count = 0
         
-        return True
+        for c in s:
+            char_map[ord(c) - 97] += 1
+            
+            if(char_map[ord(c) - 97] & 1):
+                count += 1
+            else:
+                count -= 1
+        
+        return count <= 1
+            
         
         
     
