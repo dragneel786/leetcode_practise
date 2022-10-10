89b35082828848174994fb5e1823c138edee38f2
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-10 22:26:59 +0530

    Time: 2195 ms, Memory: 15.5 MB - LeetHub

---
+++ word-search-ii/word-search-ii.py
@@ -0,0 +1,52 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        def create_word_trie(words):
+            tries = dict()
+            for word in words:
+                temp = tries
+                for c in word:
+                    temp[c] = temp.get(c, dict())
+                    temp = temp[c]
+                temp['$'] = word
+                
+            return tries
+        
+        
+        def dfs(x, y, t):
+            letter = board[x][y]
+            node = t[letter]
+            
+            val = node.pop('$', False)
+            if(val):
+                ans.append(val)
+                
+            for dx, dy in [(1, 0), (0, 1),\
+                           (-1, 0), (0, -1)]:
+                nx, ny = x + dx, y + dy
+                
+                if(0 <= nx < m and 0 <= ny < n and \
+                   board[nx][ny] and board[nx][ny] in node):
+                    
+                    ch = board[x][y]
+                    board[x][y] = None
+                    
+                    dfs(nx, ny, node)
+                    
+                    board[x][y] = ch
+            
+            if(not node):
+                t.pop(letter)
+                    
+                    
+        trie = create_word_trie(words)
+        m, n = len(board), len(board[0])
+        ans = []
+        for r in range(m):
+            for c in range(n):
+                if(board[r][c] in trie):
+                    dfs(r, c, trie)
+        
+        return ans
+                    
+        
