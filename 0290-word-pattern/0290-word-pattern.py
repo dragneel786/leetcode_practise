fea37b812e672c32130becc139df4b134f1391bb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-13 23:31:18 +0530

    Time: 63 ms (15.48%), Space: 13.9 MB (74.29%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,13 +1,12 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        plist = list(pattern)
         slist = s.split()
-        if(len(plist) != len(slist)):
+        if(len(pattern) != len(slist)):
             return False
         
         pmap = dict()
         smap = dict()
-        for p, w in zip(plist, slist):
+        for p, w in zip(pattern, slist):
             p1 = pmap.setdefault(p, w)
             w1 = smap.setdefault(w, p)
             if(p1 != w or w1 != p):
