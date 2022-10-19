78c92851088843fadc3436482f0c124340ba5dfb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-19 23:08:30 +0530

    Time: 164 ms, Memory: 16.8 MB - LeetHub

--- candy/candy.py
+++ candy/candy.py
@@ -4,7 +4,8 @@ class Solution:
         rates = [1] * n
         candy = 1
         for i in range(1, n):
-            if(ratings[i] > ratings[i - 1]):
+            if(ratings[i] > ratings[i - 1] \
+               and rates[i] <= rates[i - 1]):
                 rates[i] = rates[i - 1] + 1
         
         for i in range(n - 2, -1, -1):
