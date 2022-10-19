89cf00bc7d0d377a873414cc1ae0ce43ac02361b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-19 23:13:50 +0530

    Time: 289 ms, Memory: 16.7 MB - LeetHub

--- candy/candy.py
+++ candy/candy.py
@@ -1,17 +1,19 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        def traverse(ratings, reverse):
+            indexes = range(n - 2, -1, -1)\
+            if(reverse) else range(1, n) 
+            
+            direc = 1 if(reverse) else -1
+            for i in indexes:
+                if(ratings[i] > ratings[i + direc] \
+                   and rates[i] <= rates[i + direc]):
+                    rates[i] = rates[i + direc] + 1
+        
         n = len(ratings)
         rates = [1] * n
-        candy = 1
-        for i in range(1, n):
-            if(ratings[i] > ratings[i - 1] \
-               and rates[i] <= rates[i - 1]):
-                rates[i] = rates[i - 1] + 1
-        
-        for i in range(n - 2, -1, -1):
-            if(ratings[i] > ratings[i + 1]\
-               and rates[i] <= rates[i + 1]):
-                rates[i] = rates[i + 1] + 1
+        traverse(ratings, False)
+        traverse(ratings, True)
         
         return sum(rates)
         
