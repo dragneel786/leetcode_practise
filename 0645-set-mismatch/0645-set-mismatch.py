32d16b49002fa14b44261408953786e8a6abb23b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-23 18:13:17 +0530

    Time: 554 ms (23.69%), Space: 15.4 MB (74.26%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,13 +1,16 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         xor_nums = 0
-        counts = Counter(nums)
         repeat = 0
         
         for i, num in enumerate(nums, 1):
-            xor_nums ^= (i ^ num)
-            if(counts[num] == 2):
-                repeat = num
-        
+            val = abs(num)
+            if(nums[val - 1] < 0):
+                repeat = val
+            else:
+                nums[val - 1] *= -1
+            
+            xor_nums ^= (i ^ val)
+            
         return [repeat, xor_nums ^ repeat]
         
