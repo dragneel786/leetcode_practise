de4be3ee934bd293bf10747871adb06641a6187b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-23 23:59:19 +0530

    Time: 1970 ms, Memory: 17.4 MB - LeetHub

---
+++ minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        from fractions import Fraction
+        
+        combos = sorted((Fraction(w / q), q, w)\
+                        for w, q in zip(wage, quality))
+        sumq = 0
+        heap = []
+        ans = inf
+        
+        for ratio, q, w in combos:
+            heappush(heap, -q)
+            sumq += q
+            
+            if(len(heap) > k):
+                sumq += heappop(heap)
+            
+            if(len(heap) == k):
+                ans = min(ans, sumq * ratio)
+    
+        return ans
+                    
+                
+        
+        
+        
