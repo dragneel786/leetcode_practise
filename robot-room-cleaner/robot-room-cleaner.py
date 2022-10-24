b4fde5c1297b8f467d54ac5ba3989139829b78ce
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-24 18:45:53 +0530

    Time: 174 ms, Memory: 16 MB - LeetHub

---
+++ robot-room-cleaner/robot-room-cleaner.py
@@ -0,0 +1,84 @@
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot:
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """
+
+class Solution:
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
+        def move_robot(x, y, din):
+            robot.clean()
+            
+            for i in range(4):
+                ndin = (din + i) % 4
+                dx, dy = faces[ndin]
+                nx, ny = x + dx, y + dy
+                if((nx, ny) not in visited\
+                   and robot.move()):
+                    visited.add((nx, ny))
+                    move_robot(nx, ny, ndin)
+                
+                robot.turnLeft()
+            
+            robot.turnLeft()
+            robot.turnLeft()
+            robot.move()
+            robot.turnLeft()
+            robot.turnLeft()
+            
+        visited = set([(0, 0)])
+        faces = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        move_robot(0, 0, 0)
+        
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
