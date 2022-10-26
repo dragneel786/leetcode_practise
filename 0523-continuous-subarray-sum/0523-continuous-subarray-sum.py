698df250be9e29488ce705e7cb803f4d8787617b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-26 21:07:25 +0530

    Time: 2337 ms (46.34%), Space: 33.2 MB (73.31%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,26 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         
-        def check_and_append(imap, psum, index):
-            j = psum // k
-            while(j > -1):
-                diff = psum - (k * j)
-                if(diff in imap and imap[diff] < index - 1):
-                    return True
-                j -= 1
-            return False
-        
-        
-        psum = nums[0]
-        imap = {0:-1}
-        imap[psum] = imap.get(psum, 0)
-        for i, num in enumerate(nums[1:], 1):
+        imap = {0: 0}
+        psum = 0
+        for i, num in enumerate(nums):
             psum += num
+            mod = psum % k
             
-            if(check_and_append(imap, psum, i)):
+            if(imap.get(mod, inf) < i):
                 return True
             
-            imap[psum] = imap.get(psum, i)
+            if(mod not in imap):
+                imap[mod] = i + 1
         
         return False
             
