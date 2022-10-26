9f397dcbb13599867065b8e457b68aeee604628d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-26 22:55:46 +0530

    Time: 760 ms (12.14%), Space: 16.8 MB (56.28%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,3 +1,18 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        return sum((num for num in sorted(nums)[::2]))
+        num_counts = [0] * (20_001)
+        for num in nums:
+            num_counts[num + 10_000] += 1
+        
+        odd = True
+        ans = 0
+        for i in range(20_001):
+            while(num_counts[i]):
+                if(odd):
+                    ans += (i - 10_000)
+                odd = not odd
+                num_counts[i] -= 1
+        
+        return ans
+                
+            
