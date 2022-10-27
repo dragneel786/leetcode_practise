76baace6a3d2fca7961268aed69f5a5e8c9232a7
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-27 23:19:42 +0530

    Time: 57 ms (88.66%), Space: 14 MB (89.50%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,18 +1,5 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        dcount = Counter(moves)
-        return dcount["L"] == dcount["R"] and\
-    dcount["U"] == dcount["D"]
-        
-#         move_map = {"L":(0, -1), "R":(0, 1),\
-#                     "U":(-1, 0), "D":(1, 0)}
-        
-#         pos = [0, 0]
-#         for move in moves:
-#             dx, dy = move_map[move]
-#             pos[0] += dx
-#             pos[1] += dy
-        
-#         return [0, 0] == pos
-
+        return moves.count("L") == moves.count("R") and \
+    moves.count("U") == moves.count("D")
             
