ec332814b7a3fa94607719166de57e065b9cdd3a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-28 19:09:26 +0530

    Time: 263 ms (23.91%), Space: 17.8 MB (67.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        def create_bits_key(word):
-            imap = [0] * 26
-            for c in word:
-                imap[ord(c) - 97] += 1
-            print(imap)
-            return "|".join(map(str, imap))
-        
-        
-        vmap = dict()
+        groups = defaultdict(list)
         for word in strs:
-            key = create_bits_key(word)
-            print(key)
-            vmap[key] = vmap.get(key, [])
-            vmap[key].append(word)
-        
-        return vmap.values()
-            
+            key = "".join(sorted(word))
+            groups[key].append(word)
+        return groups.values()
