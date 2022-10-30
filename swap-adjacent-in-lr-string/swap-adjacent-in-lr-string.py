29cf985c548f5d92b2fdb2d82ad3d9a59bdbb217
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-30 19:52:07 +0530

    Time: 117 ms, Memory: 14 MB - LeetHub

--- swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.py
+++ swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.py
@@ -6,18 +6,16 @@ class Solution:
         ps = pe = 0
         sn, en = len(start), len(end)
         while(ps < sn and pe < en):
-            if(start[ps] == 'X' or end[pe] == 'X'):
-                ps += start[ps] == 'X'
-                pe += end[pe] == 'X'
-            
-            else:
+            if(start[ps] != 'X' and end[pe] != 'X'):
                 if(start[ps] == 'L' and ps < pe):
                     return False
 
                 if(start[ps] == 'R' and ps > pe):
                     return False
+                ps, pe = ps + 1, pe + 1
+                continue
             
-                ps += 1
-                pe += 1
+            ps += start[ps] == 'X'
+            pe += end[pe] == 'X'
         
         return True
