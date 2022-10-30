c53127b0225158f863e43701ef063b33f2d415a6
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-30 13:29:46 +0530

    Time: 2010 ms (7.63%), Space: 14.4 MB (51.12%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,20 +1,22 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        def sub_direction(x, y):
-            nonlocal ans
-            for dx, dy in [(0, 1), (0, -1),\
-                           (1, 0), (-1, 0)]:
-                nx, ny = x + dx, y + dy
-                if(0 <= nx < m and 0 <= ny < n and grid[nx][ny]):
-                    ans -= 1
         
         m, n = len(grid), len(grid[0])
         ans = 0
-        for r in range(m):
-            for c in range(n):
-                if(grid[r][c]):
-                    ans += 4
-                    sub_direction(r, c)
-        
+        for x in range(m):
+            for y in range(n):
+                for dx, dy in [(0, -1), (-1, 0)]:
+                    nx, ny = x + dx, y + dy
+                    if(0 <= nx < m and 0 <= ny < n):
+                        ans += grid[x][y] != grid[nx][ny]
+                    else:
+                        ans += grid[x][y]
+                        
+        for x in range(n):
+            ans += grid[m - 1][x]
+            
+        for y in range(m):
+            ans += grid[y][n - 1]
+                        
         return ans
         
