d398d8e1f111d89079fa0741ccf7236413a1f00e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-10-30 13:13:55 +0530

    Time: 709 ms (80.98%), Space: 14.2 MB (73.73%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,24 +1,20 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        
-        def dfs(x, y, visited = set()):
-            visited.add((x, y))
-            res = 0
-            for dx, dy in [(0, 1), (1, 0),\
-                           (-1, 0), (0, -1)]:
+        def sub_direction(x, y):
+            nonlocal ans
+            for dx, dy in [(0, 1), (0, -1),\
+                           (1, 0), (-1, 0)]:
                 nx, ny = x + dx, y + dy
                 if(0 <= nx < m and 0 <= ny < n and grid[nx][ny]):
-                    if((nx, ny) in visited):
-                        continue
-                    res += dfs(nx, ny)
-                else:
-                    res += 1
-                      
-            return res
+                    ans -= 1
         
         m, n = len(grid), len(grid[0])
+        ans = 0
         for r in range(m):
             for c in range(n):
                 if(grid[r][c]):
-                    return dfs(r, c)
+                    ans += 4
+                    sub_direction(r, c)
+        
+        return ans
         
