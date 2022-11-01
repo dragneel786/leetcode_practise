1b1a5c64c9b75bd3af529229b35c2c1ae320f9cb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-01 13:15:37 +0530

    Time: 284 ms (77.04%), Space: 14.2 MB (98.48%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -7,16 +7,13 @@ class Solution:
             x, y = 0, c
             while(x != rows):
                 val = grid[x][y]
-                if(val == 1 and (y == cols - 1 or grid[x][y + val] == -1)):
-                    res.append(-1)
-                    break
-                
-                if(val == -1 and (y == 0 or grid[x][y + val] == 1)):
+                y += val
+                if(y < 0 or y == cols or\
+                   grid[x][y - val] != grid[x][y]):
                     res.append(-1)
                     break
                 
                 x += 1
-                y += val
             else:
                 res.append(y)
         
