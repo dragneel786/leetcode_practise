2ff4546d2aa971be4ed7714eeda6402e599de493
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-02 23:36:29 +0530

    Time: 88 ms, Memory: 17.9 MB - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        size = max(map(max, intervals)) + 1
-        index_arr = [0] * size
-        for (s, e) in intervals:
-            index_arr[s] += 1
-            index_arr[e] -= 1
+        intervals.sort()
+        heap = []
+        max_size = 0
+        print(intervals)
+        for s, e in intervals:
+            while(heap and heap[0] <= s):
+                heappop(heap)
+            
+            heappush(heap, e)
+            
+            max_size = max(len(heap), max_size)
         
-        val = maxv = 0
-        for i in range(1, size):
-            val += index_arr[i - 1]
-            maxv = max(maxv, val)
-    
-        return maxv
+        return max_size
