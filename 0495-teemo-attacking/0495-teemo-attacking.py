80fd77c424af44a6c57dcac9f6619e55a084b74e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-05 20:46:46 +0530

    Time: 669 ms (34.01%), Space: 15.6 MB (38.11%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.py
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -1,17 +1,13 @@
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        prev_shot = timeSeries[0]
-        poisoned_till = prev_shot + duration - 1
-        total_time = 0
         
-        for shot in timeSeries[1:]:
-            if(shot > poisoned_till):
-                total_time += (poisoned_till - prev_shot + 1)
-                prev_shot = shot
+        total_time = 0
+        for i, shot in enumerate(timeSeries[:-1], 1):
+            total_time += min(timeSeries[i]\
+                              - timeSeries[i - 1], duration)
+            
                 
-            poisoned_till = shot + duration - 1
-                
-        return total_time + (poisoned_till - prev_shot + 1)
+        return total_time + duration
                 
                 
         
