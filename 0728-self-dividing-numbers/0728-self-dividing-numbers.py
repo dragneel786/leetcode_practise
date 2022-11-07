8d704cd1bef83451799d1df061331a183206b9eb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-07 23:08:44 +0530

    Time: 75 ms (77.80%), Space: 13.8 MB (72.69%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -1,15 +1,17 @@
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         
-        def is_div(num):
-            temp = num
-            while(temp):
-                mod = temp % 10
-                temp //= 10
-                if(not mod or num % mod):
-                    return False
-            return True
+        def is_div():
+            for num in range(left, right + 1):
+                temp = num
+                while(temp):
+                    mod = temp % 10
+                    if(not mod or num % mod):
+                        break
+                    temp //= 10
+                else:
+                    yield num
         
-        return filter(is_div, range(left, right + 1))
+        return [i for i in is_div()]
         
             
