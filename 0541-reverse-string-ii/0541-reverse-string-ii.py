be5ac8124c31b64b00b56cafa22e56915d65f1fd
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-08 21:23:34 +0530

    Time: 70 ms (31.72%), Space: 14.1 MB (58.69%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,17 +1,10 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        
-        def rivers(left, right):
-            while(left < right):
-                s[left], s[right] = s[right], s[left]
-                left += 1
-                right -= 1
-            
-        
         n = len(s)
         s = list(s)
         
         for i in range(0, n, 2 * k):
-            rivers(i, min(i + k - 1, n - 1))
+            left, right = i, min(i + k, n)
+            s[left: right] = s[left: right][::-1]
         
         return "".join(s)
