139218ad95c1f80106114c33276a97ad691c53dc
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-08 21:10:01 +0530

    Time: 79 ms (24.04%), Space: 13.8 MB (61.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,12 +3,13 @@ class Solution:
         n = len(s)
         if(n == 1): return s
         
-        st = deque()
+        st = deque([chr(0)])
         for c in s:
-            if(st and abs(ord(st[-1]) - ord(c)) == 32):
+            if(abs(ord(st[-1]) - ord(c)) == 32):
                 st.pop()
                 continue
             
             st.append(c)
         
+        st.popleft()
         return "".join(st)
