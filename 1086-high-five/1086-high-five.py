5c1dfc4c60146e8a9b45c135df81f8d244376b0e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-19 21:25:11 +0530

    Time: 205 ms (8.49%), Space: 14.4 MB (10.69%) - LeetHub

--- 1086-high-five/1086-high-five.py
+++ 1086-high-five/1086-high-five.py
@@ -3,11 +3,9 @@ class Solution:
         scores = defaultdict(list)
         for ide, score in items:
             heappush(scores[ide], score)
-            if(len(scores[ide]) > 5):
-                heappop(scores[ide])
         
         res = []
         for ide, sc in scores.items():
-            res.append([ide, sum(sc) // 5])
+            res.append([ide, sum(nlargest(5, sc)) // 5])
         
         return sorted(res, key=lambda x: x[0])
