ee607ad51eb06c4685a11c650ff98ff844043097
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-23 21:26:07 +0530

    Time: 201 ms (61.76%), Space: 13.9 MB (36.06%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,16 +1,12 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         hash_board = defaultdict(set)
-        k = 0
         for r in range(len(board)):
-            if(r and r % 3 == 0):
-                k += 3
             for c in range(len(board[0])):
                 val = board[r][c]
                 if(val != '.'):
                     for st in ['r' + str(r), 'c' + str(c),\
-                               str((r//3) * 3 + (c//3))]:
-                        
+                               str(r//3) + str(c//3)]:
                         if(val in hash_board[st]):
                             return False
 
