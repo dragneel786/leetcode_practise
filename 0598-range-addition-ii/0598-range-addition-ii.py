22eb6a84899b9be1aafa3ebc80ad1c68840b6a3d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-11-27 18:40:10 +0530

    Time: 165 ms (35.54%), Space: 16 MB (33.57%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.py
+++ 0598-range-addition-ii/0598-range-addition-ii.py
@@ -1,14 +1,7 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        if(not ops):
-            return m * n
-        
-        minx, miny = m, n
-        for x, y in ops:
-            minx = min(x, minx)
-            miny = min(y, miny)
-        
-        return minx * miny
+        ops.append([m, n])
+        return min(ops, key=lambda x: x[0])[0] * min(ops, key=lambda x: x[1])[1]
             
         
         
