8efd594d55ee03c8ac1a71a0114a1149534fb70b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-02 21:48:50 +0530

    Time: 32 ms (98.88%), Space: 14.1 MB (39.42%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,9 +1,4 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        counter = Counter(arr1)
-        res = []
-        for a in arr2:
-            res.extend([a] * counter[a])
-            del counter[a]
-        
-        return res + sorted([a for a, c in counter.items() for _ in range(c)])
+        k = {a: i for i, a in enumerate(arr2)}
+        return sorted(arr1, key=lambda a: k.get(a, 1000+a))
