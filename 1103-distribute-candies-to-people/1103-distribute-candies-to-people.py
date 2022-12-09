02681ecfae3450b81be8edb83abbed4e2af05770
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-09 22:34:35 +0530

    Time: 91 ms (36.03%), Space: 13.9 MB (53.91%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
@@ -4,17 +4,17 @@ class Solution:
         compute = lambda i, n: (i * n + 1) + (i * n + n)
         factor = (num_people / 2)
         c = 0
+        res = [0] * num_people
+        
         val = factor * compute(c, num_people)
         while(val <= candies):
             c += 1
+            for i in range(1, num_people + 1):
+                res[i - 1] += (num_people * (c - 1)) + i
+                
             candies -= val
             val = factor * compute(c, num_people)
-        
-        res = [0] * num_people
-        for i in range(1, num_people + 1):
-            for j in range(c):
-                res[i - 1] += (num_people * j) + i
-        
+    
         i = 0
         while(candies):
             val = (num_people * c) + (i + 1)
