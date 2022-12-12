008dccd683c6b76588623a99bca2aacb84e5ad94
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-12 20:57:21 +0530

    Time: 67 ms (8.08%), Space: 13.8 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        
+        @lru_cache(None)
         def climb_it(n):
             if(n <= 0):
                 return n == 0
             
-            if(n in cmap):
-                return cmap[n]
-            
-            cmap[n] = climb_it(n - 1) + climb_it(n - 2)
-            return cmap[n]
+            return climb_it(n - 1) + climb_it(n - 2)
         
-        cmap = dict()
         return climb_it(n)
