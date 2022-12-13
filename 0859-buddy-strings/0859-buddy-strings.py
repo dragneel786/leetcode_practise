4d2308447e67ed194ee6084ce679b0ae3bac74f2
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-13 22:21:07 +0530

    Time: 52 ms (70.46%), Space: 14.2 MB (63.09%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -3,23 +3,17 @@ class Solution:
         if(len(s) != len(goal)):
             return False
         
+        if(s == goal):
+            return len(s) > 26 or\
+        len(set(s)) != len(s)
+        
         diff = dict()
-        counts = Counter()
-        consider = False
         for a, b in zip(s, goal):
             if(a != b):
                 if(a in diff or len(diff) >= 2):
                     return False
                 
                 diff[a] = b
-            
-            else:
-                counts[a] += 1
-                if(counts[a] >= 2):
-                    consider = True
-            
-        if(s == goal):
-            return consider
-         
+                
         return sorted(diff.keys()) == sorted(diff.values())
     
