594b9a54ab95dfad4e902307f1ac2fd72b536575
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-14 20:01:47 +0530

    Time: 31 ms (94.97%), Space: 13.9 MB (66.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        max_till = max_so_far = nums[0]
-        prev_max = 0
+        max_till = 0
+        max_so_far = nums[0]
         
         for num in nums[1:]:
-            curr_max = max(max_till, prev_max + num)
-            max_so_far = max(curr_max, max_so_far)
-            if(curr_max == max_till):
-                prev_max = max_till
+            curr_max = max_till + num
+            if(curr_max > max_so_far):
+                max_till = max_so_far
+                max_so_far = curr_max
             else:
-                prev_max = max_till
-                max_till = curr_max
+                max_till = max_so_far
         
         return max_so_far
         
