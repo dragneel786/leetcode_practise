652f4d32538b7297da0647f04274e0be49a01789
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-14 19:32:56 +0530

    Time: 24 ms (99.53%), Space: 14 MB (20.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        @lru_cache(None)
         def rob_it(idx):
             if(idx >= len(nums)):
                 return 0
             
-            if(idx in memo):
-                return memo[idx]
+            return max(rob_it(idx + 2) + nums[idx], rob_it(idx + 1))
             
-            memo[idx] = max(rob_it(idx + 2) + nums[idx], rob_it(idx + 1))
-            return memo[idx]
-        
-        memo = dict()
         return max(rob_it(0), rob_it(1))
