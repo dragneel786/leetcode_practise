bcc06a12e4401bd7b61e94f79913eac38b585efe
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2022-12-26 15:49:45 +0530

    Time: 41 ms (76.37%), Space: 13.9 MB (63.81%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def swap(red, blue, i):
+            if(i >= red and nums[i] == 0):
+                nums[red], nums[i] = nums[i], nums[red]
+
+            if(i <= blue and nums[i] == 2):
+                nums[blue], nums[i] = nums[i], nums[blue]
+        
+        n = len(nums)
+        i = red = 0
+        blue = n - 1
+        
+        while(red != blue and i < n):
+            while(red < n and nums[red] == 0):
+                red += 1
+            
+            while(blue > 0 and nums[blue] == 2):
+                blue -= 1
+            
+            swap(red, blue, i)
+            swap(red, blue, i)
+                
+            i += 1
+        
+        
