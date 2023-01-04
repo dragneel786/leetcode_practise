dba887ca7ddac4cd9417e944c2532bd4b2697251
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-04 19:07:50 +0530

    Time: 999 ms (83.61%), Space: 28.3 MB (60.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,19 +1,20 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        freq_val = Counter(Counter(tasks).values())
-        if(1 in freq_val):
-            return -1
+        freq_val = Counter(tasks).values()
+        # if(1 in freq_val):
+        #     return -1
         
-        n = max(3, max(freq_val.keys())) + 1
         ans = 0
-        dp = [inf] * n
-        dp[0] = 0
-        for i in range(2, n):
-            dp[i] = min(dp[i - 2], dp[i - 3]) + 1
-            if(i in freq_val):
-                ans += dp[i] * freq_val[i]
-
+        for f in freq_val:
+            if(f == 1):
+                return -1
+            
+            ans += (f // 3)
+            if(f % 3):
+                ans += 1
+        
         return ans
+                
         
         
         
