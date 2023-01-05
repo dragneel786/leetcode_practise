f7bbbb4fcbf275b3071dcbabf13d4893e0802c5a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-05 10:55:48 +0530

    Time: 644 ms (48.82%), Space: 17.8 MB (45.73%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,28 @@
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        q = deque()
+        rows, cols = len(rooms), len(rooms[0])
+        for r in range(rows):
+            for c in range(cols):
+                if(rooms[r][c] == 0):
+                    q.append((r, c))
+                
+        while(q):
+            visited = set()
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in [(1, 0), (0, 1),\
+                               (-1, 0), (0, -1)]:
+                    nr, nc = r + dr, c + dc
+                    if(0 <= nr < rows and 0 <= nc < cols\
+                       and rooms[nr][nc] != -1 and\
+                       rooms[r][c] + 1 < rooms[nr][nc]):
+                    
+                        rooms[nr][nc] = rooms[r][c] + 1
+                        if((nr, nc) not in visited):
+                            q.append((nr, nc))
+                            visited.add((nr, nc))
+        
