248c49214eb27caa372a53dee5015118f672b459
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-08 17:08:04 +0530

    Time: 50 ms (94.08%), Space: 14 MB (80.92%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,25 +1,13 @@
 class Solution:
     def canBeIncreasing(self, nums: List[int]) -> bool:
-        def issorted(skip):
-            prev = 0 if(skip) else 1
-            for i in range(prev + 1, n):
-                if(i == skip):
-                    continue
-                elif(nums[prev] < nums[i]):
-                    prev = i
-                else:
-                    return False
-
-            return True
-
-
-        n = len(nums)
+        count = 0
         for i in range(1, len(nums)):
             if(nums[i - 1] >= nums[i]):
-                j = i
-                break
-
-        return i == n - 1 or issorted(j) or issorted(j - 1)
+                count += 1
+                if(i >= 2 and nums[i - 2] >= nums[i]):
+                    nums[i] = nums[i - 1]
+        
+        return count < 2
         
                 
                     
