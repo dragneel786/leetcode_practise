a4a22f82f3e25e3286cf3a94507871c467f261e3
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-08 12:21:34 +0530

    Time: 70 ms (96.06%), Space: 14 MB (75.79%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,12 +1,15 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         maxl = 1
-        for i, (x1, y1) in enumerate(points):
-            has = defaultdict(int)
-            for j, (x2, y2) in enumerate(points):
-                if(i != j):
-                    val = atan2(x2 - x1, y2 - y1)
-                    has[val] += 1
-                    maxl = max(maxl, has[val] + 1)
-        
+        for i, (x1, y1) in enumerate(points[:-1]):
+            has = defaultdict(lambda:0)
+            for j, (x2, y2) in enumerate(points[i + 1:]):
+                if(x2 == x1):
+                    has[inf] += 1
+                    maxl = max(maxl, has[inf] + 1)
+                else:
+                    slope = (y2 - y1) / (x2 - x1)
+                    has[slope] += 1
+                    maxl = max(maxl, has[slope] + 1)
+                
         return maxl
