f4e354f9bd14ef75f824be79a0ec18582416cce1
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-12 18:55:58 +0530

    Time: 2780 ms (78.85%), Space: 200.6 MB (30.13%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -9,16 +9,12 @@ class Solution:
             
             return tree
         
-        def count_labels(s = 0, visited = set()):
-            visited.add(s)
-            lcount = Counter()
+        def count_labels(s = 0, p = -1):
+            lcount = Counter(labels[s])
             for v in tree[s]:
-                if(v not in visited):
-                    lcount.update(count_labels(v, visited))
-            
-            lcount[labels[s]] += 1
+                if(v != p):
+                    lcount.update(count_labels(v, s))
             ans[s] += lcount[labels[s]]
-            visited.remove(s)
             return lcount
         
         
