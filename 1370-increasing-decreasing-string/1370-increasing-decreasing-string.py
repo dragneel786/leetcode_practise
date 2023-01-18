a1841af29b3b668de4e249909f9478e9d8234584
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-18 22:14:59 +0530

    Time: 70 ms (70.71%), Space: 13.8 MB (99.05%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortString(self, s: str) -> str:
+        scounts = Counter(s)
+        keys = list(sorted(scounts.keys()))
+        ans = []
+        while(scounts):
+            for c in keys + keys[::-1]:
+                if(scounts[c]):
+                    ans.append(c)
+                    scounts[c] -= 1
+                    if(not scounts[c]):
+                        del scounts[c]
+        
+        return ''.join(ans)
+
+            
