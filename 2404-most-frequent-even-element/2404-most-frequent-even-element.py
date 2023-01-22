25ed4878e5851bc5029f9102b4a3aaf536d0ad9a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-22 11:47:45 +0530

    Time: 472 ms (51.56%), Space: 14.2 MB (97.34%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -1,12 +1,15 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         res = count = -1
-        for c, num in sorted([(c, n) for n, c in Counter(nums).items()]):
+        ncount = Counter()
+        for num in nums:
+            ncount[num] += 1
             if(num % 2 == 0):
-                if(count < c):
+                if(ncount[num] > count or\
+                   (ncount[num] == count and num < res)):
                     res = num
-                    count = c
-            
+                    count = ncount[num]
+                
         return res
                 
             
