bd92fe66d6026cfab10ef16f6a404ce5164aa57f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-22 20:52:18 +0530

    Time: 71 ms (5.08%), Space: 13.9 MB (41.27%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.py
+++ 2409-count-days-spent-together/2409-count-days-spent-together.py
@@ -1,27 +1,17 @@
 class Solution:
     def countDaysTogether(self, arriveAlice: str, leaveAlice: str, arriveBob: str, leaveBob: str) -> int:
         days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        
-        arriveAlice = list(map(int, arriveAlice.split('-')))
-        leaveAlice = list(map(int, leaveAlice.split('-')))
-        
-        arriveBob = list(map(int, arriveBob.split('-')))
-        leaveBob = list(map(int, leaveBob.split('-')))
-        
-        alice = [sum(days[:arriveAlice[0]]) + arriveAlice[1],\
-                 sum(days[:leaveAlice[0]]) + leaveAlice[1]]
-        bob = [sum(days[:arriveBob[0]]) + arriveBob[1],\
-                 sum(days[:leaveBob[0]]) + leaveBob[1]]
-        
         years = [0] * 367
-        years[alice[0]] += 1
-        years[alice[1] + 1] -= 1
-        years[bob[0]] += 1
-        years[bob[1] + 1] -= 1
+        years[sum(days[:int(arriveAlice[0:2])])\
+                  + int(arriveAlice[3:])]+= 1
+        years[sum(days[:int(leaveAlice[0:2])])\
+                  + int(leaveAlice[3:]) + 1] -= 1
         
-        count = 0
+        years[sum(days[:int(arriveBob[0:2])])\
+                  + int(arriveBob[3:])] += 1
+        years[sum(days[:int(leaveBob[0:2])])\
+                  + int(leaveBob[3:]) + 1] -= 1
+        
         for i in range(1, 367):
             years[i] += years[i - 1]
-            if(years[i] > 1):
-                count += 1
-        return count
+        return years.count(2)
