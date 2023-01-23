cd23f3acc30015ca3a88e5b0c213f8a608074c77
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-23 17:59:56 +0530

    Time: 1997 ms (16.93%), Space: 19 MB (59.07%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,17 +1,14 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        people = {i for i in range(1, n + 1)}
-        tcount = defaultdict(int)
-        
+        trust_to = [0] * (n + 1)
+        trust_by = [0] * (n + 1)
         for a, b in trust:
-            people.discard(a)
-            tcount[b] += 1
+            trust_to[b] += 1
+            trust_by[a] += 1
         
-        if(len(people) != 1):
-            return -1
+        ans = -1
+        for i in range(1, n + 1):
+            if(trust_to[i] == (n - 1) and trust_by[i] == 0):
+                ans = i
         
-        val = list(people)[0]
-        if(tcount[val] == n - 1):
-            return val
-        
-        return -1 
+        return ans
