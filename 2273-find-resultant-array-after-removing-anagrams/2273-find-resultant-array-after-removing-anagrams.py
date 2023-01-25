01ae23536967984adac2e56f579685b9eeffeabc
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-25 23:38:46 +0530

    Time: 101 ms (35.48%), Space: 14 MB (28.65%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,8 +1,8 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         
-        make = lambda w: ''.join(sorted(w))
-        curr = ''
+        make = lambda w: Counter(w)
+        curr = None
         ans = []
         for w in words:
             val = make(w)
