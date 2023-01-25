68fe770523c75623b70583bc0eeb2dde3853a443
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-25 22:12:36 +0530

    Time: 360 ms (44.71%), Space: 21.1 MB (38.14%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.py
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def canThreePartsEqualSum(self, arr: List[int]) -> bool:
         n = len(arr)
-        imap = defaultdict(list)
-        imap[arr[0]].append(0)
-        for i in range(1, n):
-            arr[i] += arr[i - 1]
-            imap[arr[i]].append(i)
+        tot = sum(arr)
+        sums = 0
+        parts = 0
+        for i in range(n):
+            sums += arr[i]
+            if(sums == tot / 3):
+                parts += 1
+                sums = 0
+                
+        return parts >= 3
+            
         
-        for i in range(n - 2):
-            for idx in imap[(arr[-1] - arr[i])]:
-                if(i < idx < n - 1 and arr[i] == (arr[idx] - arr[i])):
-                    return True
-        
-        return False
                 
