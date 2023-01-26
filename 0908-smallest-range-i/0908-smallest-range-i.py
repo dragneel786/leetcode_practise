ed6269cb74ec4bdf64b3074a16198e47343a72a8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-26 16:58:25 +0530

    Time: 117 ms (80.11%), Space: 15.1 MB (86.43%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.py
+++ 0908-smallest-range-i/0908-smallest-range-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        diff = nums[-1] - nums[0]
+        
+        diff = max(nums) - min(nums)
         if(diff <= (k * 2)):
             return 0
         
