53044e20c988455d5a65881c5ee26784ba10bb9f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-01-26 17:12:04 +0530

    Time: 40 ms (54.10%), Space: 13.9 MB (87.28%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -7,31 +7,25 @@
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         
-        q = deque([root])
-        
-        while(q):
-            sibling = cousin = False
-            
-            for _ in range(len(q)):
-                node = q.popleft()
-                
-                if(not node):
-                    sibling = False
-                else:
-                    if(node.val == x or node.val == y):
-                        if(not cousin):
-                            sibling = cousin = True
-                        else:
-                            return not sibling
-                
-                    q.append(node.left)
-                    q.append(node.right)
-                    q.append(None)
-            
-            if(cousin):
+        def is_cousins(node, p, curr_level):
+            nonlocal level
+            if(not node or (level and curr_level > level[0])):
                 return False
+            
+            if(node.val == x or node.val == y):
+                if(not level):
+                    level = (curr_level, p)
+                    return False
+                
+                return level[0] == curr_level and level[1] != p
+            
+            return is_cousins(node.left, node.val, curr_level + 1) or\
+        is_cousins(node.right, node.val, curr_level + 1)
         
-        return False
+        
+        level = None
+        return is_cousins(root, -1, 0)
+        
                 
                 
             
