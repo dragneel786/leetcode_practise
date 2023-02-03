73112cecdbe26a5879231a2c3783ee21c4be802e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-03 20:41:51 +0530

    Time: 56 ms (22.82%), Space: 13.9 MB (42.79%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.py
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -4,11 +4,10 @@ class Solution:
         m, n = len(sentence1), len(sentence2)
         res = False
         if(m == n):
-            pair_fun = lambda a, b: (a, b) if(a < b) else (b, a)
-            pairs = set([pair_fun(a, b) for a, b in similarPairs])
-            
+            pairs = set([a + '-' + b for a, b in similarPairs])
             for a, b in zip(sentence1, sentence2):
-                if(a != b and pair_fun(a, b) not in pairs):
+                if(a != b and a+'-'+b not in pairs\
+                   and b + '-' + a not in pairs):
                     break
             else:
                 res = True
