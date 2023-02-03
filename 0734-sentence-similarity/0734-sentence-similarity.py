d39f3db0188c4c4285511e9d2cd981cc062d529c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-03 20:34:24 +0530

    Time: 54 ms (33.48%), Space: 14 MB (42.79%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.py
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -1,10 +1,16 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
         
-        pairs = {tuple(sp) for sp in similarPairs}
-        for a, b in zip_longest(sentence1, sentence2):
-            if(a != b and (a, b) not in pairs\
-               and (b, a) not in pairs):
-                return False
-        
-        return True
+        m, n = len(sentence1), len(sentence2)
+        res = False
+        if(m == n):
+            pair_fun = lambda a, b: (a, b) if(a < b) else (b, a)
+            pairs = set([pair_fun(a, b) for a, b in similarPairs])
+            
+            for a, b in zip(sentence1, sentence2):
+                if(a != b and pair_fun(a, b) not in pairs):
+                    break
+            else:
+                res = True
+            
+        return res
