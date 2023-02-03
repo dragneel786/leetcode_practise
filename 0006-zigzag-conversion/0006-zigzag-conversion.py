5dabe46c564c6ecedb34e84489fedfdf3b576945
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-03 18:44:39 +0530

    Time: 49 ms (95.18%), Space: 14.1 MB (29.22%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,21 +1,15 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         change_dir = lambda i: i < 0 or i >= numRows
-        d = 1
-        k = 0
+        d = -1
+        k = -1
         ans = [[] for _ in range(numRows)]
         for c in s:
-            if(change_dir(k)):
+            if(change_dir(k + d)):
                 d *= -1
-                k += (d * 2)
-            ans[k].append(c)
+            ans[k + d].append(c)
             k += d
         
         return ''.join(map(''.join, ans))
-            
-            
-            
-        
-        return ans
         
         
