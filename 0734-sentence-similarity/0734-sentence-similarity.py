067981666a25b6a7423d4be6d89ad6a1a78165b8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-03 20:44:32 +0530

    Time: 46 ms (82.43%), Space: 14 MB (42.79%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.py
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -7,10 +7,10 @@ class Solution:
             pairs = defaultdict(set)
             for a, b in similarPairs:
                 pairs[a].add(b)
+                pairs[b].add(a)
                 
             for a, b in zip(sentence1, sentence2):
-                if(a != b and b not in pairs[a]\
-                   and a not in pairs[b]):
+                if(a != b and a not in pairs[b]):
                     break
             else:
                 res = True
