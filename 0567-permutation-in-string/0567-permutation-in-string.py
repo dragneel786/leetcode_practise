d1f8582098d1d7de7dbf5cea19219dc16e253c56
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-04 14:04:09 +0530

    Time: 58 ms (95.99%), Space: 14 MB (28.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         m, n = len(s1), len(s2)
-#         if(m > n):
-#             return False
-        
         target = [0] * 26
-        for c in s1:
+        window = [0] * 26
+        for c, a in zip(s1, s2):
+            window[ord(a) - 97] += 1
             target[ord(c) - 97] += 1
         
-        window = [0] * 26
-        for i in range(n):
-            if(i >= m):
-                if(window == target):
-                    return True
-                window[ord(s2[i - m]) - 97] -= 1
+        for i in range(m, n):
+            if(window == target):
+                return True
+            window[ord(s2[i - m]) - 97] -= 1
             window[ord(s2[i]) - 97] += 1
             
-        return target == window
+        return m <= n and target == window
         
