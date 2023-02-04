725f6c6da14e1abe0d5f035090d99bdc1cc9f2fb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-04 13:59:40 +0530

    Time: 51 ms (98.98%), Space: 13.9 MB (64.59%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        m, n = len(s1), len(s2)
+        if(m > n):
+            return False
+        
+        target = [0] * 26
+        for c in s1:
+            target[ord(c) - 97] += 1
+        
+        window = [0] * 26
+        for i in range(n):
+            if(i >= m):
+                if(window == target):
+                    return True
+                window[ord(s2[i - m]) - 97] -= 1
+            window[ord(s2[i]) - 97] += 1
+            
+        return target == window
+        
