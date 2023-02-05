0a0edaa84e11808df7cd107c3440c495fa28635e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-05 19:28:41 +0530

    Time: 50 ms (65.17%), Space: 13.8 MB (86.76%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,19 +1,14 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        
-        @lru_cache(None)
-        def min_cost(i = 0):
-            if(i >= len(days)):
-                return 0
-            
-            ans = inf
+        n = len(days)
+        dp = [inf] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
             for c, d in zip(costs, [0, 6, 29]):
-                j = i
-                while(j < len(days) and days[j] <= days[i] + d):
-                    j += 1
+                j = i - 1
+                while(j > 0 and days[j - 1] >= days[i - 1] - d):
+                    j -= 1
                 
-                ans = min(ans, c + min_cost(j))
+                dp[i] = min(dp[i], dp[j] + c)
             
-            return ans
-    
-        return min_cost()
+        return dp[n]
