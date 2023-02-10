3565aee6574a41a922adf3e0f1fde8bf8d8eb99e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-10 20:05:41 +0530

    Time: 535 ms (87.29%), Space: 14.6 MB (80.10%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -4,23 +4,21 @@ class Solution:
         
         def bfs(q):
             nonlocal rows, cols
-            ans = -1
+            max_dis = 0
             while(q):
                 for _ in range(len(q)):
-                    key, r, c = q.popleft()
-                    ans = max(ans, grid[r][c])
-                    
+                    r, c = q.popleft()
                     for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
                         
                         nr, nc = (r + dr), (c + dc)
-                        kr, kc = comap[key]
-                        md = abs(kr - nr) + abs(kc - nc)
-                        
                         if(0 <= nr < rows and 0 <= nc < cols\
-                           and grid[nr][nc] > md):
-                            grid[nr][nc] = md
-                            q.append((key, nr, nc))
-            return ans
+                           and not grid[nr][nc]):
+                            grid[nr][nc] = max_dis + 1
+                            q.append((nr, nc))
+                 
+                if(q):
+                    max_dis += 1
+            return max_dis
         
         
         
@@ -31,14 +29,9 @@ class Solution:
         for r in range(rows):
             for c in range(cols):    
                 if(grid[r][c] == 1):
-                    grid[r][c] = -1
-                    comap[counter] = (r, c)
-                    q.append((counter, r, c))
-                else:
-                    grid[r][c] = 3000
-                counter += 1
+                    q.append((r, c))
                 
-
-        return bfs(q)
+        ret = bfs(q)
+        return ret if(ret) else -1
                     
                     
