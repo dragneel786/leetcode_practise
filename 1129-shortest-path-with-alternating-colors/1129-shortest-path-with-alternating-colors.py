893e3d415ee3955625f732a01c78617609cec1d5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-11 18:48:57 +0530

    Time: 95 ms (49.71%), Space: 14.3 MB (31.23%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -0,0 +1,45 @@
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+        
+        def edge_graph():
+            graph = {'R': defaultdict(list),\
+                     'B': defaultdict(list)}
+            
+            for a, b in redEdges:
+                graph['R'][a].append(b)
+                
+        
+            for u, v in blueEdges:
+                graph['B'][u].append(v)
+            
+            return graph
+        
+        
+        def bfs():
+            answer[0] = 0
+            q = deque([(0, 'R'), (0, 'B')])
+            seen = set([(0, 'R'), (0, 'B')])
+            path = 0
+            
+            while(q):
+                path += 1
+                for _ in range(len(q)):
+                    node, color = q.popleft()
+                    ac = alternate_color(color)
+                    for neigh in egraph[color][node]:
+                        if((neigh, color) not in seen):
+                            seen.add((neigh, color))
+                            q.append((neigh, ac))
+                            
+                            answer[neigh] = min(answer[neigh], path)
+                
+                            
+            for i in range(len(answer)):
+                if(answer[i] == inf):
+                    answer[i] = -1
+        
+        alternate_color = lambda x: 'B' if(x == 'R') else 'R'
+        answer = [inf] * n
+        egraph = edge_graph()
+        bfs()
+        return answer
