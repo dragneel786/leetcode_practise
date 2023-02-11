7b996a7e8412f37b94138326436c3843bab7e1e4
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-11 18:52:20 +0530

    Time: 82 ms (93.26%), Space: 14.2 MB (73.17%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -2,30 +2,30 @@ class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
         
         def edge_graph():
-            graph = {'R': defaultdict(list),\
-                     'B': defaultdict(list)}
+            graph = {1: defaultdict(list),\
+                     -1: defaultdict(list)}
             
             for a, b in redEdges:
-                graph['R'][a].append(b)
+                graph[1][a].append(b)
                 
         
             for u, v in blueEdges:
-                graph['B'][u].append(v)
+                graph[-1][u].append(v)
             
             return graph
         
         
         def bfs():
             answer[0] = 0
-            q = deque([(0, 'R'), (0, 'B')])
-            seen = set([(0, 'R'), (0, 'B')])
+            q = deque([(0, 1), (0, -1)])
+            seen = set([(0, 1), (0, -1)])
             path = 0
             
             while(q):
                 path += 1
                 for _ in range(len(q)):
                     node, color = q.popleft()
-                    ac = alternate_color(color)
+                    ac = -1 * color
                     for neigh in egraph[color][node]:
                         if((neigh, color) not in seen):
                             seen.add((neigh, color))
@@ -33,13 +33,7 @@ class Solution:
                             
                             if(answer[neigh] == -1):
                                 answer[neigh] = path
-                
-                            
-            # for i in range(len(answer)):
-            #     if(answer[i] == inf):
-            #         answer[i] = -1
         
-        alternate_color = lambda x: 'B' if(x == 'R') else 'R'
         answer = [-1] * n
         egraph = edge_graph()
         bfs()
