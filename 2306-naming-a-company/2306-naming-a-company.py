2f48f69715908cd249299f2da5ab9cf70da4fe77
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-15 20:41:43 +0530

    Time: 554 ms (82.68%), Space: 28.8 MB (12.28%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -1,19 +1,26 @@
 class Solution:
     def distinctNames(self, ideas: List[str]) -> int:
-        # Group idea by their initials.
-        initial_groups = [set() for _ in range(26)]
-        for idea in ideas:
-            initial_groups[ord(idea[0]) - ord('a')].add(idea[1:])
         
-        answer = 0
-        # Calculate number of valid names from every pair of groups.
-        for i in range(25):
-            for j in range(i + 1, 26):
-                # Get the number of mutual suffixes.
-                num_of_mutual = len(initial_groups[i] & initial_groups[j]) 
+        def create_map():
+            first = defaultdict(set)
+            for idea in ideas:
+                first[idea[0]].add(idea[1:])
+            return first
+
+        fmap = create_map()
+        count = 0
+        keys = list(fmap.keys())
+        for i, k1 in enumerate(keys):
+            for k2 in keys[i + 1:]:
+                mutual_count = len(fmap[k1] & fmap[k2])
+                count += 2 * ((len(fmap[k1]) - mutual_count) * 
+                              (len(fmap[k2]) - mutual_count))
+
+
+        return count
+
                 
-                # Valid names are only from distinct suffixes in both groups.
-                # Since we can swap a with b and swap b with a to create two valid names, multiple answer by 2.
-                answer += 2 * (len(initial_groups[i]) - num_of_mutual) * (len(initial_groups[j]) - num_of_mutual)
                 
-        return answer
+                
+                
+        
