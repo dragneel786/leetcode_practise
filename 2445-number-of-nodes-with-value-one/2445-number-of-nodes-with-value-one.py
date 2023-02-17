bebdb455f80f119881765cf012ac9a64065ebc06
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-17 23:55:31 +0530

    Time: 1224 ms (73.91%), Space: 32.3 MB (52.17%) - LeetHub

---
+++ 2445-number-of-nodes-with-value-one/2445-number-of-nodes-with-value-one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfNodes(self, n: int, queries: List[int]) -> int:
+        def update(start, add = 0):
+            if(start > n):
+                return
+            
+            nodes[start] += add
+            update(start * 2, nodes[start])
+            update(start * 2 + 1, nodes[start])
+            
+        nodes = [0] * (n + 1)
+        for q in queries:
+            nodes[q] += 1
+        update(1)
+        return len(list(filter(lambda x: x % 2 == 1, nodes)))
