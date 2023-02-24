ad2e9591ef8351b63ff3b19b2d36617614c87c68
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-02-24 22:27:39 +0530

    Time: 1637 ms (82.09%), Space: 26.7 MB (13.43%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -1,19 +1,21 @@
+from sortedcontainers import SortedSet
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        q = []
-        minV = float('inf')
-        for i in range(len(nums)):
-            if(nums[i] & 1):
-                nums[i] *= 2
-            minV = min(minV,  nums[i])
-            heapq.heappush(q, -1 * nums[i])
-
-        dev = float('inf')
-        while(not q[0] % 2):
-            val = -1 * q[0]
-            dev = min(dev, val - minV)
-            heapq.heappop(q)
-            minV = min(minV, val // 2)
-            heapq.heappush(q, -1 * val // 2)
+        scontain = SortedSet()
+        for num in nums:
+            if(num % 2):
+                num *= 2
+            scontain.add(num)
+    
+        ans = inf
+        while(True):
+            max_val = scontain.pop()
+            min_val = scontain.pop(0) if(scontain) else max_val
+            ans = min(max_val - min_val, ans)
+            if(max_val % 2 == 1):
+                break
+            
+            scontain.add(max_val // 2)
+            scontain.add(min_val)
         
-        return min(dev, (-1 * q[0]) - minV)
+        return ans
