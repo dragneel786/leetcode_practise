a651f66639eb4e3fd7626dc34a21d1a20409a822
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-01 19:24:58 +0530

    Time: 2303 ms (27.31%), Space: 23.1 MB (29.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,12 +1,28 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        if len(nums) <= 1:
+        def divide(nums):
+            n = len(nums)
+            if(n > 1):
+                left = divide(nums[:n // 2])
+                right = divide(nums[n // 2:])
+                return merge(left, right)
+        
             return nums
-
-        pivot = random.choice(nums)
-        lt = [v for v in nums if v < pivot]
-        eq = [v for v in nums if v == pivot]
-        gt = [v for v in nums if v > pivot]
-
-        return self.sortArray(lt) + eq + self.sortArray(gt)
+        
+        def merge(left, right):
+            merged = []
+            i = 0
+            j = 0
+            while(i < len(left) or j < len(right)):
+                if(j >= len(right) or \
+                    (i < len(left) and left[i] < right[j])):
+                    merged.append(left[i])
+                    i += 1
+                else:
+                    merged.append(right[j])
+                    j += 1
+            
+            return merged
+        
+        return divide(nums)
