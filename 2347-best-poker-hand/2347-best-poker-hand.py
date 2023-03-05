67e8ec13aa91d085f46a0fdea17a61c2e2311268
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-05 18:10:06 +0530

    Time: 30 ms (81.42%), Space: 13.9 MB (10.64%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.py
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -1,15 +1,12 @@
 class Solution:
     def bestHand(self, ranks: List[int], suits: List[str]) -> str:
-        rcounts = Counter(ranks)
-        scounts = Counter(suits)
-        if(len(scounts) == 1):
+        if(len(set(suits)) == 1):
             return "Flush"
         
-        count_values = set(rcounts.values())
-        if(set([3,4,5]) & count_values):
-            return "Three of a Kind"
-        
-        if(2 in count_values):
-            return "Pair"
-        
-        return "High Card"
+        match max(Counter(ranks).values()):
+            case 3 | 4 | 5:
+                return "Three of a Kind"
+            case 2:
+                return "Pair"
+            case _:
+                return "High Card"
