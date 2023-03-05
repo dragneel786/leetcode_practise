586d723c38d27a71ededd7a1bb09a418410b0090
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-05 18:56:40 +0530

    Time: 44 ms (36.59%), Space: 13.7 MB (98.07%) - LeetHub

---
+++ 2451-odd-string-difference/2451-odd-string-difference.py
@@ -0,0 +1,24 @@
+class Solution:
+    def oddString(self, words: List[str]) -> str:
+        
+        def compute(w):
+            ans = []
+            for i in range(1, len(w)):
+                ans.append(ord(w[i]) - ord(w[i - 1]))
+            
+            return ans
+        
+        pattern = compute(words[0])
+        c = 1
+        for w in words[1:]:
+            npattern = compute(w)
+            if(pattern != npattern):
+                if(c > 1):
+                    return w
+                else:
+                    break
+            
+            c += 1
+        return words[0] if(pattern != compute(words[-1])) else w
+                
+                
