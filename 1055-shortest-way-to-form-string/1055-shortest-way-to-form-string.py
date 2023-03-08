9dfea68c713dbb6b7b147613f3ef5c01756e1ba5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-08 16:05:51 +0530

    Time: 32 ms (96.68%), Space: 13.9 MB (76.45%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,26 +1,25 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        maps = defaultdict(lambda:([0] * n))
-        n = len(source)
-        for i, c in enumerate(source):
-            maps[c][i] = i + 1
+        def get_idx(indexes, idx):
+            in_index = bisect_right(indexes, idx)
+            if(in_index == len(indexes)):
+                return indexes[0]
+            return indexes[in_index]
         
-        for c in maps.keys():
-            for j in range(n - 2, -1, -1):
-                if(not maps[c][j]):
-                    maps[c][j] = maps[c][j + 1]
         
-        j = 0
-        counts = 1
-        for c in target:
-            if(not maps[c][0]): return -1
+        imap = defaultdict(list)
+        for i, ch in enumerate(source):
+            imap[ch].append(i)
             
-            if(j == n or not maps[c][j]):
-                counts += 1
-                j = 0
-                
-            j = maps[c][j]
-        
-        return counts
+        steps = 1
+        curr_idx = -1
+        for ch in target:
+            if(ch not in imap):
+                return -1
             
+            idx = get_idx(imap[ch], curr_idx)
+            steps += (curr_idx >= idx)
+            curr_idx = idx
+        
+        return steps
             
