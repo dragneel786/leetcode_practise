35b12c348c981aedc22d60ea9bc1540157b87132
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-13 18:35:12 +0530

    Time: 39 ms (43.16%), Space: 13.8 MB (99.68%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -14,10 +14,7 @@ class Solution:
             if(node1.val != node2.val):
                 return False
             
-            left = is_simp(node1.left, node2.right)
-            if(not left):
-                return False
-            
-            return is_simp(node1.right, node2.left)            
+            return is_simp(node1.left, node2.right)\
+        and is_simp(node1.right, node2.left)            
         
         return is_simp(root.left, root.right)
