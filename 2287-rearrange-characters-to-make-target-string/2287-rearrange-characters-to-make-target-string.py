d66896de2a8cb2a01dd96939789c20c73bfa37e7
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-18 18:20:04 +0530

    Time: 36 ms (38.32%), Space: 13.9 MB (55.16%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -3,9 +3,6 @@ class Solution:
         sc = Counter(s)
         ans = inf
         for k, v in Counter(target).items():
-            if((sc[k] // v) == 0):
-                return 0
-            
             ans = min(ans, sc[k] // v)
         
-        return ans if(ans != inf) else 0
+        return ans
