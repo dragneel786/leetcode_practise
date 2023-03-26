c1d9c4cf4c1320899f15121af89fe6b1b13c052b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-26 11:57:51 +0530

    Time: 1493 ms (43.30%), Space: 138.2 MB (54.21%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -2,21 +2,22 @@ class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         
         def longest(node, before, steps = 0):
+            nonlocal ans
             if(node == -1):
                 return 
 
             if(node in before):
-                visited[node] = steps - before[node]
+                ans = max(ans, steps - before[node])
                 return
 
             before[node] = steps
             longest(edges[node], before, steps + 1)
+            edges[node] = -1
 
-        visited = dict()
+        ans = -1
         for v in range(len(edges)):
             longest(v, dict())
-            edges[v] = -1
 
-        return max(visited.values()) if (visited) else -1
+        return ans
 
             
