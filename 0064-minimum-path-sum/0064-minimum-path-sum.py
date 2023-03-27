46c7b65c0d1db1a52a2e60d634001f8b2b0f2ee9
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-03-27 21:52:42 +0530

    Time: 104 ms (47.72%), Space: 15.9 MB (26.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
-        @lru_cache(None)
-        def min_sum(r, c):
-            nonlocal rows, cols
-            if(r == rows - 1 and c == cols - 1):
-                return grid[r][c]
-            
-            if(r >= rows or c >= cols):
-                return inf
-            
-            return grid[r][c] + min(min_sum(r + 1, c),\
-                                    min_sum(r, c + 1))
-        
+        val = lambda x, y: grid[x][y] \
+        if(x < rows and y < cols) else inf
         rows, cols = len(grid), len(grid[0])
-        return min_sum(0, 0)
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                if(r == rows - 1 and c == cols - 1):
+                    continue
+                    
+                grid[r][c] += min(val(r + 1, c),\
+                                 val(r, c + 1))
+        
+        return grid[0][0]
+                
+                           
+        
