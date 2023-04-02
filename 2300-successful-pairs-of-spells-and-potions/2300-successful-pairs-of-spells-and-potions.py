cb1bf4e0553c72a8cc33a4c2ce4fc4430c995a64
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-04-02 13:12:31 +0530

    Time: 1320 ms (75.35%), Space: 44.1 MB (5.38%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,24 +1,22 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         
-        def bin_search(s):
-            nonlocal success, n
-            low = 0
-            high = n
-            while(low < high):
-                mid = (high - low) // 2 + low
-                if(potions[mid] * s < success): low = mid + 1
-                else:
-                    high = mid
-            
-            return low
+        spell_index = [(s, i) for i, s in enumerate(spells)]
         
-        m, n = len(spells), len(potions)
+        spell_index.sort()
         potions.sort()
-        ans = []
-        for s in spells:
-            ans.append(n - bin_search(s))
+        ans = [0] * len(spells)
+        n = len(potions)
+
+        potion_index = len(potions) - 1
+        for spell, index in spell_index:
+            while(potion_index >= 0 and \
+                    (spell * potions[potion_index]) >= success):
+
+                potion_index -= 1
+                
+            ans[index] = (n - potion_index - 1)
         
         return ans
-        
+            
             
