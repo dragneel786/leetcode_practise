cf384048f484cef2a02230a4e51111548c949bd5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-04-07 21:41:22 +0530

    Time: 728 ms (49.95%), Space: 75.9 MB (57.02%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,81 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        
+        def dfs(r, c):
+            nonlocal rows, cols, count
+            if(r < 0 or r >= rows or\
+               c < 0 or c >= cols):
+                count = None
+                return
+            
+            if(not grid[r][c]):
+                return
+            
+            grid[r][c] = 0
+            if(count is not None):
+                count += 1
+            
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+
+        
+        
+        rows, cols = len(grid), len(grid[0])
+        ans = count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if(grid[r][c]):
+                    count = 0
+                    dfs(r, c)
+                    if(count):
+                        ans += count
+        
+        return ans
+            
+            
+        
+        
+        
+#         def union(x, y):
+#             px, py = find(x), find(y)
+#             parent[max(px, py)] = min(px, py)
+        
+        
+#         def find(x):
+#             if(parent[x] != x):
+#                 parent[x] = find(parent[x])
+#             return parent[x]
+        
+        
+#         parent = {-1:-1}
+#         rows, cols = len(grid), len(grid[0])
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if(grid[r][c]):
+#                     cell = (r * rows) + c
+#                     parent[cell] = cell
+#                     if(r == 0 or r == rows - 1 or \
+#                        c == 0 or c == cols - 1):
+#                         parent[cell] = -1
+
+#                     nr, nc = r - 1, c - 1
+#                     if(nr >= 0 and grid[nr][c]):
+#                         new_cell = (nr * rows) + c
+#                         union(new_cell, cell)
+
+#                     if(nc >= 0 and grid[r][nc]):
+#                         new_cell = (r * rows) + nc
+#                         union(new_cell, cell)
+        
+#         counts = 0
+#         for k in parent.keys():
+#             if find(k) != -1:
+#                 print(k, parent[k])
+#             counts += (find(k) != -1)
+        
+#         return counts
+                
+                
+        
