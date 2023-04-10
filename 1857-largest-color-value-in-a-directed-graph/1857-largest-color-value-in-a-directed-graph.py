281d01ef9f85a0f2c2c8c569ce16060c38c9a00d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-04-10 23:39:59 +0530

    Time: 3016 ms (24.84%), Space: 84.9 MB (77.58%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -1,24 +1,41 @@
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        n, k = len(colors), 26
-        indegrees = [0] * n
-        graph = [[] for _ in range(n)]
-        for u, v in edges:
-          graph[u].append(v)
-          indegrees[v] += 1
-        zero_indegree = set(i for i in range(n) if indegrees[i] == 0)
-        counts = [[0] * k for _ in range(n)]
-        for i, c in enumerate(colors):
-          counts[i][ord(c) - ord('a')] += 1
-        max_count, visited = 0, 0
-        while zero_indegree:
-          u = zero_indegree.pop()
-          visited += 1
-          for v in graph[u]:
-              for i in range(k):
-                  counts[v][i] = max(counts[v][i], counts[u][i] + (ord(colors[v]) - ord('a') == i))
-              indegrees[v] -= 1
-              if indegrees[v] == 0:
-                  zero_indegree.add(v)
-          max_count = max(max_count, max(counts[u]))
-        return max_count if visited == n else -1
+        def create_graph():
+            for a, b in edges:
+                graph[a].append(b)
+                indegree[b] += 1
+        
+        graph = defaultdict(list)
+        indegree = Counter()
+        create_graph()
+        
+        n = len(colors)
+        q = deque()
+        for i in range(n):
+            if(not indegree[i]):
+                q.append(i)
+        
+        seen = 0
+        max_color = 0
+        color_counts = [([0] * 26) for _ in range(n)]
+        while(q):
+            node = q.popleft()
+            color_idx = ord(colors[node]) - 97
+            color_counts[node][color_idx] += 1
+            max_color = max(max_color, color_counts[node][color_idx])
+            
+            for v in graph[node]:
+                for i in range(26):
+                    color_counts[v][i] = max(color_counts[v][i],\
+                                             color_counts[node][i])
+                    
+                indegree[v] -= 1
+                if(indegree[v] <= 0):
+                    q.append(v)
+
+            
+            seen += 1
+        
+        return max_color if(seen >= n) else -1
+            
+            
