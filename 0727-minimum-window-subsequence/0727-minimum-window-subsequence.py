d438785a9fe53a4876c1bca27c918f1927e4e0ae
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-04-22 13:55:24 +0530

    Time: 2570 ms (19.34%), Space: 51 MB (18.23%) - LeetHub

--- 0727-minimum-window-subsequence/0727-minimum-window-subsequence.py
+++ 0727-minimum-window-subsequence/0727-minimum-window-subsequence.py
@@ -7,21 +7,19 @@ class Solution:
         for j in range(1, n + 1):
             dp[0][j] = inf
         
+        min_len = inf
+        pos = -1
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if(s1[i - 1] == s2[j - 1]):
                     dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
                     dp[i][j] = dp[i - 1][j] + 1
-         
-        min_len = inf
-        pos = -1
-        for i in range(1, m + 1):
             
             if(dp[i][n] < min_len):
                 min_len = dp[i][n]
                 pos = i - 1
-        
+         
         if(min_len == inf):
             return ""
         
