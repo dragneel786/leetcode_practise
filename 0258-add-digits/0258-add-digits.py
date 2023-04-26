a3fc4b0de136a8dae398790175b84361496902d9
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-04-26 20:16:27 +0530

    Time: 43 ms (6.65%), Space: 13.9 MB (39.79%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,11 +1,9 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while(num >= 10):
-            temp = 0
-            while(num):
-                temp += num % 10
-                num //= 10
-            
-            num = temp
+        if not num:
+            return 0
         
-        return num
+        if num % 9 == 0:
+            return 9
+        
+        return num % 9
