ffe7793ca940cc4bf91f7a55157897a9ad37f6d7
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-01 19:34:31 +0530

    Time: 56 ms (25.79%), Space: 16.5 MB (5.03%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -1,28 +1,33 @@
 class Solution:
     def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
         
-        def create_graph():
-            g = defaultdict(list)
-            for i, c in enumerate(text):
-                g[c].append(i)
+        def create_trie():
+            t = dict()
+            for word in words:
+                temp = t
+                for c in word:
+                    temp[c] = temp.get(c, {})
+                    temp = temp[c]
+                
+                temp['$'] = None
             
-            return g
+            return t
         
-        
-        def dfs(i, end = -1):
-            nonlocal word
-            if(i == len(word)):
-                ans.append([end - len(word) + 1, end])
-                return 
-        
-            for idx in graph[word[i]]:
-                if(end == -1 or end + 1 == idx):
-                    dfs(i + 1, idx)
-            
-        
-        graph = create_graph()
+        trie = create_trie()
         ans = []
-        for word in words:
-            dfs(0)
+        for i in range(len(text)):
+            temp = trie
+            for j in range(i, len(text)):
+                if(text[j] not in temp):
+                    break
+                
+                temp = temp[text[j]]
+                if('$' in temp):
+                    ans.append([i, j])
+                
+    
+        return ans
+                
+                    
         
-        return sorted(ans)
+                    
