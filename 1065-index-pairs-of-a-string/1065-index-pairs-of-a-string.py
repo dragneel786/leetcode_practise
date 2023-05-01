0dafea30bb23d06951eadf12de861e3b1a641e26
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-01 19:18:38 +0530

    Time: 455 ms (5.03%), Space: 16.5 MB (5.03%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -1,28 +1,27 @@
 class Solution:
     def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
         
-        def word_hash(words):
-            word_dict = defaultdict(list)
-            for i, word in enumerate(words):
-                word_dict[word[0]].append(i)
-            return word_dict
-        
-        
-        hash_word = word_hash(words)
-        curr = deque()
-        res = []
-        for i, c in enumerate(text + '-'):
-            n = len(curr)
-            for _ in range(n):
-                ci, wi, idx = curr.popleft()
+        def create_graph():
+            g = defaultdict(list)
+            for i, c in enumerate(text):
+                g[c].append(i)
             
-                if(idx == len(words[wi])):
-                    res.append([ci, ci + idx - 1])
-                elif(words[wi][idx] == c):
-                    curr.append((ci, wi, idx + 1))
-            
-            for wi in hash_word[c]:
-                curr.append((i, wi, 1))
+            return g
         
-        return sorted(res)
-                    
+        
+        def dfs(word, start = -1, end = -1):
+            if(not word):
+                ans.append([start, end])
+                return 
+        
+            for idx in graph[word[0]]:
+                if(end == -1 or end + 1 == idx):
+                    dfs(word[1:], start if(start != -1) else idx, idx)
+            
+        
+        graph = create_graph()
+        ans = []
+        for word in words:
+            dfs(word)
+        
+        return sorted(ans)
