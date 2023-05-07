e82a078c8f6dfe33993e573392138f66629a5efa
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-07 19:46:01 +0530

    Time: 193 ms (87.56%), Space: 19.3 MB (9.41%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        
+        def dfs(r, c):
+            nonlocal rows, cols
+            grid[r][c] = 0
+            ret = ''
+            for dr, dc, ch in [(0, 1, 'r'), (1, 0, 'l'),\
+                               (0, -1, 'u'), (-1, 0, 'd')]:
+                nr, nc = r + dr, c + dc
+                if(0 <= nr < rows and\
+                   0 <= nc < cols and\
+                   grid[nr][nc]):
+                    ret += ch + dfs(nr, nc) + '|'
+            
+            return ret
+        
+        
+        rows, cols = len(grid), len(grid[0])
+        iset = set()
+        for r in range(rows):
+            for c in range(cols):
+                if(grid[r][c]):
+                    island = 'c' + dfs(r, c)
+                    iset.add(island)
+        
+        return len(iset)
+        
+        
+                
+                
+        
