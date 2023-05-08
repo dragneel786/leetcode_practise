13165b533d73e7479243a85ad577c7aff456545f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-08 19:39:44 +0530

    Time: 4507 ms (17.02%), Space: 16.3 MB (21.53%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -2,7 +2,9 @@ class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         
         n = len(digits)
-        ans = set()
+        ans = []
+        digits.sort()
+        present = set()
         for i in range(n):
             if(digits[i] == 0):
                 continue
@@ -15,10 +17,13 @@ class Solution:
                     if(k == i or k == j\
                        or digits[k] % 2 != 0):
                         continue
-                        
-                    ans.add(digits[i] * 100\
-                            + digits[j] * 10\
-                            + digits[k])
+                    
+                    val = digits[i] * 100 + digits[j] * 10\
+                        + digits[k]
+                    
+                    if(val not in present):
+                        ans.append(val)
+                        present.add(val)
 
-        return sorted(ans)
+        return ans
         
