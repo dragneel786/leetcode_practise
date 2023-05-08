8974a0171e1492ecdceadfca46187812b86ef4d1
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-08 19:35:12 +0530

    Time: 4789 ms (16.67%), Space: 16.3 MB (21.53%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -4,32 +4,21 @@ class Solution:
         n = len(digits)
         ans = set()
         for i in range(n):
-            if(digits[i] != 0):
-                for j in range(n):
-                    if(i != j):
-                        for k in range(n):
-                            if(k != i and k != j\
-                               and digits[k] % 2 == 0):
-                                ans.add(digits[i] * 100\
-                                        + digits[j] * 10\
-                                        + digits[k])
-        
-#         def elements(num = 0, picked = set()):
-#             if(num >= 100):
-# [1,8,7,7,1,1,5,4,0,0,7,5,1,7,9]
-#                 if(num % 2 == 0):
-#                     ans.add(num)
-#                 return
-            
-#             for i in range(len(digits)):
-#                 if(i not in picked):
-#                     if(digits[i] or (not digits[i] and num)):
-#                         picked.add(i)
-#                         elements(num * 10 + digits[i])
-#                         picked.remove(i)
-            
-        
-#         ans = set()
-#         elements(0)
+            if(digits[i] == 0):
+                continue
+                
+            for j in range(n):
+                if(i == j):
+                    continue
+                    
+                for k in range(n):
+                    if(k == i or k == j\
+                       or digits[k] % 2 != 0):
+                        continue
+                        
+                    ans.add(digits[i] * 100\
+                            + digits[j] * 10\
+                            + digits[k])
+
         return sorted(ans)
         
