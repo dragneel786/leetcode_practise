6fd37239f6a829956fb22aa27403a72d05eadd69
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-08 18:42:58 +0530

    Time: 116 ms (27.63%), Space: 16.5 MB (11.57%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -3,9 +3,7 @@ class Solution:
         size = len(mat)
         sums = 0 
         for i in range(size):
-            if(size % 2 == 0 or i != (size // 2)):
-                sums += mat[i][i]
-            sums += mat[i][size - i - 1]
+            sums += mat[i][i] + mat[i][size - i - 1]
         
-        return sums
+        return sums - (mat[size//2][size//2] if(size % 2) else 0)
         
