48a425c81c9cb01f6ded81e7c74f5eec86594ee8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-08 19:12:43 +0530

    Time: 78 ms (39.71%), Space: 16.6 MB (21.64%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -4,9 +4,9 @@ class Solution:
         r2, c2 = len(mat2), len(mat2[0])
         ans = [([0] * c2) for _ in range(r1)]
         for i in range(r1):
-            for j in range(c2):
-                
-                for k in range(c1):
-                    ans[i][j] += (mat1[i][k] * mat2[k][j])
+            for k in range(c1):
+                if(mat1[i][k]):
+                    for j in range(c2):
+                        ans[i][j] += (mat1[i][k] * mat2[k][j])
         
         return ans
