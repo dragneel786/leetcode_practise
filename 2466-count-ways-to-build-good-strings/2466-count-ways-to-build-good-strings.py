c44f7912ecdf994ab2300605bdfe6bfbeb0cc5ec
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-13 13:17:05 +0530

    Time: 175 ms (97.64%), Space: 20.2 MB (73.23%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,24 +1,24 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         
-        @lru_cache(None)
-        def const_string(size):
-            nonlocal low, high, zero, one
-            if(size > high):
-                return 0
+#         @lru_cache(None)
+#         def const_string(size):
+#             nonlocal low, high, zero, one
+#             if(size > high):
+#                 return 0
             
-            return ((size >= low) + const_string(size + zero) +\
-                    const_string(size + one)) % MOD
+#             return (1 + const_string(size + zero) +\
+#                     const_string(size + one)) % MOD
         
-        MOD = 10 ** 9 + 7
-        return (const_string(zero) +\
-                const_string(one)) % MOD
-
 #         MOD = 10 ** 9 + 7
-#         dp = [0] * (high + max(zero, one) + 1)
-#         for i in range(high, low - 1, -1):
-#             dp[i] = (dp[i + zero] + dp[i + one] + 1) % MOD
+#         return (const_string(zero) +\
+#                 const_string(one)) % MOD
+
+        MOD = 10 ** 9 + 7
+        dp = [0] * (high + max(one, zero) + 1)
+        for i in range(high, -1, -1):
+            dp[i] = (dp[i + zero] + dp[i + one] + (i >= low)) % MOD
         
-#         print(dp)
-#         return dp[low]
+        return dp[0]
+        
         
