8751450671eedf006594ece9d4d20bb3816d64a0
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-13 13:09:24 +0530

    Time: 321 ms (55.12%), Space: 182.8 MB (37.01%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -2,14 +2,23 @@ class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         
         @lru_cache(None)
-        def const_string(size = 0):
+        def const_string(size):
             nonlocal low, high, zero, one
             if(size > high):
                 return 0
             
-            return ((size >= low) +\
-                    const_string(size + zero) +\
+            return ((size >= low) + const_string(size + zero) +\
                     const_string(size + one)) % MOD
         
         MOD = 10 ** 9 + 7
-        return const_string() % MOD
+        return (const_string(zero) +\
+                const_string(one)) % MOD
+
+#         MOD = 10 ** 9 + 7
+#         dp = [0] * (high + max(zero, one) + 1)
+#         for i in range(high, low - 1, -1):
+#             dp[i] = (dp[i + zero] + dp[i + one] + 1) % MOD
+        
+#         print(dp)
+#         return dp[low]
+        
