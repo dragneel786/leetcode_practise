ee2babd2a1af599943e189bba40c507fc8923a1f
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-21 11:59:07 +0530

    Time: 58 ms (11.83%), Space: 16.3 MB (37.40%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.py
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.py
@@ -4,10 +4,12 @@ class Solution:
         ans = []
         s += '-'
         for i in range(1, len(s)):
-            if(s[start] != s[i]):
-                if(i - start >= 3):
-                    ans.append([start, i - 1])
-                start = i
+            if(s[start] == s[i]):
+                continue
+                
+            if(i - start >= 3):
+                ans.append([start, i - 1])
+            start = i
         
         return ans
                 
