bfc49d1ba3e8390e2cce22ad8be58fccbcd299a7
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-05-22 19:16:43 +0530

    Time: 141 ms (8.99%), Space: 21.1 MB (30.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        bucket = [[] for _ in range(10 ** 5)]
+        bucket = [[] for _ in range(10 ** 4)]
         for v, c in Counter(nums).items():
             bucket[c].append(v)
         
         ans = []
-        for i in range(10 ** 5 - 1, -1, -1):
+        for i in range(10 ** 4 - 1, -1, -1):
             for v in bucket[i]:
                 ans.append(v)
                 
