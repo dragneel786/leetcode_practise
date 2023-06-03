f6cee795e1fab2ebf75e1d975f500ee93bf96aab
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-06-03 16:01:12 +0530

    Time: 1107 ms (97.71%), Space: 59.7 MB (12.54%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,23 +1,23 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        mang = defaultdict(lambda:[])
-        for i in range(n):
-            mang[manager[i]].append(i)
-
-        st = deque()
-        st.append([headID, 0])
-        maxV = float('-inf')
-        while(len(st)):
-            for _ in range(len(st)):
-                m = st.pop()
-                t = informTime[m[0]]
-                if(not t):
-                    maxV = max(maxV, m[1])
-                    continue
-
-                for c in mang[m[0]]:
-                    st.appendleft([c, m[1] + t])
-
-        return maxV
-                        
         
+        def populate(src):
+            if(time[src] != -1):
+                return time[src]
+            
+            time[src] = populate(manager[src])\
+            + informTime[manager[src]]
+            return time[src]
+        
+        time = [-1] * n
+        time[headID] = 0
+        for i in range(n):
+            time[i] = populate(i)
+        
+        return max(time)
+        
+                
+                
+                
+        
+    
