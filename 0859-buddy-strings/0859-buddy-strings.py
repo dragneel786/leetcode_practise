2743624fe9dca6b2ab2119ec49e8c61ee592833a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-07-03 20:16:55 +0530

    Time: 55 ms (28.98%), Space: 16.7 MB (6.43%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -1,19 +1,29 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        if(len(s) != len(goal)):
+        m, n = len(s), len(goal)
+        if(m != n):
             return False
         
-        if(s == goal):
-            return len(s) > 26 or\
-        len(set(s)) != len(s)
+        a, x = '1', '1'
+        b, y = '0', '0'
+        counts = Counter()
+        for c, d in zip(s, goal):
+            if(c == d):
+                counts[c] += 1
+                continue
+                
+            if(a == '1'):
+                a, x = c, d
+            
+            elif(b == '0'):
+                y, b = c, d
+            
+            else:
+                return False
         
-        diff = dict()
-        for a, b in zip(s, goal):
-            if(a != b):
-                if(a in diff or len(diff) >= 2):
-                    return False
+        if(a != '1'):
+            return (a == b and x == y)
+        
+        return len(list(filter(lambda v:v>=2, counts.values())))
                 
-                diff[a] = b
                 
-        return sorted(diff.keys()) == sorted(diff.values())
-    
