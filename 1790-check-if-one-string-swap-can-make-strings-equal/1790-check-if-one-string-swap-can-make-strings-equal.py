8c7429d8af1b8b44bfcf9b8b80234312bf71f098
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-07-07 22:53:58 +0530

    Time: 48 ms (52.29%), Space: 16.2 MB (74.34%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,10 +1,10 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
         diff = 0
-        imap = [0] * 26
+        counts = Counter()
         for a, b in zip(s1, s2):
-            imap[ord(a) - 97] += 1
-            imap[ord(b) - 97] -= 1
+            counts[a] += 1
+            counts[b] -= 1
             diff += a != b
         
-        return diff < 3 and not any(imap)
+        return diff < 3 and not any(counts.values())
