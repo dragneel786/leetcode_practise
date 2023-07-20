069529c117bf9d81e1a659068a23cd1c1327e0bb
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-07-20 21:57:16 +0530

    Time: 109 ms (71.15%), Space: 17.6 MB (57.73%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -2,28 +2,21 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         st = deque()
         for ast in asteroids:
+            if(ast > 0):
+                st.append(ast)
+                continue
+                
+            while(st and st[-1] > 0 and \
+                  abs(st[-1]) < abs(ast)):
+                st.pop()
+                
             if(not st):
                 st.append(ast)
-                
+            
             elif(st[-1] * ast > 0):
                 st.append(ast)
-                
-            else:
-                same = False
-                while(st and st[-1] > 0 and ast < 0):
-                    if(abs(st[-1]) < abs(ast)):
-                        st.pop()
-                    
-                    else:
-                        if(abs(st[-1]) == abs(ast)):
-                            st.pop()
-                            same = True
-                        break
             
-                if(not same and not st):
-                    st.append(ast)
-                
-                elif(not same and st and st[-1] < 0):
-                    st.append(ast)
+            elif(abs(st[-1]) == abs(ast)):
+                st.pop()
         
         return st
