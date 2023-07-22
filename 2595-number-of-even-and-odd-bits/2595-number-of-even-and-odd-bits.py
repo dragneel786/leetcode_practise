2e68cc386737207223575cc1db012919d2780a9a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-07-22 12:45:01 +0530

    Time: 54 ms (32.64%), Space: 16.4 MB (31.94%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
@@ -3,15 +3,17 @@ class Solution:
         even = 0
         odd = 0
         switch = False
+        n <<= 1
         while(n):
-            if(n % 2 == 1):
-                if(switch):
-                    odd += 1
-                else:
-                    even += 1
-            
             switch = not switch
             n >>= 1
+            if(n % 2 == 0):
+                continue
+                
+            if(switch):
+                even += 1
+            else:
+                odd += 1
         
         return [even, odd]
                 
