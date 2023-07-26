e407eb357b4e48a963c9faaf66937a04674e67e8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-07-26 21:55:56 +0530

    Time: 52 ms (63.81%), Space: 16.4 MB (60.48%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.py
@@ -1,25 +1,23 @@
 class Solution:
     def captureForts(self, forts: List[int]) -> int:
-        
-        def traverse(itr):
-            prev_i = -1
-            count = 0
-            ans = 0
-            for i in itr:
-                f = forts[i]
-                if(f != 0):
-                    if(f == -1):
-                        ans = max(count, ans)
-                    
-                    count = 0
-                    prev_i = f
-
-                elif(prev_i != -1):
-                    count += 1
+        index_one = -1
+        index_none = -1
+        ans = 0
+        for i, f in enumerate(forts):
+            if(f == 1):
+                if(index_none != -1):
+                    ans = max(ans, i - index_none - 1)
+                
+                index_one = i
+                index_none = -1
             
-            return ans
+            elif(f == -1):
+                if(index_one != -1):
+                    ans = max(ans, i - index_one - 1)
+                
+                index_none = i
+                index_one = -1
         
-        n = len(forts)
-        return max(traverse(range(n)),\
-                   traverse(range(n - 1, -1,-1)))
+        return ans
+                
             
