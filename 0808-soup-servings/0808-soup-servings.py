d2c1b7b47e3c2279596cb99909b26acfe883131b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-07-29 19:03:18 +0530

    Time: 48 ms (86.79%), Space: 17.2 MB (34.91%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -2,7 +2,7 @@ class Solution:
     def soupServings(self, n: int) -> float:
         
         @lru_cache(None)
-        def empty_prob(a, b):
+        def serve(a, b):
             if(a <= 0 and b <= 0):
                 return 0.5
             
@@ -12,9 +12,9 @@ class Solution:
             if(b <= 0):
                 return 0
             
-            return 0.25 * (empty_prob(a - 100, b)\
-                           + empty_prob(a - 75, b - 25)\
-                           + empty_prob(a - 50, b - 50)\
-                           + empty_prob(a - 25, b - 75))
+            return 0.25 * (serve(a - 100, b) \
+            + serve(a - 75, b - 25)\
+            + serve(a - 50, b - 50)\
+            + serve(a - 25, b - 75))
         
-        return 1 if(n >= 4800) else empty_prob(n, n)
+        return 1 if(n > 4800) else serve(n, n)
