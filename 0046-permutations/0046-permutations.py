795e93baeef96f018e027a60ac417481722d2466
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-08-02 19:42:16 +0530

    Time: 46 ms (91.49%), Space: 16.5 MB (72.51%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,6 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        def perm(nums, curr = []):
-            if not nums:
-                return [curr[:]]
-            
-            ret = []
-            for i in range(len(nums)):
-                ret.extend(perm(nums[:i] + nums[i + 1:],\
-                                curr + [nums[i]]))
-            
-            return ret
-        
-        return perm(nums)
+        return list(permutations(nums, len(nums)))
         
                 
             
