fdb8a4f8cd9d1638b0b4e61cbb29bbb581c8c431
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-08-25 19:31:27 +0530

    Time: 101 ms (78.02%), Space: 16.1 MB (91.81%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -1,9 +1,12 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        if(not num1 or not num2):
-            return 0
+        count = 0
+        while(num1 > 0 and num2 > 0):
+            if(num1 > num2):
+                num1, num2 = num1 - num2, num2
+            else:
+                num1, num2 = num2 - num1, num1
+            
+            count += 1
         
-        if(num1 > num2):
-            return 1 + self.countOperations(num1 - num2, num2)
-        else:
-            return 1 + self.countOperations(num2 - num1, num1)
+        return count
