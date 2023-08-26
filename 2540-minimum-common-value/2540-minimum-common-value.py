65e80fbeac30184cc42d31ae3208027c85fcdc22
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-08-26 12:18:00 +0530

    Time: 422 ms (59.65%), Space: 39.5 MB (29.47%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,9 +1,9 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        len2 = len(nums2)
+        counts = Counter(nums2)
         for num in nums1:
             idx = bisect_left(nums2, num)
-            if(idx < len2 and nums2[idx] == num):
+            if(num in counts):
                 return num
         
         return -1
