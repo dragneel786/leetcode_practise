1893553ad584aa56910bbb2bc5054855f85186b5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-08-30 23:06:13 +0530

    Time: 496 ms (63.64%), Space: 30.3 MB (81.82%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -1,21 +1,13 @@
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
-        answer = 0
-        n = len(nums)
-
-        # Start from the second last element, as the last one is always sorted.
-        for i in range(n - 2, -1, -1):
-            # No need to break if they are already in order.
-            if nums[i] <= nums[i + 1]:
-                continue
-            
-            # Count how many elements are made from breaking nums[i].
-            num_elements = (nums[i] + nums[i + 1] - 1) // nums[i + 1]
-            
-            # It requires numElements - 1 replacement operations.
-            answer += num_elements - 1
-
-            # Maximize nums[i] after replacement.
-            nums[i] = nums[i] // num_elements
-
-        return answer
+        ans = 0
+        prev = nums[-1]
+        for num in nums[:-1][::-1]:
+            if prev < num:
+                div = (num + prev - 1) // prev
+                ans += (div - 1)
+                prev = num // div
+            else:
+                prev = num
+        
+        return ans
