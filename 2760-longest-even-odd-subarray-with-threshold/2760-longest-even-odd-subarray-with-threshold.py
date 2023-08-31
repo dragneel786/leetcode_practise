c5c88b31acfcae32fd131d308087631b525b1744
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-08-31 23:16:45 +0530

    Time: 278 ms (98.01%), Space: 16.4 MB (18.62%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
@@ -2,24 +2,26 @@ class Solution:
     def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
         start = -1
         max_len = 0
-        is_even = True
+        even = True
         valid = lambda state, num: (state == (num % 2 == 0)\
                                     and num <= threshold)
         nums.append(nums[-1])
         
         for end, num in enumerate(nums):
-            if(valid(is_even, num)):
+            if(valid(even, num)):
                 if(start == -1):
                     start = end
+                even = not even
+                continue
                 
-                is_even = not is_even
-            else:
-                print(is_even, start, end, max_len)
-                if(start != -1):
-                    max_len = max(end - start, max_len)
-                is_even = False if(valid(True, num)) else True
-                start = end if(valid(True, num)) else -1
-                # print(is_even, start, end, max_len)
+            if(start != -1):
+                max_len = max(end - start, max_len)
+
+            even = True
+            start = -1
+            if(valid(True, num)):
+                even = False
+                start = end
                 
         return max_len
             
