1788450221ffaed9a38ba985d475c64485ec0742
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-09-05 22:30:53 +0530

    Time: 49 ms (37.18%), Space: 16.3 MB (60.44%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -1,28 +1,17 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-#         ans = deque()
-#         idx = 0
-#         n = len(part)
-#         for c in s + '-':
-#             if(idx == n):
-#                 for _ in range(n):
-#                     ans.pop()
-#                 idx = 0
-            
-#             if(ans and ans[-1] == part[idx]):
-#                 idx += 1
-#             else:
-#                 idx = 1 if(ans and ans[-1] == part[0]) else 0
-            
-#             ans.append(c)
-        
-#             print(ans, idx)
-#         return ''.join(ans)
-        n=len(part)
-        while part in s:
-            i=s.index(part)
-            s=s[:i]+s[i+n:]
-        return s
+        ans = []
+        idx = 0
+        n = len(part)
+        part = list(part)
+        for c in s:
+            ans.append(c)
+            size = len(ans)
+            if(size >= n and part == ans[size - n:]):
+                for _ in range(n):
+                    ans.pop()
+
+        return ''.join(ans)
                 
             
                 
