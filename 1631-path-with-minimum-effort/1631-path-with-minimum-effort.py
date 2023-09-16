914efe08969f1223d2c45a875d77cd374c7f287e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-09-16 17:02:20 +0530

    Time: 596 ms (73.12%), Space: 20.3 MB (12.76%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,33 +1,41 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        st = []
+        
+        def min_effort(sr, sc, visited = set()):
+            if(sr >= rows - 1 and sc >= cols - 1):
+                return 0
+            
+            min_path = inf
+            for dr, dc in [(0,1),(0,-1),(-1,0),(1,0)]:
+                nr, nc = sr + dr, sc + dc
+                if(0 <= nr < rows and 0 <= nc < cols\
+                   and (nr, nc) not in visited):
+                    visited.add((nr, nc))
+                    path = max(min_effort(nr, nc, visited),
+                               abs(heights[sr][sc] - heights[nr][nc]))
+                    min_path = min(min_path, path)
+                    visited.discard((nr, nc))
+            
+            return min_path
+                    
         rows, cols = len(heights), len(heights[0])
-        dist = [([math.inf] * cols) for _ in range(rows)]
-        st.append([0, [0, 0]])
-        while(len(st)):
-            node = heappop(st)
-            weight = node[0]
-            r = node[1][0]
-            c = node[1][1]
-            if(r == rows - 1 and c == cols - 1):
-                return weight
-            if(dist[r][c] < weight):
-                continue
-            for d in [[0,1], [0,-1], [1,0],[-1,0]]:
-                dr = r + d[0]
-                dc = c + d[1]
-                if(dr < 0 or dr >= rows or dc < 0 or dc >= cols):
-                    continue  
-                newWeight = max(weight, abs(heights[dr][dc] - heights[r][c]))
-                if(dist[dr][dc] <= newWeight):
-                    continue
-                dist[dr][dc] = newWeight
-                heappush(st, [newWeight, [dr, dc]])
-        return 0
-                
-                
+        heap = [(0,0,0)]
+        visited = defaultdict(lambda:inf)
+        visited[(0,0)] = 0
+        while(heap):
+            effort, sr, sc = heappop(heap)
+            if(sr >= rows - 1 and sc >= cols - 1):
+                return effort
             
-            
-            
-            
-            
+            for dr, dc in [(0,1),(0,-1),(-1,0),(1,0)]:
+                nr, nc = sr + dr, sc + dc
+                if(0 <= nr < rows and 0 <= nc < cols):
+                    diff = abs(heights[sr][sc] - heights[nr][nc])
+                    curr_effort = max(effort, diff)
+                    
+                    if(visited[(nr,nc)] > curr_effort):
+                        visited[(nr, nc)] = curr_effort
+                        heappush(heap, (curr_effort, nr, nc))
+        
+                    
+        
