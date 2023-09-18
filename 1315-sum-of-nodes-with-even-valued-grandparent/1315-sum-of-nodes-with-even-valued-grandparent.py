8ae5985159f4d31c4ad3ee7ef563f032c7e075ee
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-09-18 23:34:52 +0530

    Time: 77 ms (99.19%), Space: 19.9 MB (90.58%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -6,30 +6,16 @@
 #         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        
-        def add(node):
-            nonlocal sums
-            if not node:
-                return 
-            
-            if(node.left):
-                sums += node.left.val
-            
-            if(node.right):
-                sums += node.right.val
-        
-        def sumed(node):
+                
+        def sumed(node, p, gp):
             if(not node):
-                return
+                return 0
             
-            if(node.val % 2 == 0):
-                add(node.left)
-                add(node.right)
-            
-            sumed(node.left)
-            sumed(node.right)
+            res = 0
+            if(gp and gp.val % 2 == 0):
+                res = node.val
+                
+            return res + sumed(node.left, node, p) + sumed(node.right, node, p)
         
-        sums = 0
-        sumed(root)
-        return sums
+        return sumed(root, None, None)
             
