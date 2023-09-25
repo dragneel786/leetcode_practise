b662b9b251deab77f462315e70dd46414f888345
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-09-25 18:28:34 +0530

    Time: 43 ms (47.34%), Space: 16.2 MB (96.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,12 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        counter_s = Counter(s)
-        for c in t:
-            if(c not in counter_s):
-                return c
-            
-            counter_s[c] -= 1
-            if(not counter_s[c]):
-                del counter_s[c]
+        xor = 0
+        for c in s:
+            xor ^= ord(c)
         
-        return list(counter_s.keys())[0]
+        for c in t:
+            xor ^= ord(c)
+        
+        return chr(xor)
