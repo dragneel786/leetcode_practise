4c4ad4e01e3ee58f25c36c8b404fb3b374d097df
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-09-29 19:27:00 +0530

    Time: 385 ms (79.52%), Space: 26.1 MB (74.10%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.py
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.py
@@ -3,10 +3,11 @@ class Solution:
         ret = 0
         for num in nums:
             place = 0
-            while(num):
-                ret |= (num & 1) << place
-                num >>= 1
-                place += 1
+            ret |= num
+            # while(num):
+            #     ret |= (num & 1) << place
+            #     num >>= 1
+            #     place += 1
         
         return ret
             
