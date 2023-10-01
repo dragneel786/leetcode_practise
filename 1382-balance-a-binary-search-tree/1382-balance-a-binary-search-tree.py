70f3636f93db60e8ac2a734bd100ed15d39d584b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-10-01 14:57:13 +0530

    Time: 290 ms (18.74%), Space: 23.6 MB (23.27%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        
+        def preorder(node):
+            if(not node):
+                return 
+            
+            preorder(node.left)
+            ordered.append(node.val)
+            preorder(node.right)
+        
+        def construct(start, end):
+            if(start > end):
+                return None
+            
+            mid = (start + end) // 2
+            print(ordered[mid])
+            node = TreeNode(ordered[mid])
+            node.left = construct(start, mid - 1)
+            node.right = construct(mid + 1, end)
+            return node
+        
+        ordered = []
+        preorder(root)
+        print(ordered)
+        return construct(0, len(ordered) - 1)
+        
