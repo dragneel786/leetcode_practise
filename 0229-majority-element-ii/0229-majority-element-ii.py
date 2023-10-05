039be28df14f76f1bc60a6d4ec2460156f90d607
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-10-05 18:58:33 +0530

    Time: 105 ms (84.77%), Space: 17.7 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,11 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        res = set()
-        n = len(nums)
-        cmap = Counter()
+        cand1, cand2 = 0, 1
+        cnt1 = cnt2 = 0
         for num in nums:
-            cmap[num] += 1
-            if(cmap[num] > (n // 3)):
-                res.add(num)
+            if(cand1 == num):
+                cnt1 += 1
+            
+            elif(cand2 == num):
+                cnt2 += 1
+            
+            elif(cnt1 == 0):
+                cand1, cnt1 = num, 1
+            
+            elif(cnt2 == 0):
+                cand2, cnt2 = num, 1
+            
+            else:
+                cnt1, cnt2 = cnt1 - 1, cnt2 - 1
         
-        return res
+        return [num for num in (cand1, cand2) \
+               if nums.count(num) > (len(nums) // 3)]
+            
+        
