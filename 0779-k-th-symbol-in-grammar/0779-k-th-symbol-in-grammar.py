b7478d0a2456183b6f99d13880875c5d7c07fc07
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-10-25 19:08:16 +0530

    Time: 24 ms (99.87%), Space: 16 MB (97.42%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,7 +1,15 @@
 class Solution:
-    def kthGrammar(self, N: int, K: int) -> int:
-        if N == 1: return 0
-        val = (1 - K%2) ^ self.kthGrammar(N-1, (K+1)//2)
-        print(K, bin(1 - K%2), val)
-        return val
+    def kthGrammar(self, n: int, k: int) -> int:
+        ladder = [k]
+        while(k > 1):
+            k = (k + 1) // 2
+            ladder.append(k)
         
+        
+        curr = '0'
+        t = {'0': '01', '1':'10'}
+        for l in reversed(ladder):
+            curr = t[curr][(l - 1) % 2]
+        
+        return int(curr)
+            
