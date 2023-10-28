8cfe251a63c2ae19d7ef9f8c6d0c977d6416f220
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2023-10-28 13:34:06 +0530

    Time: 81 ms (94.31%), Space: 16.2 MB (86.67%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,26 +1,10 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        
-        options = {'a': ['e'], 'e':['a', 'i'],\
-                   'i':['a', 'e', 'o', 'u'],\
-                   'o':['i', 'u'], 'u':['a']}
-        
+
         MOD = (10 ** 9) + 7
+        a = e = i = o = u = 1
+        for _ in range(n - 1):
+            a, e, i, o, u = e, (a + i) % MOD, (a + e + o + u) % MOD,\
+            (i + u) % MOD, a
         
-        @cache
-        def counts(curr = '' , size = 0):
-            nonlocal MOD
-            if(size == n):
-                return 1
-            
-            tot = 0
-            op = ['a', 'e', 'i', 'o', 'u'] \
-            if(curr not in options) else options[curr]
-            
-            for c in op:
-                tot = (tot + counts(c, size + 1)) % MOD
-            
-            return tot
-        
-        return counts()
-                
+        return sum([a, e, i, o, u]) % MOD
