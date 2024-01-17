a80c56ef63ef95a3bc73363f6d35d5302bb3a765
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-01-17 18:48:01 +0530

    Delete 1207-unique-number-of-occurrences directory

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        s = set()
-        for c in Counter(arr).values():
-            if(c in s):
-                return False
-            s.add(c)
-        return True
