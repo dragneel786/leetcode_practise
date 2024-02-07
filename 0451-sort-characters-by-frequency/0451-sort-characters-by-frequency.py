1ee3794e1eca2a0e08abb7c7c88155291c7f987d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-02-07 21:37:22 +0530

    Delete 0451-sort-characters-by-frequency directory

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def frequencySort(self, s: str) -> str:
-        bucket = defaultdict(list)
-        for c, v in Counter(s).items():
-            bucket[v].append(c)
-            
-        return ''.join([k * a for k in range(len(s), 0, -1)\
-                        for a in bucket[k]])
-        
