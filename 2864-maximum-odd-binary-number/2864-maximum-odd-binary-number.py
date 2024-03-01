5a52fb38f3e492ee04182980631cf26bed2e03cd
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-03-01 20:26:41 +0530

    Delete 2864-maximum-odd-binary-number directory

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumOddBinaryNumber(self, s: str) -> str:
-        n = len(s)
-        ones = 0
-        for c in s:
-            ones += c == '1'
-        
-        return ((ones - 1) * '1') + ((n - ones) * '0') + '1'
