98070ffd4b229bac33f54318f0632e4e0f64d3b8
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-03-03 23:05:29 +0530

    Time: 19 ms (99.61%), Space: 16.4 MB (85.66%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -1,25 +1,15 @@
 class Solution:
     def printVertically(self, s: str) -> List[str]:
         words = s.split(" ")
-        i = 0
-        st = deque(["*"])
         res = []
-        while(len(st) > 0):
-            st.clear()
-            for word in words:
-                if(i < len(word)):
-                    st.append(word[i])
-                
-                else:
-                    st.append(' ')
+        for values in zip_longest(*words, fillvalue=" "):
+            values = list(values)
+            while(values[-1] == ' '):
+                values.pop()
             
-            while(st and st[-1] == " "):
-                st.pop()
-            
-            if(st):
-                res.append("".join(st))
-            i += 1
+            res.append("".join(values))
     
         return res
+            
                 
                 
