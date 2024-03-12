de405711d688057883be2df512abeb7a5e7f84c4
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-03-12 22:58:39 +0530

    Time: 39 ms (83.67%), Space: 16.9 MB (53.78%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_head = ListNode(0, head)
+        temp = head
+        curr_sum = 0
+        seen_so_far = set([0])
+        q = deque([(0, new_head)])
+        while(temp):
+            curr_sum += temp.val
+            # print(f'=={curr_sum}, {seen_so_far} {q}==')
+            if(curr_sum in seen_so_far):
+                while(q and q[-1][0] != curr_sum):
+                    val, _ = q.pop()
+                    seen_so_far.remove(val)
+                    
+                q[-1][1].next = temp.next
+            
+            else:
+                q.append((curr_sum, temp))
+                seen_so_far.add(curr_sum)
+            
+            
+                
+            temp = temp.next
+                
+        return new_head.next
