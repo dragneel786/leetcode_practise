948456b4345e375d5b5dab8424d953d4cce449b7
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-04-14 13:05:13 +0530

    Time: 435 ms (45.45%), Space: 35.5 MB (51.82%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -1,13 +1,9 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         m, n = len(nums1), len(nums2)
-        res = 0
-        for num in nums1:
-            res ^= 0 if(n % 2 == 0) else num
+        xor_1 = 0 if(n % 2 == 0) else reduce(lambda a, b: a ^ b, nums1)
+        xor_2 = 0 if(m % 2 == 0) else reduce(lambda a, b: a ^ b, nums2)
         
-        for num in nums2:
-            res ^= 0 if(m % 2 == 0) else num
-        
-        return res
+        return xor_1 ^ xor_2
             
             
