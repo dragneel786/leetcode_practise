b24653a1271be9395d7609c8c21d496d120d8f02
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-04-20 15:43:09 +0530

    Time: 149 ms (30.00%), Space: 33.1 MB (10.00%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.go
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.go
@@ -3,7 +3,7 @@ func findFarmland(land [][]int) [][]int {
     for r, hectareRows := range land {
         for c, hect := range hectareRows {
             if hect == 1 {
-                br, bc := landCoor(land, r, c)
+                br, bc := landCoor(&land, r, c)
                 res = append(res, []int{r, c, br, bc})
             }
         }
@@ -11,15 +11,15 @@ func findFarmland(land [][]int) [][]int {
     return res
 }
 
-func landCoor(land [][]int, sr, sc int) (int, int) {
+func landCoor(land *[][]int, sr, sc int) (int, int) {
     
-    rows, cols := len(land), len(land[0])
+    rows, cols := len(*land), len((*land)[0])
     maxR, maxC := sr, sc
     dir := []int{0, 1, 0, -1, 0}
-    land[sr][sc] = 0
+    (*land)[sr][sc] = 0
     for i := range dir[:len(dir) - 1] {
         nr, nc := sr + dir[i], sc + dir[i + 1]
-        if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && land[nr][nc] == 1){
+        if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && (*land)[nr][nc] == 1){
             
             mr, mc := landCoor(land, nr, nc)
             if(maxR < mr || maxC < mc){
