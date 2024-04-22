08f14d3680f8305cb6fd0638c4c558f755d352d4
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-04-22 21:17:55 +0530

    Time: 127 ms (31.87%), Space: 7.2 MB (56.04%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.go
+++ 0752-open-the-lock/0752-open-the-lock.go
@@ -1,68 +1,64 @@
-type Queue struct {
-    items []string
+type Queue []string
+
+func (q *Queue) Enqueue(val string) {
+	*q = append(*q, val)
 }
 
-func (que *Queue) Enque(val string) {
-    que.items = append(que.items, val)
-}
-
-func (que *Queue) Deque() string {
-    if len(que.items) == 0 {
-        return ""
-    }
-    
-    val := que.items[0]
-    que.items = que.items[1:]
-    return val
+func (q *Queue) Dequeue() string {
+	if len(*q) == 0 {
+		return ""
+	}
+	val := (*q)[0]
+	*q = (*q)[1:]
+	return val
 }
 
 func NewQueue() *Queue {
-    return &Queue{items: make([]string, 0)}
+	return &Queue{}
 }
 
 func openLock(deadends []string, target string) int {
-    hashSet := make(map[string]bool)
-    for _, dead := range deadends {
-        hashSet[dead] = true
-    }
-    
-    steps := 0
-    queue := NewQueue()
-    queue.Enque("0000")
-    for len(queue.items) > 0 {
-        for range queue.items {
-            combo := queue.Deque()
-            if hashSet[combo] {
-                continue
-            }
-            hashSet[combo] = true
-            if combo == target {
-                return steps
-            }
-            spinLock(combo, queue, hashSet)
-        }
-        steps += 1
-    }
-    
-    return -1
+	deadSet := make(map[string]bool)
+	for _, dead := range deadends {
+		deadSet[dead] = true
+	}
+
+	steps := 0
+	queue := NewQueue()
+	queue.Enqueue("0000")
+
+	for len(*queue) > 0 {
+		size := len(*queue)
+		for i := 0; i < size; i++ {
+			combo := queue.Dequeue()
+			if deadSet[combo] {
+				continue
+			}
+			if combo == target {
+				return steps
+			}
+            deadSet[combo] = true
+			spinLock(combo, queue, deadSet)
+		}
+		steps++
+	}
+	return -1
 }
 
-func spinLock(combo string, queue *Queue, hashSet map[string]bool){
-    for i, char := range combo {
-        num := int(char - '0')
-        spinBack := (num - 1)
-        spinFront := (num + 1) % 10
-        if spinBack < 0 {
-            spinBack = 9
-        }
-        str1 := combo[:i] + strconv.Itoa(int(spinBack)) + combo[i + 1:]
-        if hashSet[str1] == false {
-            queue.Enque(str1)
-        }
-        
-        str2 := combo[:i] + strconv.Itoa(int(spinFront)) + combo[i + 1:]
-        if hashSet[str2] == false{
-            queue.Enque(str2)
-        }
-    }
+func spinLock(combo string, queue *Queue, deadSet map[string]bool) {
+	for i, char := range combo {
+		num := int(char - '0')
+		spinBack := (num - 1 + 10) % 10
+		spinFront := (num + 1) % 10
+
+		str1 := combo[:i] + strconv.Itoa(spinBack) + combo[i+1:]
+		if !deadSet[str1] {
+			queue.Enqueue(str1)
+		}
+
+		str2 := combo[:i] + strconv.Itoa(spinFront) + combo[i+1:]
+		if !deadSet[str2] {
+			queue.Enqueue(str2)
+		}
+	}
 }
