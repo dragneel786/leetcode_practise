87df91dcf77e8389ff14475f4d8ddf1c4933f6f2
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-05-01 15:22:43 +0530

    Time: 165 ms (30.00%), Space: 40.6 MB (70.00%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.go
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.go
@@ -1,4 +1,18 @@
 func tupleSameProduct(nums []int) int {
+    var fact func(n int) int;
+    cache := make(map[int]int)
+    cache[1] = 1
+    cache[0] = 1
+    
+    fact = func(n int) int {
+        if _, ok := cache[n]; ok{
+            return cache[n]
+        }
+        cache[n] = n * fact(n - 1)
+        return cache[n]
+    }
+    
+    
     size := len(nums)
     productCounts := make(map[int]int)
     for i := range size {
@@ -17,11 +31,6 @@ func tupleSameProduct(nums []int) int {
             ans += int(fact(value) / (fact(2) * fact(value - 2))) * 8
         }
     }
+    
     return ans
-}
-
-func fact(n int) int {
-    res := 1
-    for i := 1; i < n + 1; i++ { res *= i }
-    return res
 }
