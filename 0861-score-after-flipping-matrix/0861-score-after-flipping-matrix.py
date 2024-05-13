6490c2561e06aabe01bb10589c0c904507bbd0e1
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-05-13 23:28:10 +0530

    Time: 48 ms (14.66%), Space: 16.5 MB (59.05%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -1,31 +1,11 @@
 class Solution:
-    def matrixScore(self, grid: List[List[int]]) -> int:
-        # Rows 
-        for g in grid:
-            if(g[0] == 1):
-                continue
-            for i, v in enumerate(g):
-                g[i] = 0 if(v) else 1
-                
-        
-        rows, cols = len(grid), len(grid[0])
-        for c in range(cols):
-            one_counts = zero_counts = 0
-            for r in range(rows):
-                one_counts += grid[r][c] == 1
-                zero_counts += grid[r][c] == 0
-
-            if(one_counts > zero_counts):
-                continue
-            
-            for r in range(rows):
-                grid[r][c] = 0 if(grid[r][c]) else 1
-        
-        ans = 0
-        for g in grid:
-            ans += int(''.join(map(str, g)), 2)
-        
-        return ans
+    def matrixScore(self, A: List[List[int]]) -> int:
+        M, N = len(A), len(A[0])
+        res = (1 << N - 1) * M
+        for j in range(1, N):
+            cur = sum(A[i][j] == A[i][0] for i in range(M))
+            res += max(cur, M - cur) * (1 << N - 1 - j)
+        return res
         
             
         
