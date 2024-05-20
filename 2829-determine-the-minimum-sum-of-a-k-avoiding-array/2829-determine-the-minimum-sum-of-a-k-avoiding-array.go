3fe714fc0943e1cf7f17ab6cc2ad60ed7aa79ddf
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-05-20 22:54:25 +0530

    Time: 0 ms (100.00%), Space: 5.3 MB (28.57%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.go
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.go
@@ -1,12 +1,12 @@
 func minimumSum(n int, k int) int {
-    var exists = make(map[int]bool)
+    var skip = make(map[int]bool)
     var sums, size, curr = 0, 0, 1
     for size < n {
         sums += curr
-		exists[curr] = true
+		skip[k - curr] = true
 		size++; curr++;
 
-        for exists[k - curr] {
+        for skip[curr] {
             curr++
         }
     }
