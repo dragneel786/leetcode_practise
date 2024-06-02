f078c6bacfce70735365ea20f00580c30b73baf1
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-06-03 00:01:51 +0530

    Time: 26 ms (80.00%), Space: 2.6 MB (60.00%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.go
@@ -0,0 +1,24 @@
+func maxUniqueSplit(s string) int {
+    var uniq = make(map[string]bool)
+    var splitsCounts func(start, end int) int;
+    
+    splitsCounts = func (start, end int) int {
+        if end >= len(s) {
+            if !uniq[s[start: end]] {
+                return 0
+            }
+            return 0
+        }
+        
+        var ret = splitsCounts(start, end + 1)
+        if !uniq[s[start : end + 1]] {
+            uniq[s[start : end + 1]] = true
+            
+            ret = max(ret, 1 + splitsCounts(end + 1, end + 1))
+            uniq[s[start : end + 1]] = false
+        }
+        return ret
+    }
+    
+    return splitsCounts(0, 0)
+}
