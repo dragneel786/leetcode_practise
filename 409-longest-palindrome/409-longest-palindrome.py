578b68821a4ecd94186c81dbfb24c988874a3d73
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-06-04 19:35:31 +0530

    Delete 409-longest-palindrome directory

--- 409-longest-palindrome/409-longest-palindrome.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        center = False
-        length = 0
-        for v in Counter(s).values():
-            center = center or (v & 1) == 1
-            length += v // 2
-        return (length * 2) + center
-                
