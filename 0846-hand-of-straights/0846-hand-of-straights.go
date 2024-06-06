058627af30db8c3d7edd30306f42ffc8745de582
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-06-06 22:59:20 +0530

    Time: 53 ms (18.92%), Space: 6.8 MB (94.59%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.go
@@ -0,0 +1,28 @@
+func isNStraightHand(hand []int, groupSize int) bool {
+    slices.Sort(hand)
+    var handCount = make(map[int]int)
+    for _, v := range hand {
+        handCount[v]++
+    }
+    
+    var index = 0
+    for index < len(hand) {
+        for index < len(hand) && handCount[hand[index]] == 0 {
+            index++
+        }
+
+        if index >= len(hand) {
+            break
+        }
+
+        for inc := range groupSize {
+            if handCount[hand[index] + inc] == 0 {
+                return false
+            }
+            
+            handCount[hand[index] + inc]--
+        }
+    }
+    
+    return true
+}
