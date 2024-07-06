30624a27c049ad49cd5a6f143464f07d56b00b0e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-07-06 15:32:05 +0530

    Delete 2582-pass-the-pillow directory

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def passThePillow(self, n: int, time: int) -> int:
-        dire = -1 if (time // (n - 1)) % 2 else 1
-        time = time % (n - 1)
-        # print(dire, time)
-        return (n if(dire < 0) else 1) + (dire * time)
-        
