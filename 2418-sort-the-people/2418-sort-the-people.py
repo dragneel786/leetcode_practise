a8b426e85ccb30a3591df0cd745fd17caae6537a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-07-22 18:03:28 +0530

    Delete 2418-sort-the-people directory

--- 2418-sort-the-people/2418-sort-the-people.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        nh = [(h, n) for n, h in zip(names, heights)]
-        return [n for _, n in sorted(nh, reverse=True)]
