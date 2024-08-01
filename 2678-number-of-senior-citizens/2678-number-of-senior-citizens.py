ffb37caa605efe84d792ad9b36e57abeabea17fe
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-08-01 18:09:01 +0530

    Delete 2678-number-of-senior-citizens directory

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countSeniors(self, details: List[str]) -> int:
-        pattern = r'[MFO]'
-        counts = 0
-        for detail in details:
-            counts += int(re.split(pattern, detail)[1][:2]) > 60
-        
-        return counts
