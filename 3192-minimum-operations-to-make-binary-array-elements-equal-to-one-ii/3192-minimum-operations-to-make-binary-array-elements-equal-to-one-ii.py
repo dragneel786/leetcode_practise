abd0533482a573650c0c71375e3cb6ac71c9fcd4
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-08-03 19:00:52 +0530

    Time: 1058 ms (81.04%), Space: 20.3 MB (33.79%) - LeetHub

--- 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.py
+++ 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.py
@@ -2,6 +2,6 @@ class Solution:
     def minOperations(self, A: List[int]) -> int:
         res = 0
         for a in A:
-            if a == res & 1:
+            if a == (res & 1):
                 res += 1
         return res
