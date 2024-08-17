cd2e4fac7c73b813e65df4b6799bac362293c4cd
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-08-17 16:08:33 +0530

    Delete 1768-merge-strings-alternately directory

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        empty = lambda a: a if(a) else ''
-        return ''.join([empty(a) + empty(b) \
-                for a, b in zip_longest(word1, word2)])
