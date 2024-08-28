681ae8799385d9a7aa0f2fde486609cb7fbac629
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-08-28 22:01:19 +0530

    Delete 2215-find-the-difference-of-two-arrays directory

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        s1, s2 = set(nums1), set(nums2)
-        return [[e for e in s1 if e not in s2],\
-                [e for e in s2 if e not in s1]]
