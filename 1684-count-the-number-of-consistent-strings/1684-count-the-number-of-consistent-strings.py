25a501da9df1c2fa89acb8b31dd32cca3d564b9e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-09-12 23:04:05 +0530

    Delete 1684-count-the-number-of-consistent-strings directory

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        s1 = set(allowed)
-        return sum([set(w).issubset(s1) for w in words])
-        
