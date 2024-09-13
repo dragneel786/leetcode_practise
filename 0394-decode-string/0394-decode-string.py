d596be83f71235dec1df036903fce466de326bb5
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-09-13 23:58:51 +0530

    Time: 33 ms (68.08%), Space: 16.6 MB (32.56%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        def pop_and_create():
+            temp = ''
+            while(st[-1] != '['):
+                temp = st.pop() + temp
+
+            st.pop()
+            st.append(int(st.pop()) * temp)
+
+        st = deque()
+        for c in s:
+            if(c == ']'):
+                pop_and_create()
+            else:
+                if(st and st[-1].isdigit() and c.isdigit()):
+                    st[-1] = st[-1] + c
+                else:
+                    st.append(c)
+
+        return ''.join(st)
+                        
+                        
+                
