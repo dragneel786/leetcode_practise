d00cba13f240e2f3af6d89d87eb91de4ae4add78
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-09-30 00:54:44 +0530

    Time: 154 ms (62.33%), Space: 33 MB (21.28%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,106 +1,111 @@
 class Node:
-    def __init__(self, freq):
-        self.freq = freq
-        self.prev = None
-        self.next = None
-        self.keys = set()
-
+    def __init__(self, key = "", val = 0, prev = None, nxt = None,):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.nxt = nxt
 
 class AllOne:
+
     def __init__(self):
-        self.head = Node(0)  # Dummy head
-        self.tail = Node(0)  # Dummy tail
-        self.head.next = self.tail  # Link dummy head to dummy tail
-        self.tail.prev = self.head  # Link dummy tail to dummy head
-        self.map = {}  # Mapping from key to its node
+        self.keys = dict()
+        self.head = self.tail = None
 
     def inc(self, key: str) -> None:
-        if key in self.map:
-            node = self.map[key]
-            freq = node.freq
-            node.keys.remove(key)  # Remove key from current node
-
-            nextNode = node.next
-            if nextNode == self.tail or nextNode.freq != freq + 1:
-                # Create a new node if next node does not exist or freq is not freq + 1
-                newNode = Node(freq + 1)
-                newNode.keys.add(key)
-                newNode.prev = node
-                newNode.next = nextNode
-                node.next = newNode
-                nextNode.prev = newNode
-                self.map[key] = newNode
+        self.keys.setdefault(key, Node(key))
+        node = self.keys[key]
+        node.val += 1
+        
+        if node.val > 1 and node.nxt and node.nxt.val < node.val:
+            nxt = node.nxt
+            prev = node.prev
+            nxt.prev = prev
+            
+            if self.head == node:
+                self.head = nxt
             else:
-                # Increment the existing next node
-                nextNode.keys.add(key)
-                self.map[key] = nextNode
-
-            # Remove the current node if it has no keys left
-            if not node.keys:
-                self.removeNode(node)
-        else:  # Key does not exist
-            firstNode = self.head.next
-            if firstNode == self.tail or firstNode.freq > 1:
-                # Create a new node
-                newNode = Node(1)
-                newNode.keys.add(key)
-                newNode.prev = self.head
-                newNode.next = firstNode
-                self.head.next = newNode
-                firstNode.prev = newNode
-                self.map[key] = newNode
+                prev.nxt = nxt
+                
+            
+            while(nxt is not None and nxt.val < node.val):
+                prev = nxt
+                nxt = nxt.nxt
+            
+            node.prev = prev
+            node.nxt = nxt
+            prev.nxt = node
+            if nxt: 
+                nxt.prev = node
             else:
-                firstNode.keys.add(key)
-                self.map[key] = firstNode
-
+                self.tail = node
+        
+        elif node.val == 1:
+            node.nxt = self.head
+            if self.head:
+                self.head.prev = node
+            else:
+                self.tail = node
+                
+            self.head = node
+        
+        # self.printItems()
+        
+    
     def dec(self, key: str) -> None:
-        if key not in self.map:
-            return  # Key does not exist
-
-        node = self.map[key]
-        node.keys.remove(key)
-        freq = node.freq
-
-        if freq == 1:
-            # Remove the key from the map if freq is 1
-            del self.map[key]
-        else:
-            prevNode = node.prev
-            if prevNode == self.head or prevNode.freq != freq - 1:
-                # Create a new node if the previous node does not exist or freq is not freq - 1
-                newNode = Node(freq - 1)
-                newNode.keys.add(key)
-                newNode.prev = prevNode
-                newNode.next = node
-                prevNode.next = newNode
-                node.prev = newNode
-                self.map[key] = newNode
+        self.keys.setdefault(key, Node())
+        node = self.keys[key]
+        node.val -= 1
+        
+        nxt, prev = node.nxt, node.prev
+        
+        if node.val >= 1 and node.prev and node.prev.val > node.val:
+            prev.nxt = nxt
+            if self.tail == node:
+                self.tail = prev
             else:
-                # Decrement the existing previous node
-                prevNode.keys.add(key)
-                self.map[key] = prevNode
-
-        # Remove the node if it has no keys left
-        if not node.keys:
-            self.removeNode(node)
+                nxt.prev = prev
+                
+            while(prev is not None and prev.val > node.val):
+                nxt = prev
+                prev = prev.prev
+            
+            node.prev = prev
+            node.nxt = nxt
+            nxt.prev = node
+            if prev: prev.nxt = node
+        
+        elif node.val == 0:
+            del self.keys[key]
+            if self.head == node:
+                self.head = nxt
+            else:
+                prev.nxt = nxt
+            
+            if self.tail == node:
+                self.tail = prev
+            else:
+                nxt.prev = prev
+    
+        # self.printItems()
 
     def getMaxKey(self) -> str:
-        if self.tail.prev == self.head:
-            return ""  # No keys exist
-        return next(
-            iter(self.tail.prev.keys)
-        )  # Return one of the keys from the tail's previous node
+        if self.tail:
+            return self.tail.key
+        
+        return ""
 
+    
     def getMinKey(self) -> str:
-        if self.head.next == self.tail:
-            return ""  # No keys exist
-        return next(
-            iter(self.head.next.keys)
-        )  # Return one of the keys from the head's next node
-
-    def removeNode(self, node):
-        prevNode = node.prev
-        nextNode = node.next
-
-        prevNode.next = nextNode  # Link previous node to next node
-        nextNode.prev = prevNode  # Link next node to previous node
+        if self.head:
+            return self.head.key
+        
+        return ""
+    
+    
+    def printItems(self):
+        curr = self.head
+        print("------")
+        while curr is not None:
+            print(curr.key, curr.val)
+            curr = curr.nxt
+        print("Tail", self.tail.key, self.tail.val)
