cc4ba2e541a4b40c647ff239bbe06efb8aedbe87
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-10-24 02:28:14 +0530

    Time: 263 ms (88.78%), Space: 87 MB (13.14%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
@@ -6,38 +6,33 @@
 #         self.right = right
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        level_sum = Counter()
-        
-        def init_level_sum(node, level):
-            if(not node):
+        def lsums(node, level = 0):
+            nonlocal level_sums
+            if not node:
                 return
             
-            level_sum[level] += node.val
-            init_level_sum(node.left, level + 1)
-            init_level_sum(node.right, level + 1)
+            level_sums[level] += node.val
+            lsums(node.left, level + 1)
+            lsums(node.right, level + 1)
         
-        def init_nodes(node, level):
-            if(not node):
+        def assign(node, level = 0):
+            if not node:
                 return
             
-            tot = 0
-            if(node.left):
-                tot += node.left.val
+            tot = ((node.left and node.left.val) or 0) + ((node.right and node.right.val) or 0)
+            if node.left:
+                node.left.val = level_sums[level + 1] - tot
             
-            if(node.right):
-                tot += node.right.val
+            if node.right:
+                node.right.val = level_sums[level + 1] - tot
             
-            if(node.left):
-                node.left.val = level_sum[level + 1] - tot
-            
-            if(node.right):
-                node.right.val = level_sum[level + 1] - tot
-            
-            init_nodes(node.left, level + 1)
-            init_nodes(node.right, level + 1)
+            assign(node.left, level + 1)
+            assign(node.right, level + 1)
         
-        init_level_sum(root, 0)
-        # print(level_sum)
-        init_nodes(root, 0)
+        level_sums = Counter()
+        lsums(root)
         root.val = 0
+        assign(root)
         return root
+        
+        
