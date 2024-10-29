edfd518af9713d89d365b5b37f01ce1e1302a6a9
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-10-30 00:02:20 +0530

    Time: 391 ms (12.12%), Space: 27.5 MB (56.18%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -1,34 +1,30 @@
 class Solution:
-    def maxMoves(self, grid):
-        M, N = len(grid), len(grid[0])
-
-        # Create a 2D list for dp, initialized to 0.
-        dp = [[0] * N for _ in range(M)]
-
-        # Initialize the first column with moves as 1.
-        for i in range(M):
-            dp[i][0] = 1
-
-        max_moves = 0
-        for j in range(1, N):
-            for i in range(M):
-                # Check all three possible previous cells:
-                # (i, j-1), (i-1, j-1), (i+1, j-1)
-                if grid[i][j] > grid[i][j - 1] and dp[i][j - 1] > 0:
-                    dp[i][j] = max(dp[i][j], dp[i][j - 1] + 1)
-                if (
-                    i - 1 >= 0
-                    and grid[i][j] > grid[i - 1][j - 1]
-                    and dp[i - 1][j - 1] > 0
-                ):
-                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
-                if (
-                    i + 1 < M
-                    and grid[i][j] > grid[i + 1][j - 1]
-                    and dp[i + 1][j - 1] > 0
-                ):
-                    dp[i][j] = max(dp[i][j], dp[i + 1][j - 1] + 1)
-
-                max_moves = max(max_moves, dp[i][j] - 1)
-
-        return max_moves
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        dp = [([0] * cols) for _ in range(rows)]
+        for r in range(rows):
+            dp[r][0] = 1
+        
+        max_val = 0
+        for c in range(1, cols):
+            for r in range(rows):
+                path = 0
+                for dr, dc in [(-1,-1), (0,-1), (1,-1)]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and \
+                    grid[nr][nc] < grid[r][c] and dp[nr][nc] > 0:
+                        path = max(path, dp[nr][nc] + 1)
+                
+                dp[r][c] = path        
+                max_val = max(max_val, path - 1)
+        
+        return max_val
+                    
+                    
+                    
+                
+        
+        
+        
+        
+        
