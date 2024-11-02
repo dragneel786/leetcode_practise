d3245ab025a6476804876d6821ed4f9894031370
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-11-02 14:04:41 +0530

    Delete 2490-circular-sentence directory

--- 2490-circular-sentence/2490-circular-sentence.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isCircularSentence(self, s: str) -> bool:
-        for i in range(len(s)):
-            if(s[i] == " " and s[i - 1] != s[i + 1]):
-                return False
-        
-        return s[0] == s[-1]
