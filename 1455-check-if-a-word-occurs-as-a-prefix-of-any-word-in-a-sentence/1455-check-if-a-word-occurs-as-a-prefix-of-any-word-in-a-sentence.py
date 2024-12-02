a7b546a45d06e6013e86c198d95f143ece11a20d
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-12-02 23:23:05 +0530

    Delete 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence directory

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        size = len(searchWord)
-        for i, word in enumerate(sentence.split()):
-            if word[:size] == searchWord:
-                return i + 1
-        
-        return -1
