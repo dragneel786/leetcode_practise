80ddc79cbdc7fc9e9aac2293857e9c7ebce0308a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-12-04 20:19:26 +0530

    Time: 78 ms (56.94%), Space: 17.8 MB (5.69%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        index2 = 0
+        for c in str1:
+            if index2 >= len(str2):
+                break
+                
+            inc_ch = chr(((ord(c) - 97) % 26 + 1) + 97) if c != 'z' else 'a'
+            if c == str2[index2] or inc_ch == str2[index2]:
+                index2 += 1
+                
+        return index2 >= len(str2)
