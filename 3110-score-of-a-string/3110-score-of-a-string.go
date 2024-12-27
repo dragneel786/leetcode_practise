1cae36b19488bc49aff345b350fabec631799912
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2024-12-27 19:18:01 +0530

    Delete 3110-score-of-a-string directory

--- 3110-score-of-a-string/3110-score-of-a-string.go
+++
@@ -1,7 +0,0 @@
-func scoreOfString(s string) int {
-    var score = 0
-    for i := 1; i < len(s); i++ {
-        score += int(math.Abs(float64(int(s[i - 1]) - int(s[i]))))
-    }
-    return score
-}
