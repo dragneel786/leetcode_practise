f3381076e7ba3f6fa376cff2b76eb3059e3d267a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-01-22 23:49:17 +0530

    [1765] [Time Beats: 32.00%] [Memory Beats: 19.29%] - LeetPush

---
+++ DCP-01-25/1765-Map-of-Highest-Peak.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        tail = list2
+        while(tail.next):
+            tail = tail.next
+        
+        temp = list1
+        prev = None
+        idx = 0
+        while(temp and idx < a):
+            prev = temp
+            temp = temp.next
+            idx += 1
+        
+        while(temp and idx < b):
+            temp = temp.next
+            idx += 1
+        
+        prev.next = list2
+        tail.next = temp.next
+        return list1
+        
+            
+            
+            
+        
