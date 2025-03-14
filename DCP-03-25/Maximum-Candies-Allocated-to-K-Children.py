4bf4224f2ecbc7abd7b08a9cbe094e8d73cfaa89
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-03-14 15:50:27 +0530

    [2226] [Time Beats: 70.01%] [Memory Beats: 18.92%] - LeetPush

--- DCP-03-25/Maximum-Candies-Allocated-to-K-Children.py
+++ DCP-03-25/Maximum-Candies-Allocated-to-K-Children.py
@@ -1,24 +1,26 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-        
         def is_possible(val):
+            if val == 0:
+                return True
             div_in_k = 0
             for candy in candies:
                 div_in_k += (candy // val)
-
             return div_in_k >= k
-
-
-        left, right = 1, sum(candies) // k
-        res = 0
-        while(left <= right):
+        
+        # Handle edge case where we can't give any candy
+        if sum(candies) < k:
+            return 0
+            
+        left, right = 1, max(candies)
+        
+        while left <= right:
             middle = left + (right - left) // 2
             if is_possible(middle):
-                res = middle
                 left = middle + 1
             else:
                 right = middle - 1
         
-        return res
+        return right
 
         
