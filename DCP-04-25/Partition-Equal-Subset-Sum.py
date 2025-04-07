bb8d60b00320a562261f41be6f8c4e77992310d2
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-04-07 20:14:33 +0530

    [416] [Time Beats: 13.59%] [Memory Beats: 100.00%] - LeetPush

---
+++ DCP-04-25/Partition-Equal-Subset-Sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        n = len(nums)
+        if(total & 1): return False
+
+        half = total // 2
+        dp = [True] + ([False] * half)
+        
+        for i in range(1, n + 1):
+            temp = [True] + ([False] * half)
+            
+            for j in range(1, half + 1):
+                temp[j] = dp[j]
+                if(j >= nums[i - 1] and not temp[j]):
+                    temp[j] = dp[j - nums[i - 1]]
+            
+            dp = temp[:]
+            
+        return dp[half]
