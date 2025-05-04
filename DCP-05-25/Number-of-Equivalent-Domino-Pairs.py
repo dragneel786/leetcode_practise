a59e9851cd306ce4cdde2a5cb1a2058f53e4f70a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-05-04 22:06:40 +0530

    [1128] [Time Beats: 41.51%] [Memory Beats: 81.31%] - LeetPush

--- DCP-05-25/Number-of-Equivalent-Domino-Pairs.py
+++ DCP-05-25/Number-of-Equivalent-Domino-Pairs.py
@@ -1,16 +1,16 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        pmap = defaultdict(int)
-        pair = 0
+        counts = Counter()
+        res = 0
         for a, b in dominoes:
-            v1 = pmap[(a, b)]
-            v2 = pmap[(b, a)]
+            v1 = counts[(a,b)]
+            v2 = counts[(b,a)]
             
-            if(a == b):
+            if a == b:
                 v2 = 0
             
-            pair += (v1 + v2)
-            pmap[(a, b)] += 1
-        
-        return pair
+            res += (v1 + v2)
+            counts[(a,b)] += 1
+
+        return res
             
