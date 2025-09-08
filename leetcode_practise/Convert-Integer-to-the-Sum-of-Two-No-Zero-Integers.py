143a478c1401964bdae35a3a3301944db8de8b95
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-09-09 00:50:04 +0530

    [1317] [Time Beats: 11.68%] [Memory Beats: 52.92%] - LeetPush

--- leetcode_practise/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.py
+++ leetcode_practise/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.py
@@ -1,9 +1,30 @@
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        def check(x):
-            return '0' not in str(x)
+        def check(a, b):
+            while(a > 0):
+                rem_a = a % 10
+                a //= 10
+                if rem_a == 0:
+                    return False
+            
+            while(b > 0):
+                rem_b = b % 10
+                if rem_b == 0:
+                    return False
+                
+                b //= 10
+            
+            return True
 
-        for i in range(1, n):
-            j = n - i
-            if check(i) and check(j):
-                return [i, j]
+        a, b = 1, n - 1
+        while(True):
+            if (a + b) == n and check(a, b):
+                return [a, b]
+
+            a += 1
+            b -= 1
+        
+        return True
+        
+
+        
