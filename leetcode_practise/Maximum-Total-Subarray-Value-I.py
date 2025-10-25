fa238f99c98a07f66a30bbed91381ae67e3d7e5e
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-10-25 18:17:32 +0530

    [3689] [Time Beats: 74.70%] [Memory Beats: 58.46%] - LeetPush

--- leetcode_practise/Maximum-Total-Subarray-Value-I.py
+++ leetcode_practise/Maximum-Total-Subarray-Value-I.py
@@ -1,9 +1,4 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
-        ans = 0
-        min_val = min(nums)
-        max_val = max(nums)
-        for _ in range(k):
-            ans += (max_val - min_val)
+        return (max(nums) - min(nums)) * k
         
-        return ans
