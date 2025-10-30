1a59324c4b7b141a5931e33bb4814c46c385d5c0
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-10-30 23:33:17 +0530

    [1526] [Time Beats: 36.34%] [Memory Beats: 19.19%] - LeetPush

---
+++ leetcode_practise/Minimum-Number-of-Increments-on-Subarrays-to-Form-a-Target-Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minNumberOperations(self, target: List[int]) -> int:
+        res = pre = 0
+        for t in target:
+            res += max(t - pre, 0)
+            pre = t
+        
+        return res
+            
+                
+                
+            
