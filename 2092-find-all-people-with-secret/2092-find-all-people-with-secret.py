8c588db1c12dfdf93bee0f0d31c829bf498c4e8b
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-12-20 01:45:23 +0530

    Time: 388 ms (34.57%), Space: 68.8 MB (39.5%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,27 +1,22 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        def create_graph(meetings):
-            g = defaultdict(list)
-            for x, y, time in meetings:
-                g[x].append((y, time))
-                g[y].append((x, time))
-            
-            return g
-        
-        graph = create_graph(meetings)
-        earliest = [inf] * n
-        earliest[0] = 0
-        earliest[firstPerson] = 0
-        
-        q = deque([(0, 0), (0, firstPerson)])
-        ans = set()
-        while(q):
-            ptime, person = q.popleft()
-            ans.add(person)
-            for ns, time in graph[person]:
-                if(time >= ptime and earliest[ns] > time):
-                    q.append((time, ns))
-                    earliest[ns] = time
-                    
-        return list(ans)
-            
+        g = defaultdict(list)
+        for x, y, t in meetings:
+            g[x].append((y, t))
+            g[y].append((x, t))
+
+        inf = 10**18
+        time = [inf] * n
+        time[0] = time[firstPerson] = 0
+        h = [(0, 0), (0, firstPerson)]
+
+        while h:
+            t, u = heapq.heappop(h)
+            if t > time[u]:
+                continue
+            for v, mt in g[u]:
+                if mt >= t and mt < time[v]:
+                    time[v] = mt
+                    heapq.heappush(h, (mt, v))
+
+        return [i for i in range(n) if time[i] < inf]
