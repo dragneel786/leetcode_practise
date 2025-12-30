02650f2bd361449b41440258ee04ea6f7483d75c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2025-12-30 22:57:11 +0530

    Time: 4 ms (12.12%), Space: 17.3 MB (90.91%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,38 +1,37 @@
 class Solution:
-    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        
-        def helper(sr, sc):
-            row_sum = [0] * 3
-            col_sum = [0] * 3
-            dig = [0, 0]
-            seen = set()
-            for dr in range(3):
-                for dc in range(3):
-                    nr, nc = r + dr, c + dc
-                    if grid[nr][nc] > 9 or grid[nr][nc] < 1 \
-                    or grid[nr][nc] in seen:
-                        return False
-                    
-                    row_sum[dr] += grid[nr][nc]
-                    col_sum[dc] += grid[nr][nc]
-                    seen.add(grid[nr][nc])
-            
-            d = 0
-            for dr, dc in [[1,1], [1,-1]]:
-                for _ in range(3):
-                    nr, nc = sr + dr, sc + dc
-                    dig[d] += grid[nr][nc]
-                
-                d += 1
-                sc = sc + 2
-            print(row_sum, col_sum, dig)
-            return len(set(row_sum) | set(col_sum) | set(dig)) == 1
-                
-        
-        res = 0
-        rows, cols = len(grid), len(grid[0])
-        for r in range(rows - 2):
-            for c in range(cols - 2):
-                res += helper(r, c)
-        
-        return res
+    def isMagicSquare(self, grid, r, c):
+        if grid[r+1][c+1] != 5:
+            return False
+
+        a = grid[r][c]; b = grid[r][c+1]; d = grid[r][c+2]  # grid ele except centre
+        e = grid[r+1][c]; f = grid[r+1][c+2]
+        h = grid[r+2][c]; i = grid[r+2][c+1]; j = grid[r+2][c+2]
+
+        seen = [False] * 10
+        arr = [a,b,d,e,f,h,i,j,5]
+        for x in arr:
+            if x < 1 or x > 9 or seen[x]:
+                return False
+            seen[x] = True
+
+        if a + j != 10: return False
+        if b + i != 10: return False
+        if d + h != 10: return False
+        if e + f != 10: return False
+
+        if a + b + d != 15: return False
+        if h + i + j != 15: return False
+        if a + e + h != 15: return False
+        if d + f + j != 15: return False
+        if a + 5 + j != 15: return False
+        if d + 5 + h != 15: return False
+
+        return True
+
+    def numMagicSquaresInside(self, grid):
+        m = len(grid); n = len(grid[0]); c = 0
+        for i in range(m-2):
+            for j in range(n-2):
+                if self.isMagicSquare(grid,i,j):
+                    c += 1
+        return c
