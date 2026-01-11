b57ec13d6a18d13fa46779756ad6ca1c4e245220
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-01-11 19:24:57 +0530

    Time: 1127 ms (5.03%), Space: 23.5 MB (93.86%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,33 +1,47 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        
-        def max_histo_area(histo):
-            max_area = 0
-            histo.append(0)
-            st = deque()
-            for i, h in enumerate(histo):
-                while(st and histo[st[-1]] > h):
-                    width = histo[st.pop()]
-                    left_small = st[-1] if(st) else -1
-                    temp_area = (i - left_small - 1) * width
-                    max_area = max(max_area, temp_area)
+        if not matrix or not matrix[0]:
+            return 0
 
-                st.append(i)
+        M = len(matrix)
+        N = len(matrix[0])
 
-            return max_area
-        
-        m, n = len(matrix), len(matrix[0])
-        heights = [0] * n
-        res = 0
-        for i in range(m):
-            for j in range(n):
-                if(matrix[i][j] == '0'):
-                    heights[j] = 0
-                else:
-                    heights[j] += 1
-            
-            res = max(res, max_histo_area(heights))
-            
-        
-        return res
-                
+        # Convert characters to integers
+        for i in range(M):
+            for j in range(N):
+                matrix[i][j] = int(matrix[i][j])
+
+        # Row-wise prefix widths
+        for i in range(M):
+            for j in range(1, N):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i][j - 1]
+
+        Ans = 0
+
+        # Fix each column
+        for j in range(N):
+            for i in range(M):
+                width = matrix[i][j]
+                if width == 0:
+                    continue
+
+                # Expand downward
+                currWidth = width
+                k = i
+                while k < M and matrix[k][j] > 0:
+                    currWidth = min(currWidth, matrix[k][j])
+                    height = k - i + 1
+                    Ans = max(Ans, currWidth * height)
+                    k += 1
+
+                # Expand upward
+                currWidth = width
+                k = i
+                while k >= 0 and matrix[k][j] > 0:
+                    currWidth = min(currWidth, matrix[k][j])
+                    height = i - k + 1
+                    Ans = max(Ans, currWidth * height)
+                    k -= 1
+
+        return Ans
