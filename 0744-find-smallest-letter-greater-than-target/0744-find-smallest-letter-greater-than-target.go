041934be68ffe29f697c80eb6027fb73494bd42a
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-02-06 00:34:39 +0530

    Time: 0 ms (100%), Space: 4.6 MB (73.76%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.go
@@ -0,0 +1,8 @@
+func nextGreatestLetter(letters []byte, target byte) byte {
+    for _, c := range letters {
+        if c > target {
+            return c
+        }
+    }
+    return letters[0]
+}
