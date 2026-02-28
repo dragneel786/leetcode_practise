149c17fcc48b080626a32feb8790be0f39623075
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-02-28 22:20:41 +0530

    Time: 747 ms (47.8%), Space: 19 MB (94.34%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,10 +1,12 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        MOD = 10 ** 9 + 7
-        res = 1
-        size = 1
-        for i in range(2, n + 1):
-            size += (i & (i - 1) == 0)
-            res = ((res << size) | i) % MOD
-        
+        MOD = 10**9 + 7
+        res = 0
+        bits = 0
+
+        for i in range(1, n+1):
+            if (i & (i-1)) == 0:
+                bits += 1
+            res = ((res << bits) | i) % MOD
+
         return res
