d723fe60ff55d9648995999206fb9ca39f4518fe
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-03-17 23:51:32 +0530

    Time: 127 ms (67.74%), Space: 33.5 MB (100%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -1,32 +1,20 @@
 class Solution:
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
-        rows = len(matrix)
-        cols = len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
+        prev_row = [0] * n
         ans = 0
-        prev_heights = []
         
-        for row in range(rows):
-            heights = []
-            seen = [False] * cols
-            
-            for height, col in prev_heights:
-                if(matrix[row][col] == 1):
-                    heights.append((height + 1, col))
-                    seen[col] = True
-            
-            for col in range(cols):
-                if(not seen[col] and matrix[row][col] == 1):
-                    heights.append((1, col))
-            
-            min_val = inf
-            for i, (h, c) in enumerate(heights):
-                min_val = min(min_val, h)
-                ans = max(h * (i + 1), ans)
-            
-            prev_heights = heights
-        
+        for row in range(m):
+            curr_row = matrix[row][:]
+            for col in range(n):
+                if curr_row[col] != 0:
+                    curr_row[col] += prev_row[col]
+
+            sorted_row = sorted(curr_row, reverse=True)
+            for i in range(n):
+                ans = max(ans, sorted_row[i] * (i + 1))
+
+            prev_row = curr_row
+
         return ans
-            
-            
-            
-            
