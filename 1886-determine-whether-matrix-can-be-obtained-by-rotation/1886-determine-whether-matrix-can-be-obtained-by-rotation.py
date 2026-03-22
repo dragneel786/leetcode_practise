4eadad05693f026063b8b44f36ccf85248f3074c
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-03-22 22:40:32 +0530

    Time: 0 ms (100%), Space: 19.1 MB (100%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,18 +1,25 @@
 class Solution:
-    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        def make90(mat):
-            res = []
-            for i in range(len(mat)):
-                temp = []
-                for j in range(len(mat) - 1, -1, -1):
-                    temp.append(mat[j][i])
-                res.append(temp)
-            return res
-            
-        for _ in range(4):
-            if(mat == target):
+    def findRotation(
+        self, mat: List[List[int]], target: List[List[int]]
+    ) -> bool:
+        n = len(mat)
+        # at most 4 rotations
+        for k in range(4):
+            # rotation operation
+            for i in range(n // 2):
+                for j in range((n + 1) // 2):
+                    (
+                        mat[i][j],
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                    ) = (
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                        mat[i][j],
+                    )
+
+            if mat == target:
                 return True
-            mat = make90(mat)
-        
         return False
-                
