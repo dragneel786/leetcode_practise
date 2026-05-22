e384f5535aa7530cb7985724e156f196bad05712
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-05-22 19:15:22 +0530

    Time: 0 ms (100%), Space: 19.6 MB (10.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,34 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        def find_pivot():
-            low = 0
-            high = n - 1
-            while(low <= high):
-                mid = low + ((high - low) // 2)
-                if(mid != 0 and nums[mid - 1] > nums[mid]):
-                    return mid
-                
-                if(nums[0] > nums[mid]):
-                    high = mid - 1
-                else:
-                    low = mid + 1
-            
-            return low
-                    
         n = len(nums)
-        idx = find_pivot()
-        left = nums[:idx]
-        right = nums[idx:]
-    
-        if(left):
-            i = bisect_right(left, target)
-            if(left[i - 1 if(i) else i] == target):
-                return i - 1
+        rot = bisect_left(nums, True, key=lambda n: n <= nums[-1])
         
-        if(right):
-            i = bisect_right(right, target)
-            if(right[i - 1] == target):
-                return idx + i - 1
-        
+        lo, hi = 0, n - 1
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            real = (mid + rot) % n
+
+            if nums[real] == target:
+                return real
+                
+            if nums[real] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
         return -1
