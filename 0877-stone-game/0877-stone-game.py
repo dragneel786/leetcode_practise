e537255ad189c9e691bfdd59c321f34ce0f5e884
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-08-02 19:07:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (75.06%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,19 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        
-        @lru_cache(None)
-        def check_win(alice, i, j):
-            if(i > j):
-                return 0
-            
-            val1 = piles[i] + check_win(not alice, i + 1, j)
-            val2 = piles[j] + check_win(not alice, i, j - 1)
-            if(alice):
-                return max(val1, val2)
-            else:
-                return min(val1, val2)
-            
-        
-        val = check_win(True, 0, len(piles) - 1)
-        sums = sum(piles)
-        return val > sums-val
+        return True
