79fc3fbd748ef75295621f4eaefcba997363e1e3
Mohammad Shahrukh <55229284+dragneel786@users.noreply.github.com>
2026-08-09 22:36:04 +0530

    Time: 67 ms (94.78%), Space: 26.2 MB (49.15%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,22 +1,13 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
-        
-        @lru_cache(None)
-        def max_stone(alice, i, M):
-            if(i >= len(piles)):
-                return 0
-            
-            res = 0 if(alice) else inf
-            tot = 0
-            for j in range(1, 2 * M  + 1):
-                if(i + j > len(piles)):
-                    break
-                
-                tot += piles[i + j - 1]
-                val = max_stone(not alice, i + j, max(M, j))
-                res = max(res, tot + val) if(alice) else min(res, val)
-            
-            return res
-            
-        
-        return max_stone(True, 0, 1)
+        for i in range(len(piles) - 2, -1, -1):
+            piles[i] += piles[i + 1]
+
+        @cache
+        def dfs(i, M):
+            if i + M * 2 >= len(piles):
+                return piles[i]
+
+            return piles[i] - min(dfs(i + j, max(M, j)) for j in range(1, M * 2 + 1))
+
+        return dfs(0, 1)
